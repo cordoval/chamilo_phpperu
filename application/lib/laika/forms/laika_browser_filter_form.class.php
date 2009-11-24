54845c8b8f0d052ab5d8623372d5f1d251b7f651
scaramanga <devnull@localhost>
2009-11-24 15:20:43 +0100

    A few more small Laika fixes.

--- application/lib/laika/forms/laika_browser_filter_form.class.php
+++ application/lib/laika/forms/laika_browser_filter_form.class.php
@@ -11,7 +11,7 @@ class LaikaBrowserFilterForm extends FormValidator
     const BROWSER_FILTER_SCALE = 'filter_scale';
     const BROWSER_FILTER_CODE = 'filter_code';
     const BROWSER_FILTER_GROUP = 'filter_group';
-    
+
     private $manager;
     private $renderer;
 
@@ -25,14 +25,14 @@ class LaikaBrowserFilterForm extends FormValidator
     function __construct($manager, $url)
     {
         parent :: __construct('laika_browser_filter_form', 'post', $url);
-        
+
         $this->renderer = clone $this->defaultRenderer();
         $this->manager = $manager;
-        
+
         $this->build_form();
-        
+
         $this->setDefaults();
-        
+
         $this->accept($this->renderer);
     }
 
@@ -42,10 +42,10 @@ class LaikaBrowserFilterForm extends FormValidator
     private function build_form()
     {
         $ldm = LaikaDataManager :: get_instance();
-        
+
         $this->renderer->setFormTemplate('<form {attributes}><div class="filter_form">{content}</div><div class="clear">&nbsp;</div></form>');
         $this->renderer->setElementTemplate('<div class="row"><div class="formw">{label}&nbsp;{element}</div></div>');
-        
+
         // The Laika Scales
         $scales = $ldm->retrieve_laika_scales(null, null, null, new ObjectTableOrder(LaikaScale :: PROPERTY_TITLE));
         $scale_options = array();
@@ -53,15 +53,15 @@ class LaikaBrowserFilterForm extends FormValidator
         {
             $scale_options[$scale->get_id()] = $scale->get_title();
         }
-        
+
         // The Laika Percentile Codes
-        $codes = $ldm->retrieve_percentile_codes(array(LaikaResult :: PROPERTY_PERCENTILE_CODE));
+        $codes = $ldm->retrieve_percentile_codes();
         $code_options = array();
         foreach ($codes as $code)
         {
             $code_options[$code] = $code;
         }
-        
+
         $this->addElement('select', self :: BROWSER_FILTER_SCALE, Translation :: get('Scale'), $scale_options);
         $this->addElement('select', self :: BROWSER_FILTER_GROUP, Translation :: get('Group'), $this->get_groups());
         $this->addElement('select', self :: BROWSER_FILTER_CODE, Translation :: get('Code'), $code_options);
@@ -75,17 +75,17 @@ class LaikaBrowserFilterForm extends FormValidator
         {
             return null;
         }
-        
+
         $filter_scale = Request :: get(self :: BROWSER_FILTER_SCALE);
         $filter_code = Request :: get(self :: BROWSER_FILTER_CODE);
         $filter_group = Request :: get(self :: BROWSER_FILTER_GROUP);
-        
+
         $form_validates = $this->validate();
-        
+
         if ($form_validates)
         {
             $values = $this->exportValues();
-            
+
             $filter_scale = $values[self :: BROWSER_FILTER_SCALE];
             $filter_code = $values[self :: BROWSER_FILTER_CODE];
             $filter_group = $values[self :: BROWSER_FILTER_GROUP];
@@ -96,27 +96,27 @@ class LaikaBrowserFilterForm extends FormValidator
             $filter_code = Request :: get(self :: BROWSER_FILTER_CODE);
             $filter_group = Request :: get(self :: BROWSER_FILTER_GROUP);
         }
-        
+
         $conditions = array();
         $conditions[] = new EqualityCondition(LaikaCalculatedResult :: PROPERTY_SCALE_ID, $filter_scale);
         $conditions[] = new EqualityCondition(LaikaCalculatedResult :: PROPERTY_PERCENTILE_CODE, $filter_code);
-        
+
         if ($filter_group != '0')
         {
             $gdm = GroupDataManager :: get_instance();
             $group = $gdm->retrieve_group($filter_group);
             $users = $group->get_users(true, true);
-            
+
             if (count($users) == 0)
             {
                 $users = array('0');
             }
-            
+
             $conditions[] = new InCondition(LaikaAttempt :: PROPERTY_USER_ID, $users, LaikaAttempt :: get_table_name());
         }
-        
+
         $condition = new AndCondition($conditions);
-        
+
         return $condition;
     }
 
@@ -126,16 +126,16 @@ class LaikaBrowserFilterForm extends FormValidator
         {
             return array();
         }
-        
+
         if ($this->validate())
         {
             $values = $this->exportValues();
-            
+
             $parameters = array();
             $parameters[self :: BROWSER_FILTER_SCALE] = $values[self :: BROWSER_FILTER_SCALE];
             $parameters[self :: BROWSER_FILTER_CODE] = $values[self :: BROWSER_FILTER_CODE];
             $parameters[self :: BROWSER_FILTER_GROUP] = $values[self :: BROWSER_FILTER_GROUP];
-            
+
             return $parameters;
         }
         else
@@ -144,7 +144,7 @@ class LaikaBrowserFilterForm extends FormValidator
             $parameters[self :: BROWSER_FILTER_SCALE] = Request :: get(self :: BROWSER_FILTER_SCALE);
             $parameters[self :: BROWSER_FILTER_CODE] = Request :: get(self :: BROWSER_FILTER_CODE);
             $parameters[self :: BROWSER_FILTER_GROUP] = Request :: get(self :: BROWSER_FILTER_GROUP);
-            
+
             return $parameters;
         }
     }
@@ -154,7 +154,7 @@ class LaikaBrowserFilterForm extends FormValidator
         $filter_scale = Request :: get(self :: BROWSER_FILTER_SCALE);
         $filter_code = Request :: get(self :: BROWSER_FILTER_CODE);
         $filter_group = Request :: get(self :: BROWSER_FILTER_GROUP);
-        
+
         return (isset($filter_scale) && isset($filter_code) && isset($filter_group));
     }
 
@@ -175,14 +175,14 @@ class LaikaBrowserFilterForm extends FormValidator
         $group_menu = new GroupMenu(null, null, false, true);
         $renderer = new OptionsMenuRenderer();
         $group_menu->render($renderer, 'sitemap');
-        
+
         $options = $renderer->toArray();
-        
+
         if (count($options) === 0)
         {
             $options[0] = '--&nbsp;' . Translation :: get('NoGroupsAvailable') . '&nbsp;--';
         }
-        
+
         return $options;
     }
 
@@ -191,18 +191,18 @@ class LaikaBrowserFilterForm extends FormValidator
         $filter_scale = Request :: get(self :: BROWSER_FILTER_SCALE);
         $filter_code = Request :: get(self :: BROWSER_FILTER_CODE);
         $filter_group = Request :: get(self :: BROWSER_FILTER_GROUP);
-        
+
         $filter_scale_set = isset($filter_scale);
         $filter_code_set = isset($filter_code);
         $filter_group_set = isset($filter_group);
-        
+
         if ($filter_scale_set && $filter_code_set && $filter_group_set)
         {
             $defaults[self :: BROWSER_FILTER_SCALE] = Request :: get(self :: BROWSER_FILTER_SCALE);
             $defaults[self :: BROWSER_FILTER_CODE] = Request :: get(self :: BROWSER_FILTER_CODE);
             $defaults[self :: BROWSER_FILTER_GROUP] = Request :: get(self :: BROWSER_FILTER_GROUP);
         }
-        
+
         parent :: setDefaults($defaults);
     }
 }
