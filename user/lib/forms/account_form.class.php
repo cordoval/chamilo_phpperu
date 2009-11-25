1be819cecfff62964ce1044bf6d59f5331c8a50d
scaramanga <devnull@localhost>
2009-11-25 14:24:24 +0100

    Continuing work on Authentication-extensions + bugfix (http://support.chamilo.org/issues/187)

--- user/lib/forms/account_form.class.php
+++ user/lib/forms/account_form.class.php
@@ -10,6 +10,7 @@ class AccountForm extends FormValidator
     const TYPE_EDIT = 2;
     const RESULT_SUCCESS = 'UserUpdated';
     const RESULT_ERROR = 'UserUpdateFailed';
+    const CURRENT_PASSWORD = 'current_password';
 
     private $parent;
     private $user;
@@ -221,7 +222,7 @@ class AccountForm extends FormValidator
         }
         if (PlatformSetting :: get('allow_change_password', UserManager :: APPLICATION_NAME) && strlen($values[User :: PROPERTY_PASSWORD]) && Authentication :: factory($this->user->get_auth_source())->is_password_changeable())
         {
-            $user->set_password(Hashing :: hash($values[User :: PROPERTY_PASSWORD]));
+            Authentication :: factory($this->user->get_auth_source())->change_password($user, $values[self :: CURRENT_PASSWORD], $values[User :: PROPERTY_PASSWORD]);
         }
         if (PlatformSetting :: get('allow_change_user_picture', UserManager :: APPLICATION_NAME))
         {
