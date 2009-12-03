55048f61b936421cc33227e94ee606d158026a4b
scaramanga <devnull@localhost>
2009-12-03 20:35:20 +0100

    Icon differentiation between adding prerequisites and changing them in the learning path builder.

--- repository/lib/complex_builder/learning_path/component/browser/learning_path_browser_table_cell_renderer.class.php
+++ repository/lib/complex_builder/learning_path/component/browser/learning_path_browser_table_cell_renderer.class.php
@@ -20,7 +20,7 @@ class LearningPathBrowserTableCellRenderer extends ComplexBrowserTableCellRender
         $this->count = RepositoryDataManager :: get_instance()->count_complex_content_object_items($condition);
         parent :: __construct($browser, $condition);
     }
-    
+
     private $lpi_ref_object;
 
     // Inherited
@@ -40,28 +40,28 @@ class LearningPathBrowserTableCellRenderer extends ComplexBrowserTableCellRender
                 $lo = $this->lpi_ref_object;
             }
         }
-        
+
         if ($column === ComplexBrowserTableColumnModel :: get_modification_column())
         {
             return $this->get_modification_links($cloi, $ref_lo);
         }
-        
+
         switch ($column->get_name())
         {
             case Translation :: get(Utilities :: underscores_to_camelcase(ContentObject :: PROPERTY_TITLE)) :
                 $title = htmlspecialchars($lo->get_title());
                 $title_short = $title;
-                
+
                 $title_short = Utilities :: truncate_string($title_short, 53, false);
-                
+
                 if ($lo->get_type() == 'learning_path')
                 {
                     $title_short = '<a href="' . $this->browser->get_url(array(ComplexBuilder :: PARAM_ROOT_LO => $this->browser->get_root(), ComplexBuilder :: PARAM_CLOI_ID => $cloi->get_id(), 'publish' => Request :: get('publish'))) . '">' . $title_short . '</a>';
                 }
-                
+
                 return $title_short;
         }
-        
+
         return parent :: render_cell($column, $cloi, $lo);
     }
 
@@ -69,22 +69,30 @@ class LearningPathBrowserTableCellRenderer extends ComplexBrowserTableCellRender
     {
         $additional_items = array();
         $parent = RepositoryDataManager :: get_instance()->retrieve_content_object($cloi->get_parent());
-        
+
         if ($lo->get_type() == 'learning_path_item')
         {
             if ($parent->get_version() == 'chamilo' && $this->count > 1)
             {
-                $additional_items[] = array('href' => $this->browser->get_prerequisites_url($cloi->get_id()), 'label' => Translation :: get('BuildPrerequisites'), 'img' => Theme :: get_common_image_path() . 'action_maintenance.png');
+                $prerequisites = $cloi->get_prerequisites();
+                if (!empty($prerequisites))
+                {
+                    $additional_items[] = array('href' => $this->browser->get_prerequisites_url($cloi->get_id()), 'label' => Translation :: get('EditPrerequisites'), 'img' => Theme :: get_common_image_path() . 'action_edit_prerequisites.png');
+                }
+                else
+                {
+                    $additional_items[] = array('href' => $this->browser->get_prerequisites_url($cloi->get_id()), 'label' => Translation :: get('BuildPrerequisites'), 'img' => Theme :: get_common_image_path() . 'action_build_prerequisites.png');
+                }
             }
-            
+
             if ($this->lpi_ref_object->get_type() == 'assessment')
             {
                 $additional_items[] = array('href' => $this->browser->get_mastery_score_url($cloi->get_id()), 'label' => Translation :: get('SetMasteryScore'), 'img' => Theme :: get_common_image_path() . 'action_quota.png');
             }
         }
-        
+
         $toolbar_data = array();
-        
+
         $edit_url = $this->browser->get_complex_content_object_item_edit_url($cloi, $this->browser->get_root());
         if ($cloi->is_extended() || get_parent_class($this->browser) == 'ComplexBuilder')
         {
@@ -94,18 +102,18 @@ class LearningPathBrowserTableCellRenderer extends ComplexBrowserTableCellRender
         {
             $toolbar_data[] = array('label' => Translation :: get('EditNA'), 'img' => Theme :: get_common_image_path() . 'action_edit_na.png');
         }
-        
+
         if ($parent->get_version() == 'chamilo')
         {
-            
+
             $delete_url = $this->browser->get_complex_content_object_item_delete_url($cloi, $this->browser->get_root());
             $moveup_url = $this->browser->get_complex_content_object_item_move_url($cloi, $this->browser->get_root(), RepositoryManager :: PARAM_DIRECTION_UP);
             $movedown_url = $this->browser->get_complex_content_object_item_move_url($cloi, $this->browser->get_root(), RepositoryManager :: PARAM_DIRECTION_DOWN);
-            
+
             $toolbar_data[] = array('href' => $delete_url, 'label' => Translation :: get('Delete'), 'img' => Theme :: get_common_image_path() . 'action_delete.png', 'confirm' => true);
-            
+
             $allowed = $this->check_move_allowed($cloi);
-            
+
             if ($allowed["moveup"])
             {
                 $toolbar_data[] = array('href' => $moveup_url, 'label' => Translation :: get('MoveUp'), 'img' => Theme :: get_common_image_path() . 'action_up.png');
@@ -113,9 +121,9 @@ class LearningPathBrowserTableCellRenderer extends ComplexBrowserTableCellRender
             else
             {
                 $toolbar_data[] = array('label' => Translation :: get('MoveUpNA'), 'img' => Theme :: get_common_image_path() . 'action_up_na.png');
-            
+
             }
-            
+
             if ($allowed["movedown"])
             {
                 $toolbar_data[] = array('href' => $movedown_url, 'label' => Translation :: get('MoveDown'), 'img' => Theme :: get_common_image_path() . 'action_down.png');
@@ -125,11 +133,11 @@ class LearningPathBrowserTableCellRenderer extends ComplexBrowserTableCellRender
                 $toolbar_data[] = array('label' => Translation :: get('MoveDownNA'), 'img' => Theme :: get_common_image_path() . 'action_down_na.png');
             }
         }
-        
+
         $toolbar_data = array_merge($toolbar_data, $additional_items);
-        
+
         return Utilities :: build_toolbar($toolbar_data);
-        
+
     //return parent :: get_modification_links($cloi, $additional_items);
     }
 }
