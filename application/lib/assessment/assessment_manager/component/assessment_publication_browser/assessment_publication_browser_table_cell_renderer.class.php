638eee3764b26b77376b0fdfbd59a373c59af59f
scaramanga <devnull@localhost>
2010-01-13 14:54:32 +0100

    Layout fixes (and tryouts)

--- application/lib/assessment/assessment_manager/component/assessment_publication_browser/assessment_publication_browser_table_cell_renderer.class.php
+++ application/lib/assessment/assessment_manager/component/assessment_publication_browser/assessment_publication_browser_table_cell_renderer.class.php
@@ -12,7 +12,7 @@ require_once dirname(__FILE__) . '/../../assessment_manager.class.php';
  * Cell rendere for the learning object browser table
  *
  * @author Sven Vanpoucke
- * @author 
+ * @author
  */
 
 class AssessmentPublicationBrowserTableCellRenderer extends DefaultAssessmentPublicationTableCellRenderer
@@ -40,7 +40,7 @@ class AssessmentPublicationBrowserTableCellRenderer extends DefaultAssessmentPub
         {
             return $this->get_modification_links($assessment_publication);
         }
-        
+
         return parent :: render_cell($column, $assessment_publication);
     }
 
@@ -53,18 +53,18 @@ class AssessmentPublicationBrowserTableCellRenderer extends DefaultAssessmentPub
     private function get_modification_links($assessment_publication)
     {
         $assessment = $assessment_publication->get_publication_object();
-        
+
         $toolbar_data = array();
         $toolbar_data[] = array('href' => $this->browser->get_assessment_publication_viewer_url($assessment_publication), 'label' => Translation :: get('TakeAssessment'), 'img' => Theme :: get_common_image_path() . 'action_next.png');
         $toolbar_data[] = array('href' => $this->browser->get_assessment_results_viewer_url($assessment_publication), 'label' => Translation :: get('ViewResults'), 'img' => Theme :: get_common_image_path() . 'action_view_results.png');
-        
+
         $user = $this->browser->get_user();
-        
+
         if ($user->is_platform_admin() || $user->get_id() == $assessment_publication->get_publisher())
         {
             $toolbar_data[] = array('href' => $this->browser->get_delete_assessment_publication_url($assessment_publication), 'label' => Translation :: get('Delete'), 'img' => Theme :: get_common_image_path() . 'action_delete.png');
             $toolbar_data[] = array('href' => $this->browser->get_update_assessment_publication_url($assessment_publication), 'label' => Translation :: get('Edit'), 'img' => Theme :: get_common_image_path() . 'action_edit.png');
-            
+
             if ($assessment_publication->get_hidden())
             {
                 $toolbar_data[] = array('href' => $this->browser->get_change_assessment_publication_visibility_url($assessment_publication), 'label' => Translation :: get('Show'), 'img' => Theme :: get_common_image_path() . 'action_visible_na.png');
@@ -73,21 +73,21 @@ class AssessmentPublicationBrowserTableCellRenderer extends DefaultAssessmentPub
             {
                 $toolbar_data[] = array('href' => $this->browser->get_change_assessment_publication_visibility_url($assessment_publication), 'label' => Translation :: get('Hide'), 'img' => Theme :: get_common_image_path() . 'action_visible.png');
             }
-            
+
             $toolbar_data[] = array('href' => $this->browser->get_export_qti_url($assessment_publication), 'label' => Translation :: get('Export'), 'img' => Theme :: get_common_image_path() . 'action_export.png');
             $toolbar_data[] = array('href' => $this->browser->get_move_assessment_publication_url($assessment_publication), 'label' => Translation :: get('Move'), 'img' => Theme :: get_common_image_path() . 'action_move.png');
-            
+
             if ($assessment->get_assessment_type() == Survey :: TYPE_SURVEY)
             {
                 $toolbar_data[] = array('href' => $this->browser->get_publish_survey_url($assessment_publication), 'label' => Translation :: get('InviteUsers'), 'img' => Theme :: get_common_image_path() . 'action_invite_users.png');
             }
-            
+
             if ($assessment->is_complex_content_object())
             {
-                $toolbar_data[] = array('href' => $this->browser->get_build_assessment_url($assessment_publication), 'img' => Theme :: get_common_image_path() . 'action_bar.png', 'label' => Translation :: get('BuildComplex'));
+                $toolbar_data[] = array('href' => $this->browser->get_build_assessment_url($assessment_publication), 'img' => Theme :: get_common_image_path() . 'action_build.png', 'label' => Translation :: get('BuildComplex'));
             }
         }
-        
+
         return Utilities :: build_toolbar($toolbar_data);
     }
 }
--- repository/lib/repository_manager/component/viewer.class.php
+++ repository/lib/repository_manager/component/viewer.class.php
@@ -26,32 +26,32 @@ class RepositoryManagerViewerComponent extends RepositoryManagerComponent
             {
                 $this->not_allowed();
             }
-            
+
             $display = ContentObjectDisplay :: factory($object);
             $trail = new BreadcrumbTrail(false);
             $trail->add_help('repository general');
-            
+
             if ($object->get_state() == ContentObject :: STATE_RECYCLED)
             {
                 $trail->add(new Breadcrumb($this->get_recycle_bin_url(), Translation :: get('RecycleBin')));
                 $this->force_menu_url($this->get_recycle_bin_url());
             }
             $trail->add(new Breadcrumb($this->get_url(), $object->get_title() . ($object->is_latest_version() ? '' : ' (' . Translation :: get('OldVersion') . ')')));
-            
+
             $version_data = array();
             $versions = $object->get_content_object_versions();
-            
+
             $publication_attr = array();
-            
+
             foreach ($object->get_content_object_versions() as $version)
             {
                 // If this learning object is published somewhere in an application, these locations are listed here.
                 $publications = $this->get_content_object_publication_attributes($this->get_user(), $version->get_id());
                 $publication_attr = array_merge($publication_attr, $publications);
             }
-            
+
             $this->action_bar = $this->get_action_bar($object);
-            
+
             if (count($versions) >= 2)
             {
                 Utilities :: order_content_objects_by_id_desc($versions);
@@ -70,22 +70,22 @@ class RepositoryManagerViewerComponent extends RepositoryManagerComponent
                     $version_entry['date'] = date('d M y, H:i', $version->get_creation_date());
                     $version_entry['comment'] = $version->get_comment();
                     $version_entry['viewing_link'] = $this->get_content_object_viewing_url($version);
-                    
+
                     $delete_url = $this->get_content_object_deletion_url($version, 'version');
                     if (isset($delete_url))
                     {
                         $version_entry['delete_link'] = $delete_url;
                     }
-                    
+
                     $revert_url = $this->get_content_object_revert_url($version, 'version');
                     if (isset($revert_url))
                     {
                         $version_entry['revert_link'] = $revert_url;
                     }
-                    
+
                     $version_data[] = $display->get_version_as_html($version_entry);
                 }
-                
+
                 $form = ContentObjectForm :: factory(ContentObjectForm :: TYPE_COMPARE, $object, 'compare', 'post', $this->get_url(array(RepositoryManager :: PARAM_CONTENT_OBJECT_ID => $object->get_id())), array('version_data' => $version_data));
                 if ($form->validate())
                 {
@@ -96,10 +96,10 @@ class RepositoryManagerViewerComponent extends RepositoryManagerComponent
                 else
                 {
                     $this->display_header($trail, false, true);
-                    
+
                     if ($this->action_bar)
                         echo '<br />' . $this->action_bar->as_html();
-                    
+
                     echo $display->get_full_html();
                     echo Utilities :: add_block_hider();
                     echo Utilities :: build_block_hider('content_object_extras');
@@ -110,10 +110,10 @@ class RepositoryManagerViewerComponent extends RepositoryManagerComponent
             elseif (count($publication_attr) > 0)
             {
                 $this->display_header($trail, false, true);
-                
+
                 if ($this->action_bar)
                     echo '<br />' . $this->action_bar->as_html();
-                
+
                 echo $display->get_full_html();
                 /*echo Utilities :: add_block_hider();
                 echo Utilities :: build_block_hider('content_object_extras');*/
@@ -121,26 +121,26 @@ class RepositoryManagerViewerComponent extends RepositoryManagerComponent
             else
             {
                 $this->display_header($trail, false, true);
-                
+
                 if ($this->action_bar)
                     echo '<br />' . $this->action_bar->as_html();
-                
+
                 echo $display->get_full_html();
             }
-            
+
             /*if (count($publication_attr) > 0)
             {
                 echo $display->get_publications_as_html($publication_attr);
                 //echo Utilities :: build_uses($publication_attr);
             }
-            
+
             if (count($versions) >= 2 || count($publication_attr) > 0)
             {
                 echo Utilities :: build_block_hider();
             }*/
-            
+
             echo $this->display_links_to_content_object($object);
-            
+
             $this->display_footer();
         }
         else
@@ -154,7 +154,7 @@ class RepositoryManagerViewerComponent extends RepositoryManagerComponent
         if ($object->get_owner_id() == $this->get_user_id())
         {
             $action_bar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
-            
+
             $edit_url = $this->get_content_object_editing_url($object);
             if (isset($edit_url))
             {
@@ -177,16 +177,16 @@ class RepositoryManagerViewerComponent extends RepositoryManagerComponent
                         $recycle_bin_button = new ToolbarItem(Translation :: get('Remove'), Theme :: get_common_image_path() . 'action_recycle_bin_na.png');
                     }
                 }
-                
+
                 if (! $in_recycle_bin)
                 {
                     $delete_link_url = $this->get_content_object_unlinker_url($object);
-                    
+
                     if (! isset($recycle_url))
                     {
                         $force_delete_button = new ToolbarItem(Translation :: get('Unlink'), Theme :: get_common_image_path() . 'action_unlink.png', $delete_link_url, ToolbarItem :: DISPLAY_ICON_AND_LABEL, true);
                     }
-                    
+
                     $edit_url = $this->get_content_object_editing_url($object);
                     if (isset($edit_url))
                     {
@@ -196,30 +196,30 @@ class RepositoryManagerViewerComponent extends RepositoryManagerComponent
                     {
                         $action_bar->add_common_action(new ToolbarItem(Translation :: get('EditNA'), Theme :: get_common_image_path() . 'action_edit_na.png'));
                     }
-                    
+
                     if (isset($recycle_bin_button))
                     {
                         $action_bar->add_common_action($recycle_bin_button);
                     }
-                    
+
                     if (isset($force_delete_button))
                     {
                         $action_bar->add_common_action($force_delete_button);
                     }
-                    
+
                     $dm = RepositoryDataManager :: get_instance();
                     if ($dm->get_number_of_categories($this->get_user_id()) > 1)
                     {
                         $move_url = $this->get_content_object_moving_url($object);
                         $action_bar->add_common_action(new ToolbarItem(Translation :: get('Move'), Theme :: get_common_image_path() . 'action_move.png', $move_url, ToolbarItem :: DISPLAY_ICON_AND_LABEL));
                     }
-                    
+
                     $metadata_url = $this->get_content_object_metadata_editing_url($object);
                     $action_bar->add_common_action(new ToolbarItem(Translation :: get('Metadata'), Theme :: get_common_image_path() . 'action_metadata.png', $metadata_url, ToolbarItem :: DISPLAY_ICON_AND_LABEL));
-                    
+
                     $rights_url = $this->get_content_object_rights_editing_url($object);
                     $action_bar->add_common_action(new ToolbarItem(Translation :: get('Rights'), Theme :: get_common_image_path() . 'action_rights.png', $rights_url, ToolbarItem :: DISPLAY_ICON_AND_LABEL));
-                    
+
                     if ($object->is_complex_content_object())
                     {
                         $clo_url = $this->get_browse_complex_content_object_url($object);
@@ -236,68 +236,68 @@ class RepositoryManagerViewerComponent extends RepositoryManagerComponent
                     }
                 }
             }
-            
-            $action_bar->add_tool_action(new ToolbarItem(Translation :: get('CopyToTemplates'), Theme :: get_common_image_path() . 'export_unknown.png', $this->get_copy_content_object_url($object->get_id(), 0)));
-            
+
+            $action_bar->add_tool_action(new ToolbarItem(Translation :: get('CopyToTemplates'), Theme :: get_common_image_path() . 'export_template.png', $this->get_copy_content_object_url($object->get_id(), 0)));
+
             return $action_bar;
         }
-    
+
     }
-    
+
 	function display_links_to_content_object($content_object)
 	{
 		$html = array();
-		
+
 		$html[] = Utilities :: add_block_hider();
 		$html[] = Utilities :: build_block_hider('links', 'Links');
-		
+
 		$html[] = '<br />';
 		$html[] = '<h3>' . Translation :: get('Links') . '</h3>';
 		$html[] = '<h4>' . Translation :: get('Publications') . '</h4>';
-		
-		$browser = new LinkBrowserTable($this, array(RepositoryManager :: PARAM_APPLICATION => RepositoryManager :: APPLICATION_NAME, 
+
+		$browser = new LinkBrowserTable($this, array(RepositoryManager :: PARAM_APPLICATION => RepositoryManager :: APPLICATION_NAME,
 													 RepositoryManager :: PARAM_CONTENT_OBJECT_ID => $this->object->get_id(),
 													 RepositoryManager :: PARAM_ACTION => RepositoryManager :: ACTION_VIEW_CONTENT_OBJECTS),
 										null, LinkBrowserTable :: TYPE_PUBLICATIONS);
 		$html[] = $browser->as_html();
-		
+
 		$html[] = '<h4>' . Translation :: get('Parents') . '</h4>';
-		
-		$browser = new LinkBrowserTable($this, array(RepositoryManager :: PARAM_APPLICATION => RepositoryManager :: APPLICATION_NAME, 
+
+		$browser = new LinkBrowserTable($this, array(RepositoryManager :: PARAM_APPLICATION => RepositoryManager :: APPLICATION_NAME,
 													 RepositoryManager :: PARAM_CONTENT_OBJECT_ID => $this->object->get_id(),
 													 RepositoryManager :: PARAM_ACTION => RepositoryManager :: ACTION_VIEW_CONTENT_OBJECTS),
 										null, LinkBrowserTable :: TYPE_PARENTS);
 		$html[] = $browser->as_html();
-		
+
 		$html[] = '<h4>' . Translation :: get('Children') . '</h4>';
-		
-		$browser = new LinkBrowserTable($this, array(RepositoryManager :: PARAM_APPLICATION => RepositoryManager :: APPLICATION_NAME, 
+
+		$browser = new LinkBrowserTable($this, array(RepositoryManager :: PARAM_APPLICATION => RepositoryManager :: APPLICATION_NAME,
 													 RepositoryManager :: PARAM_CONTENT_OBJECT_ID => $this->object->get_id(),
 													 RepositoryManager :: PARAM_ACTION => RepositoryManager :: ACTION_VIEW_CONTENT_OBJECTS),
 										null, LinkBrowserTable :: TYPE_CHILDREN);
 		$html[] = $browser->as_html();
-		
+
 		$html[] = '<h4>' . Translation :: get('AttachedTo') . '</h4>';
-		
-		$browser = new LinkBrowserTable($this, array(RepositoryManager :: PARAM_APPLICATION => RepositoryManager :: APPLICATION_NAME, 
+
+		$browser = new LinkBrowserTable($this, array(RepositoryManager :: PARAM_APPLICATION => RepositoryManager :: APPLICATION_NAME,
 													 RepositoryManager :: PARAM_CONTENT_OBJECT_ID => $this->object->get_id(),
 													 RepositoryManager :: PARAM_ACTION => RepositoryManager :: ACTION_VIEW_CONTENT_OBJECTS),
 										null, LinkBrowserTable :: TYPE_ATTACHMENTS);
 		$html[] = $browser->as_html();
-		
+
 		$html[] = '<h4>' . Translation :: get('IncludedIn') . '</h4>';
-		
-		$browser = new LinkBrowserTable($this, array(RepositoryManager :: PARAM_APPLICATION => RepositoryManager :: APPLICATION_NAME, 
+
+		$browser = new LinkBrowserTable($this, array(RepositoryManager :: PARAM_APPLICATION => RepositoryManager :: APPLICATION_NAME,
 													 RepositoryManager :: PARAM_CONTENT_OBJECT_ID => $this->object->get_id(),
 													 RepositoryManager :: PARAM_ACTION => RepositoryManager :: ACTION_VIEW_CONTENT_OBJECTS),
 										null, LinkBrowserTable :: TYPE_INCLUDES);
 		$html[] = $browser->as_html();
-		
+
 		$html[] = '</div></div><div class="clear"></div><br />';
-		
+
 		return implode("\n", $html);
 	}
-	
+
 	function get_object()
 	{
 		return $this->object;
