b32800e6beebeb882a6fcad05158f8095083929e
scaramanga <devnull@localhost>
2010-01-19 11:55:44 +0100

    New Chamilo logo + several small layout fixes / tryouts.

--- repository/lib/complex_builder/learning_path/component/prerequisites_builder/prerequisites_builder_form.class.php
+++ repository/lib/complex_builder/learning_path/component/prerequisites_builder/prerequisites_builder_form.class.php
@@ -16,12 +16,12 @@ class PrerequisitesBuilderForm extends FormValidator
     function PrerequisitesBuilderForm($user, $clo_item, $action)
     {
         parent :: __construct('prerequisites', 'post', $action);
-        
+
         $this->user = $user;
         $this->clo_item = $clo_item;
-        
+
         $this->setDefaults();
-        
+
         $this->handle_session_values();
         $this->build_basic_form();
     }
@@ -29,7 +29,7 @@ class PrerequisitesBuilderForm extends FormValidator
     function build_basic_form()
     {
         $this->build_list();
-        $buttons[] = $this->createElement('style_submit_button', 'submit', Translation :: get('BuildPrerequisites'), array('class' => 'positive'));
+        $buttons[] = $this->createElement('style_submit_button', 'submit', Translation :: get('SavePrerequisites'), array('class' => 'positive save'));
         $this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
     }
 
@@ -42,7 +42,7 @@ class PrerequisitesBuilderForm extends FormValidator
             unset($_SESSION['skip_items']);
             unset($_SESSION['skip_groups']);
         }
-        
+
         if (! isset($_SESSION['number_of_groups']))
         {
             $_SESSION['number_of_groups'] = 1;
@@ -53,17 +53,17 @@ class PrerequisitesBuilderForm extends FormValidator
             $_SESSION['skip_groups'] = array();
             $_SESSION['skip_items'][0] = array();
         }
-    
+
         if ($this->number_of_groups)
         {
             $_SESSION['number_of_groups'] = $this->number_of_groups;
         }
-        
+
         if ($this->number_of_items)
         {
             $_SESSION['number_of_items'] = $this->number_of_items;
         }
-        
+
         if (isset($_POST['add_group']))
         {
             $_SESSION['number_of_groups'] = $_SESSION['number_of_groups'] + 1;
@@ -77,7 +77,7 @@ class PrerequisitesBuilderForm extends FormValidator
             $_SESSION['skip_groups'][] = $indexes[0];
             unset($_SESSION['number_of_items'][$indexes[0]]);
         }
-        
+
         if (isset($_POST['add_item']))
         {
             $indexes = array_keys($_POST['add_item']);
@@ -96,35 +96,59 @@ class PrerequisitesBuilderForm extends FormValidator
     function build_list()
     {
         $renderer = &$this->defaultRenderer();
-        $renderer->setElementTemplate('<div style="margin: 10px 0px 10px 0px;">{element}<div class="clear"></div></div>', 'option_buttons');
-        $renderer->setGroupElementTemplate('<div style="float:left; text-align: center; margin-right: 10px;">{element}</div>', 'option_buttons');
-        
+        $renderer->setElementTemplate('<div style="margin: 10px 0px;">{element}<div class="clear"></div></div>', 'option_buttons');
+        $renderer->setGroupElementTemplate('<div style="text-align: center; margin-right: 10px;">{element}</div>', 'option_buttons');
+
         $gbuttons = array();
-        $gbuttons[] = $this->createElement('style_button', 'add_group[]', Translation :: get('AddGroup'), array('class' => 'normal add', 'id' => 'add_group'));
+        $gbuttons[] = $this->createElement('style_button', 'add_group[]', Translation :: get('AddPrerequisiteGroup'), array('class' => 'normal add', 'id' => 'add_group'));
         $this->addGroup($gbuttons, 'option_buttons', null, '', false);
-        
+
         $operator = array('' => Translation :: get('Operator'), '&' => Translation :: get('And'), '|' => Translation :: get('Or'));
-        $goperator = array('' => Translation :: get('GroupOperator'), '&' => Translation :: get('And'), '|' => Translation :: get('Or'));
-        
+        $goperator = array('&' => Translation :: get('And'), '|' => Translation :: get('Or'));
+        //$goperator = array('' => Translation :: get('GroupOperator'), '&' => Translation :: get('And'), '|' => Translation :: get('Or'));
+
         $not = array('' => '', '~' => Translation :: get('Not'));
         $sibblings = $this->retrieve_sibblings();
-        
+
         $number_of_groups = intval($_SESSION['number_of_groups']);
         $gcounter = 0;
         for($group_number = 0; $group_number < $number_of_groups; $group_number ++)
         {
             if (! in_array($group_number, $_SESSION['skip_groups']))
             {
-                if ($gcounter > 0)
+                $category_html = array();
+                $category_html[] = '<div class="prerequisite_group">';
+                $category_html[] = '<div class="header">';
+                $category_html[] = '<div class="operator">';
+                $this->addElement('html', implode("\n", $category_html));
+
+                if ($gcounter > 0 && $group_number != 0)
                 {
-                    $this->addElement('html', '<br />');
+                    $renderer->setElementTemplate('{element}', 'group_operator[' . $group_number . ']');
                     $this->addElement('select', 'group_operator[' . $group_number . ']', '', $goperator);
                 }
-                
-                $this->addElement('category', Translation :: get('Group') . ' ' . ($gcounter + 1));
-                
+
+                $category_html = array();
+                $category_html[] = '</div>';
+                $category_html[] = '<div class="title">' . Translation :: get('PrerequisiteGroup') . ' ' . ($gcounter + 1) . '</div>';
+                $category_html[] = '<div class="actions">';
+                $this->addElement('html', implode("\n", $category_html));
+
+                if ($_SESSION['number_of_groups'] - count($_SESSION['skip_groups']) > 1)
+                {
+                    $renderer->setElementTemplate('{element}', 'remove_group[' . $group_number . ']');
+                    $group[] = $this->addElement('image', 'remove_group[' . $group_number . ']', Theme :: get_common_image_path() . 'action_delete.png', array('title' => Translation :: get('RemoveGroup'), 'class' => 'remove_group', 'id' => $group_number));
+                }
+
+                $category_html = array();
+                $category_html[] = '</div>';
+                $category_html[] = '</div>';
+                $category_html[] = '<div class="body">';
+                $this->addElement('html', implode("\n", $category_html));
+                //$this->addElement('category', Translation :: get('PrerequisiteGroup') . ' ' . ($gcounter + 1));
+
                 $number_of_items = intval($_SESSION['number_of_items'][$group_number]);
-                
+
                 $counter = 0;
                 for($item_number = 0; $item_number < $number_of_items; $item_number ++)
                 {
@@ -132,7 +156,7 @@ class PrerequisitesBuilderForm extends FormValidator
                     {
                         $identifier = '[' . $group_number . '][' . $item_number . ']';
                         $group = array();
-                        
+
                         if ($counter > 0)
                         {
                             $group[] = $this->createElement('select', 'operator' . $identifier, '', $operator);
@@ -142,39 +166,48 @@ class PrerequisitesBuilderForm extends FormValidator
                             $element = $this->createElement('select', 'operator' . $identifier, '', $operator, array('disabled'));
                             $group[] = $element;
                         }
-                        
+
                         $group[] = $this->createElement('select', 'not' . $identifier, '', $not);
                         $group[] = $this->createElement('select', 'prerequisite' . $identifier, '', $sibblings);
-                        
+
                         if ($_SESSION['number_of_items'][$group_number] - count($_SESSION['skip_items'][$group_number]) > 1)
                             $group[] = & $this->createElement('image', 'remove_item[' . $group_number . '][' . $item_number . ']', Theme :: get_common_image_path() . 'action_delete.png', array('title' => Translation :: get('RemoveItem'), 'class' => 'remove_item', 'id' => $group_number . '_' . $item_number));
-                        
+
                         $this->addGroup($group, 'item_' . $group_number . '_' . $item_number, null, '', false);
                         $renderer->setGroupElementTemplate('{element} &nbsp; ', 'item_' . $group_number . '_' . $item_number);
-                        
+
                         $counter ++;
                     }
                 }
-                
+
                 $gcounter ++;
-                
-                $group = array();
+
+//                $group = array();
                 //$group[] = &$this->createElement('image', 'create_item[' . $group_number . ']', Theme :: get_common_image_path() . 'action_add.png', array('title' => Translation :: get('AddItem'), 'class' => 'create_item', 'id' => $group_number));
                 $this->addElement('image', 'add_item[' . $group_number . ']', Theme :: get_common_image_path() . 'action_add.png', array('title' => Translation :: get('AddItem'), 'class' => 'add_item', 'id' => $group_number));
-                
-                if ($_SESSION['number_of_groups'] - count($_SESSION['skip_groups']) > 1)
-                    $group[] = &$this->createElement('image', 'remove_group[' . $group_number . ']', Theme :: get_common_image_path() . 'action_delete.png', array('title' => Translation :: get('RemoveGroup'), 'class' => 'remove_group', 'id' => $group_number));
-                
-                $this->addGroup($group, 'group_' . $group_number, null, '', false);
-                $renderer->setElementTemplate('<div style="float: right; margin: 10px 0px 10px 0px;">{element}<div class="clear"></div></div>', 'group_' . $group_number);
-                
-                $this->addElement('category');
+
+//                if ($_SESSION['number_of_groups'] - count($_SESSION['skip_groups']) > 1)
+//                {
+//                    $group[] = &$this->createElement('image', 'remove_group[' . $group_number . ']', Theme :: get_common_image_path() . 'action_delete.png', array('title' => Translation :: get('RemoveGroup'), 'class' => 'remove_group', 'id' => $group_number));
+//                }
+
+//                $this->addGroup($group, 'group_' . $group_number, null, '', false);
+//                $renderer->setElementTemplate('<div style="float: right; margin: 10px 0px 10px 0px;">{element}<div class="clear"></div></div>', 'group_' . $group_number);
+
+                $category_html = array();
+                $category_html[] = '<div style="clear: both;"></div>';
+                $category_html[] = '</div>';
+                $category_html[] = '<div style="clear: both;"></div>';
+                $category_html[] = '</div>';
+                $this->addElement('html', implode("\n", $category_html));
             }
         }
-        
+
+        $gbuttons = array();
+        $gbuttons[] = $this->createElement('style_button', 'add_group[]', Translation :: get('AddPrerequisiteGroup'), array('class' => 'normal add', 'id' => 'add_group'));
         $this->addGroup($gbuttons, 'option_buttons', null, '', false);
     }
-    
+
     private $sibblings;
 
     function retrieve_sibblings()
@@ -183,19 +216,19 @@ class PrerequisitesBuilderForm extends FormValidator
         $conditions[] = new EqualityCondition(ComplexContentObjectItem :: PROPERTY_PARENT, $this->clo_item->get_parent(), ComplexContentObjectItem :: get_table_name());
         $conditions[] = new NotCondition(new EqualityCondition(ComplexContentObjectItem :: PROPERTY_ID, $this->clo_item->get_id(), ComplexContentObjectItem :: get_table_name()));
         $condition = new AndCondition($conditions);
-        
+
         $rdm = RepositoryDataManager :: get_instance();
-        
+
         $sibblings_list = $rdm->retrieve_complex_content_object_items($condition);
         while ($sibbling = $sibblings_list->next_result())
         {
             $lo = $rdm->retrieve_content_object($sibbling->get_ref());
             if ($lo->get_type() == 'learning_path_item')
                 $lo = $rdm->retrieve_content_object($lo->get_reference());
-            
+
             $sibblings[$sibbling->get_id()] = $lo->get_title();
         }
-        
+
         return $sibblings;
     }
 
@@ -205,10 +238,10 @@ class PrerequisitesBuilderForm extends FormValidator
         {
             return parent :: validate();
         }
-        
+
         return false;
     }
-    
+
     private $number_of_groups;
     private $number_of_items;
 
@@ -221,15 +254,15 @@ class PrerequisitesBuilderForm extends FormValidator
             $matches = array();
             preg_match_all($pattern, $prerequisites, $matches);
             $groups = $matches[0];
-            
+
             foreach ($groups as $i => $group)
             {
                 $prerequisites = str_replace($group, '_', $prerequisites);
                 $group = str_replace('(', '', $group);
                 $group = str_replace(')', '', $group);
-                
+
                 $or_values = explode('|', $group);
-                
+
                 $item_counter = 0;
                 foreach ($or_values as $or_value)
                 {
@@ -240,15 +273,15 @@ class PrerequisitesBuilderForm extends FormValidator
                             $or_value = substr($or_value, 1);
                             $defaults['not'][$i][$item_counter] = '~';
                         }
-                        
+
                         $defaults['prerequisite'][$i][$item_counter] = $or_value;
                         if ($item_counter > 0)
                             $defaults['operator'][$i][$item_counter] = '|';
-                        
+
                         $item_counter ++;
                         continue;
                     }
-                    
+
                     $and_values = explode('&', $or_value);
                     foreach ($and_values as $and_value)
                     {
@@ -257,25 +290,25 @@ class PrerequisitesBuilderForm extends FormValidator
                             $and_value = substr($and_value, 1);
                             $defaults['not'][$i][$item_counter] = '~';
                         }
-                        
+
                         $defaults['prerequisite'][$i][$item_counter] = $and_value;
-                        
+
                         if ($item_counter > 0)
                             $defaults['operator'][$i][$item_counter] = '&';
                         $item_counter ++;
                     }
                 }
-                
+
                 $this->number_of_items[$i] = $item_counter;
-            
+
             }
-            
+
             $this->number_of_groups = count($groups);
-            
+
             $operators = explode('_', $prerequisites);
-            
+
             $defaults['group_operator'] = $operators;
-            
+
             if($this->number_of_groups == 0 && is_numeric($prerequisites))
             {
             	$this->number_of_groups = 1;
@@ -283,7 +316,7 @@ class PrerequisitesBuilderForm extends FormValidator
             	$defaults['prerequisite'][0][0] = $prerequisites;
             }
         }
-        
+
         parent :: setDefaults($defaults);
     }
 
@@ -291,19 +324,19 @@ class PrerequisitesBuilderForm extends FormValidator
     {
         $values = $this->exportValues();
         $prereq_formula = '';
-        
+
         foreach ($values['prerequisite'] as $group_number => $items)
         {
             $prereq_formula .= $values['group_operator'][$group_number] . '(';
-            
+
             foreach ($items as $item_number => $item)
             {
                 $prereq_formula .= $values['operator'][$group_number][$item_number] . $values['not'][$group_number][$item_number] . $item;
             }
-            
+
             $prereq_formula .= ')';
         }
-        
+
         $this->clo_item->set_prerequisites($prereq_formula);
         return $this->clo_item->update();
     }
