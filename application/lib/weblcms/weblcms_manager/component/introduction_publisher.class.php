98275e6c9a70a9c42eee4d12759a11a2e9bc7fda
Kariboe <devnull@localhost>
2010-03-04 15:00:20 +0100

    Removed 'Browse' option+icon from the Introduction text publisher

--- application/lib/weblcms/weblcms_manager/component/introduction_publisher.class.php
+++ application/lib/weblcms/weblcms_manager/component/introduction_publisher.class.php
@@ -51,7 +51,6 @@ class WeblcmsManagerIntroductionPublisherComponent extends WeblcmsManagerCompone
         
         if (! isset($object))
         {
-            $html[] = '<div class="clear">&nbsp;</div><p><a href="' . $this->get_url(array('go' => 'course_viewer'), array(), true) . '"><img src="' . Theme :: get_common_image_path() . 'action_browser.png" alt="' . Translation :: get('BrowserTitle') . '" style="vertical-align:middle;"/> ' . Translation :: get('BrowserTitle') . '</a></p>';
             $html[] = $pub->as_html();
         }
         else
