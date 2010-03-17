babf09cbbe109eac5a9752d3efe841cef3df71a6
Yannick <devnull@localhost>
2010-03-17 16:57:08 +0100

    fixed crouse_type browser

--- application/lib/weblcms/weblcms_manager/component/admin_course_type_browser.class.php
+++ application/lib/weblcms/weblcms_manager/component/admin_course_type_browser.class.php
@@ -21,7 +21,6 @@ class WeblcmsManagerAdminCourseTypeBrowserComponent extends WeblcmsManagerCompon
         Header :: set_section('admin');
         
         $trail = new BreadcrumbTrail();
-        //$trail->add(new Breadcrumb($this->get_url(array(Application :: PARAM_APPLICATION => 'weblcms')), Translation :: get('MyCourseTypes')));
         if ($this->get_user()->is_platform_admin())
         {
             $trail->add(new Breadcrumb(Redirect :: get_link(AdminManager :: APPLICATION_NAME, array(AdminManager :: PARAM_ACTION => AdminManager :: ACTION_ADMIN_BROWSER), array(), false, Redirect :: TYPE_CORE), Translation :: get('Administration')));
@@ -32,27 +31,38 @@ class WeblcmsManagerAdminCourseTypeBrowserComponent extends WeblcmsManagerCompon
         $trail->add(new Breadcrumb($this->get_url(), Translation :: get('CourseTypeList')));
         $trail->add_help('coursetype general');
        
+        
         if (! $this->get_user()->is_platform_admin())
         {
             $this->display_header($trail, false, true);
-            echo '<div class="clear"></div><br />';
             Display :: error_message(Translation :: get("NotAllowed"));
             $this->display_footer();
             exit();
         }
         
-        $this->action_bar = $this->get_action_bar();
-        $menu = $this->get_menu_html();
-        $output = $this->get_course_type_html();
-        
         $this->display_header($trail, false, true);
-        echo '<div class="clear"></div>';
-        echo '<br />' . $this->action_bar->as_html() . '<br />';
-        echo $menu;
-        echo $output;
+        $this->action_bar = $this->get_action_bar();
+        echo $this->get_course_type_html();
         $this->display_footer();
     }
 
+    function get_course_type_html()
+    {
+        //$table = new AdminCourseTypeBrowserTable($this, null, $this->get_condition());      
+        $html = array();
+        
+        $html[] = '<div style="clear: both;"></div>';
+        $html[] = $this->get_course_type_admin_menu($this);
+        $html[] = '<div id="tool_browser_right">';
+        $html[] = '<div>';
+        $html[] = $this->action_bar->as_html() . '<br />';
+		$html[] = $this->get_table_html();
+        $html[] = '<div style="clear: both;"></div>';
+        $html[] = '</div>';
+        $html[] = '</div>';
+        return implode($html, "\n");
+    }
+    
 	function get_action_bar()
 	{
 		$action_bar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
@@ -60,48 +70,25 @@ class WeblcmsManagerAdminCourseTypeBrowserComponent extends WeblcmsManagerCompon
 		$action_bar->add_common_action(new ToolbarItem(Translation :: get('Add'), Theme :: get_common_image_path().'action_add.png', $this->get_url(array(Application :: PARAM_ACTION => WeblcmsManager :: ACTION_ADMIN_COURSE_TYPE_CREATOR)), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
 		$action_bar->add_common_action(new ToolbarItem(Translation :: get('ShowAll'), Theme :: get_common_image_path() . 'action_browser.png', $this->get_url(), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
 
+		$action_bar->set_search_url($this->get_url());
+		
 		return $action_bar;
 	}
 
-    function get_course_type_html()
-    {
-        $table = new AdminCourseTypeBrowserTable($this, null, $this->get_condition());
-        
-        $html = array();
-        $html[] = '<div style="float: right; width: 100%;">';
-        $html[] = $table->as_html();
-        $html[] = '</div>';
-        
-        return implode($html, "\n");
-    }
-	
-    function get_menu_html()
-    {
-        $extra_items = array();
-        if ($this->get_search_validate())
-        {
-            // $search_url = $this->get_url();
-            $search_url = '#';
-            $search = array();
-            $search['title'] = Translation :: get('SearchResults');
-            $search['url'] = $search_url;
-            $search['class'] = 'search_results';
-            $extra_items[] = $search;
-        }
-        else
-        {
-            $search_url = null;
-        }
-        
-        $url_format = $this->get_url(array(Application :: PARAM_ACTION => WeblcmsManager :: ACTION_ADMIN_COURSE_TYPE_BROWSER, WeblcmsManager));
-        $url_format = str_replace($temp_replacement, '%s', $url_format);
-        
-        $html = array();
-        $html[] = '<div style="float: left; width: 20%;">';
-        $html[] = '</div>';
-        
-        return implode($html, "\n");
-    }
+
+    
+	function get_table_html()
+	{
+		$parameters = $this->get_parameters();
+		$parameters[WeblcmsManager :: PARAM_ACTION]=  WeblcmsManager :: ACTION_ADMIN_COURSE_TYPE_BROWSER;
+
+		$table = new AdminCourseTypeBrowserTable($this, $parameters, $this->get_condition());
+
+		$html = array();
+		$html[] = $table->as_html();
+
+		return implode($html, "\n");
+	}
     
     function get_condition()
     {
@@ -123,5 +110,38 @@ class WeblcmsManagerAdminCourseTypeBrowserComponent extends WeblcmsManagerCompon
       	}     
         return $condition;
     }
+    
+    function get_course_type_admin_menu($browser)
+	{
+		$html = array();
+		
+		$html[] = '<div id="tool_bar" class="tool_bar tool_bar_right">';
+		
+		$html[] = '<div id="tool_bar_hide_container" class="hide">';
+		$html[] = '<a id="tool_bar_hide" href="#"><img src="'. Theme :: get_common_image_path() .'action_action_bar_right_hide.png" /></a>';
+		$html[] = '<a id="tool_bar_show" href="#"><img src="'. Theme :: get_common_image_path() .'action_action_bar_right_show.png" /></a>';
+		$html[] = '</div>';
+		
+		$html[] = '<div class="tool_menu">';
+		$html[] = '<ul>';
+		
+		// Browse Links
+		$html[] = '<li class="tool_list_menu title" style="font-weight: bold">' . Translation :: get('CourseTypes') . '</li><br />';
+		$html[] = '<li class="tool_list_menu" style="background-image: url(' . Theme :: get_common_image_path() . 'action_browser.png)"><a style="top: -3px; position: relative;" href="'.$browser->get_url(array(Application :: PARAM_ACTION => WeblcmsManager :: ACTION_ADMIN_COURSE_TYPE_BROWSER)) .'">'.Translation :: get('BrowseCourseType').'</a></li>';
+
+//		$html[] = '<div style="margin: 10px 0 10px 0; border-bottom: 1px dotted #4271B5; height: 0px;"></div>';
+//		
+//		// Tool Links
+//		$html[] = '<li class="tool_list_menu" style="font-weight: bold">' . Translation :: get('GradebookTools') . '</li><br />';
+		
+		$html[] = '</ul>';
+		$html[] = '</div>';
+		
+		$html[] = '</div>';
+		$html[] = '<script type="text/javascript" src="'. Path :: get(WEB_LIB_PATH) . 'javascript/tool_bar.js' .'"></script>';
+		$html[] = '<div class="clear"></div>';
+		
+		return implode("\n", $html);
+	}
 }
 ?>
