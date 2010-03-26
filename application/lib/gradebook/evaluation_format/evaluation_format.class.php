16034d3406ab23634baf203c121873c0a858e003
Johan Janssens <devnull@localhost>
2010-03-26 08:45:24 +0100

    small additions to gradebook

--- application/lib/gradebook/evaluation_format/evaluation_format.class.php
+++ application/lib/gradebook/evaluation_format/evaluation_format.class.php
@@ -1,13 +1,17 @@
 <?php
 abstract class EvaluationFormat
 {
-	const PROPERTY_NAME = $name;
-	
-	abstract function add_to_form($form);
-	
-	function set_name()
-	{
 		
+	abstract function add_to_form($form);
+
+	
+	function factory()
+	{
+		require_once dirname(__FILE__) . '/' . strtolower($type).  '/'. strtolower($type) . 'evaluation_format.class.php';
+        $class = $type . 'ReportingFormat';
+        return new $class($reporting_block);
 	}
+	
+
 }
 ?>
