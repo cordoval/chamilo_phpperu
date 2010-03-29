c1b7446a205273ede40dd4e903978bf5f06f8a54
Johan Janssens <devnull@localhost>
2010-03-29 16:06:26 +0200

    Method for evaluation check in publication forms added

--- application/lib/gradebook/forms/evaluation_form.class.php
+++ application/lib/gradebook/forms/evaluation_form.class.php
@@ -1,33 +1,40 @@
 <?php
-
+require_once dirname(__FILE__) . '/../gradebook_data_manager.class.php';
 class EvaluationForm extends FormValidator
-{/*
-	
-	private $auto_generate_evaluation_applications = array('assessment',...);
-	
-	
-	static function build_evaluation_question($form)
-	{
-		if(array_key_exists($publication, $auto_generate_evaluation_applications))
-		{
-		$choices = array();
-        $choices[] = $this->createElement('checkbox', self :: EVALUATION, '', Translation :: get('Evaluation'));
-        $form->addGroup($choices, null, Translation :: get('IsEvaluation'), '<br />', false);
-        $form->addElement('html', '<div style="margin-left: 25px; display: block;" id="' . self :: EVALUATION . '_window">');
-        $form->addElement('html', '</div>');
 {
+	private $auto_generate_evaluation_applications = array("assessment", "weblcms");
+	
+	const PROPERTY_FORMAT_LIST = 'format_list';
+	
+	static function build_evaluation_question($form, $choose_format = false)
+	{
+		$form->addElement('category', Translation :: get('EvaluationProperties'));
+		
+		if($choose_format)
+		{
+			
 		}
 		else
 		{
-		$choices = array();
-        $choices[] = $this->createElement('checkbox', self :: EVALUATION, '', Translation :: get('Evaluation'));
-        $form->addGroup($choices, null, Translation :: get('IsEvaluation'), '<br />', false);
-        $form->addElement('html', '<div style="margin-left: 25px; display: block;" id="' . self :: EVALUATION . '_window">');
-        $form->addElement('select', Translation :: get('FormatType') ,Translation :: get('FormatType') . ':', DatabaseGradebookDatamanager :: get_instance()->retrieve_all_active_evaluation_formats());
-        $form->addElement('html', '</div>');
+			
+			$formats = GradebookDatamanager :: get_instance()->retrieve_all_active_evaluation_formats();
+			while($format = $formats->next_result())
+			{
+				$formats_array[$format->get_id()] = $format->get_title();
+			}
+       			$form->addElement('checkbox', 'evaluation' , Translation :: get('CreateEvaluation'), null, 'onclick="javascript:showElement(\'' . self :: PROPERTY_FORMAT_LIST . '\')"');
+       			$form->add_element_hider('script_block');
+       			$form->add_element_hider('begin', self :: PROPERTY_FORMAT_LIST);
+        		$form->addElement('select', self :: PROPERTY_FORMAT_LIST ,Translation :: get('EvaluationFormat'), $formats_array);
+        		$form->add_element_hider('end', self :: PROPERTY_FORMAT_LIST);
 		}
-		return $form;
-	}*/
+	$form->addElement('category');
+	}
 }
+/* 	if(WebApplication :: is_active('gradebook'))
+        {
+        	require_once dirname (__FILE__) . '../../../../lib/gradebook/forms/evaluation_form.class.php';
+        	EvaluationForm :: build_evaluation_question($form);
+        }*/
 
 ?>
