9aee85d4423893cf8aef6baac7670788af33b629
jevdheyd <jevdheyd@koltempleister>
2010-03-31 14:30:57 +0200

    streaming video application

--- application/lib/streaming_video/jnlp.php
+++ application/lib/streaming_video/jnlp.php
@@ -10,5 +10,5 @@ require_once Path :: get_application_path().'lib/streaming_video/streaming_video
 	Path :: get('WEB_APP_PATH') . 'lib/streaming_video/webservices/webservice.class.php?wdsl',
 	Path :: get('WEB_APP_PATH') . 'lib/streaming_video/',                                            
         'jnlp.php?' . $_SERVER['QUERY_STRING'],
-	Path :: get('WEB_APP_PATH').'lib/streaming_video/signed_jar');                                   
+	Path :: get('WEB_APP_PATH').'lib/streaming_video/jar');                                   
 ?>
