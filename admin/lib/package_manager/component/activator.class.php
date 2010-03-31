48d10a14eb01dd1e1164692a6098e2950d104edb
vanpouckesven <devnull@localhost>
2010-03-31 11:46:30 +0200

    Small fix in the activator / deactivator for the package manager (#848)

--- admin/lib/package_manager/component/activator.class.php
+++ admin/lib/package_manager/component/activator.class.php
@@ -40,7 +40,7 @@ class PackageManagerActivatorComponent extends PackageManagerComponent
             {
                 $registration = $this->get_parent()->retrieve_registration($id);
                 
-                $registration->toggle_status();
+                $registration->set_status(Registration :: STATUS_ACTIVE);
                 if (! $registration->update())
                 {
                     $failures ++;
