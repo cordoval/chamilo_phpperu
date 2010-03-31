c11eb3817d3aff6e4ec737d6473d8aa4d47172fe
Tristan <devnull@localhost>
2010-03-31 13:20:05 +0200

    Small fix to coursesettings tool

--- application/lib/weblcms/tool/course_settings/component/course_settings_updater.class.php
+++ application/lib/weblcms/tool/course_settings/component/course_settings_updater.class.php
@@ -21,8 +21,11 @@ class CourseSettingsToolUpdaterComponent extends CourseSettingsToolComponent
             $this->display_footer();
             exit();
         }
-        
-        $form = new CourseForm(CourseForm :: TYPE_EDIT, $this->get_course(), $this->get_user(), $this->get_url(array(Tool :: PARAM_ACTION => CourseSettingsTool :: ACTION_UPDATE_COURSE_SETTINGS)));
+        $parameters = array(Tool :: PARAM_ACTION => CourseSettingsTool :: ACTION_UPDATE_COURSE_SETTINGS);
+       	$course_type_id = Request::get('course_type');
+        if(!is_null($course_type_id))
+        	$parameters['course_type'] = $course_type_id;
+        $form = new CourseForm(CourseForm :: TYPE_EDIT, $this->get_course(), $this->get_user(), $this->get_url($parameters), $this);
         
         if ($form->validate())
         {
