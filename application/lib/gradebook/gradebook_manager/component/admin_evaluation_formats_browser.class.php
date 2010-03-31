e121b65d973e478f2d9ab81e3d9c4588820f2053
Ben Vanmassenhove <devnull@localhost>
2010-03-31 13:51:08 +0200

    Gradebook Administration Component: activate and deactivate evaluation formats

---
+++ application/lib/gradebook/gradebook_manager/component/admin_evaluation_formats_browser.class.php
@@ -0,0 +1,34 @@
+<?php
+require_once dirname(__FILE__) . '/../gradebook_manager_component.class.php';
+// required table classes
+require_once dirname(__FILE__).'/evaluation_formats_browser/evaluation_formats_browser_table.class.php';
+
+class GradebookManagerAdminEvaluationFormatsBrowserComponent extends GradebookManagerComponent
+{
+	function run()
+	{
+		$trail = new BreadcrumbTrail();
+		$trail->add(new Breadcrumb(Redirect :: get_link(AdminManager :: APPLICATION_NAME, array(AdminManager :: PARAM_ACTION => AdminManager :: ACTION_ADMIN_BROWSER), array(), false, Redirect :: TYPE_CORE), Translation :: get('Administration')));
+        $trail->add(new Breadcrumb(Redirect :: get_link(AdminManager :: APPLICATION_NAME, array(AdminManager :: PARAM_ACTION => AdminManager :: ACTION_ADMIN_BROWSER, 'selected' => GradebookManager :: APPLICATION_NAME), array(), false, Redirect :: TYPE_CORE), Translation :: get('Gradebook') ));
+		$trail->add(new Breadcrumb($this->get_url(), Translation :: get('BrowseEvaluationFormats')));
+		
+		if (! $this->get_user()->is_platform_admin())
+        {
+            $this->display_header($trail, false, true);
+            Display :: error_message(Translation :: get("NotAllowed"));
+            $this->display_footer();
+            exit();
+        }
+        
+		$this->display_header($trail);
+		$this->get_table();
+		$this->display_footer();
+	}
+	
+	function get_table()
+	{
+		$table = new EvaluationFormatsBrowserTable($this, array());
+		echo $table->as_html();
+	}
+}
+?>
