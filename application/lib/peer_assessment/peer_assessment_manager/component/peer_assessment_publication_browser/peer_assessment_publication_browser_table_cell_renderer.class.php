c15f71821c7eab09ab7da995c7277fc8dabdab21
nick.van.loocke <>
2010-04-08 16:39:44 +0200

    Added the move function and the category tree. Added some smaller fixes: deleter confirm box, show build function, ...

--- application/lib/peer_assessment/peer_assessment_manager/component/peer_assessment_publication_browser/peer_assessment_publication_browser_table_cell_renderer.class.php
+++ application/lib/peer_assessment/peer_assessment_manager/component/peer_assessment_publication_browser/peer_assessment_publication_browser_table_cell_renderer.class.php
@@ -55,12 +55,34 @@ class PeerAssessmentPublicationBrowserTableCellRenderer extends DefaultPeerAsses
      */
     private function get_modification_links($peer_assessment_publication)
     {
-        $toolbar_data = array();
+
+        $toolbar_data = array();        
+        $user = $this->browser->get_user();
         
-        $toolbar_data[] = array('href' => $this->browser->get_update_peer_assessment_publication_url($peer_assessment_publication), 'label' => Translation :: get('Edit'), 'img' => Theme :: get_common_image_path() . 'action_edit.png');
+        $toolbar_data[] = array('href' => $this->browser->get_peer_assessment_publication_viewer_url($peer_assessment_publication), 'label' => Translation :: get('TakePeerAssessment'), 'img' => Theme :: get_common_image_path() . 'action_next.png');
+        $toolbar_data[] = array('href' => $this->browser->get_peer_assessment_results_viewer_url($peer_assessment_publication), 'label' => Translation :: get('ViewResults'), 'img' => Theme :: get_common_image_path() . 'action_view_results.png');
         
-        $toolbar_data[] = array('href' => $this->browser->get_delete_peer_assessment_publication_url($peer_assessment_publication), 'label' => Translation :: get('Delete'), 'img' => Theme :: get_common_image_path() . 'action_delete.png');
         
+    	if ($user->is_platform_admin() || $user->get_id() == $peer_assessment_publication->get_publisher())
+        {
+            $toolbar_data[] = array('href' => $this->browser->get_delete_peer_assessment_publication_url($peer_assessment_publication), 'label' => Translation :: get('Delete'), 'img' => Theme :: get_common_image_path() . 'action_delete.png','confirm' => true);
+            $toolbar_data[] = array('href' => $this->browser->get_update_peer_assessment_publication_url($peer_assessment_publication), 'label' => Translation :: get('Edit'), 'img' => Theme :: get_common_image_path() . 'action_edit.png');
+            
+            if ($peer_assessment_publication->get_hidden())
+            {
+                $toolbar_data[] = array('href' => $this->browser->get_change_peer_assessment_publication_visibility_url($peer_assessment_publication), 'label' => Translation :: get('Show'), 'img' => Theme :: get_common_image_path() . 'action_visible_na.png');
+            }
+            else
+            {
+                $toolbar_data[] = array('href' => $this->browser->get_change_peer_assessment_publication_visibility_url($peer_assessment_publication), 'label' => Translation :: get('Hide'), 'img' => Theme :: get_common_image_path() . 'action_visible.png');
+            }
+
+            $toolbar_data[] = array('href' => $this->browser->get_move_peer_assessment_publication_url($peer_assessment_publication), 'label' => Translation :: get('Move'), 'img' => Theme :: get_common_image_path() . 'action_move.png');
+			$toolbar_data[] = array('href' => $this->browser->get_build_peer_assessment_url($peer_assessment_publication), 'img' => Theme :: get_common_image_path() . 'action_build.png', 'label' => Translation :: get('BuildComplex'));
+        }
+        
+        
+        // Gradebook
         if(WebApplication :: is_active('gradebook'))
         {
         	require_once dirname (__FILE__) . '/../../../../gradebook/gradebook_manager/gradebook_manager.class.php';
