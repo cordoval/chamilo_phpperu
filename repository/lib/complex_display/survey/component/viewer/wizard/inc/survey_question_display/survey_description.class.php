0bc5b87820cef0188bb3e1679e57520b917ca491
eduard <devnull@localhost>
2010-04-09 09:03:17 +0200

    update

--- repository/lib/complex_display/survey/component/viewer/wizard/inc/survey_question_display/survey_description.class.php
+++ repository/lib/complex_display/survey/component/viewer/wizard/inc/survey_question_display/survey_description.class.php
@@ -13,14 +13,16 @@ class SurveyDescriptionDisplay extends SurveyQuestionDisplay
         
         if ($description->has_description())
         {
-            $html[] = '<div class="description">';
+             $html[] = '<div class="description">';
             $html[] = $this->parse($description->get_description());
+            
             $html[] = '<div class="clear">&nbsp;</div>';
             $html[] = '</div>';
+          
         }
         
         $detail = implode("\n", $html);
-        $formvalidator->addElement('html', $detail);
+        //$formvalidator->addElement('html', $detail);
     }
 
     function get_instruction()
@@ -30,7 +32,42 @@ class SurveyDescriptionDisplay extends SurveyQuestionDisplay
 
     function add_header()
     {
-    
+    	
+    	 $formvalidator = $this->formvalidator;
+     
+    $html[] = '<div class="assessment">';
+        $html[] = '<div class="question">';
+        $html[] = '<div class="title">';
+        $html[] = '<div class="number">';
+//        $html[] = '<div class="bevel">';
+//        $html[] =  'test.';
+//        $html[] = '</div>';
+        $html[] = '</div>';
+        $html[] = '<div class="text">';
+        $html[] = '<div class="bevel">';
+        $title = 'title';
+        $html[] = $this->parse($title);
+        
+        $html[] = '</div>';
+        $html[] = '</div>';
+        $html[] = '<div class="clear"></div>';
+        $html[] = '</div>';
+       $html[] = '<div class="answer">';
+      
+//        $description = $this->question->get_description();
+//        if ($this->question->has_description())
+//        {
+//            $html[] = '<div class="description">';
+//            
+//            $html[] = $this->parse($description);
+//            $html[] = '<div class="clear">&nbsp;</div>';
+//            $html[] = '</div>';
+//        }
+        $html[] = '</div>'; 
+       $html[] = '<div class="clear"></div>';
+        
+        $header = implode("\n", $html);
+       $this->get_formvalidator()->addElement('html', $header);
    
     }
 }
