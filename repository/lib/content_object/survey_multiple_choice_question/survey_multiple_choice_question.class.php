875a5c845af148cd4ac297dcf87f933f9cbd1511
eduard <devnull@localhost>
2010-04-09 16:20:57 +0200

    added required option classes

--- repository/lib/content_object/survey_multiple_choice_question/survey_multiple_choice_question.class.php
+++ repository/lib/content_object/survey_multiple_choice_question/survey_multiple_choice_question.class.php
@@ -4,6 +4,7 @@
  * @package repository.lib.content_object.survey_multiple_choice_question
  */
 require_once PATH :: get_repository_path() . '/question_types/multiple_choice_question/multiple_choice_question.class.php';
+require_once dirname(__FILE__) . '/survey_multiple_choice_question_option.class.php';
 
 class SurveyMultipleChoiceQuestion extends MultipleChoiceQuestion
 {
