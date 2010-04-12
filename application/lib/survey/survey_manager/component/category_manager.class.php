979265a195cc61f7eae7ed28484d58586cd3eb93
eduard <devnull@localhost>
2010-04-12 14:52:34 +0200

    implemented some rights for survey application

--- application/lib/survey/survey_manager/component/category_manager.class.php
+++ application/lib/survey/survey_manager/component/category_manager.class.php
@@ -12,6 +12,15 @@ class SurveyManagerCategoryManagerComponent extends SurveyManagerComponent
      */
     function run()
     {
+        
+    	if (! SurveyRights :: is_allowed(SurveyRights :: VIEW_RIGHT, 'category_manager', 'sts_component'))
+        {
+            $this->display_header($trail);
+            $this->display_error_message(Translation :: get('NotAllowed'));
+            $this->display_footer();
+            exit();
+        }
+    	
         $trail = new BreadcrumbTrail();
         $trail->add(new Breadcrumb($this->get_browse_survey_publications_url(), Translation :: get('BrowseSurveyPublications')));
         $trail->add(new Breadcrumb($this->get_manage_survey_publication_categories_url(), Translation :: get('ManageCategories')));
