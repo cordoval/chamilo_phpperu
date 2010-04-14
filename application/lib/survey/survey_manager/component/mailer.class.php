c88b13a47fcfdf9a196a0b5f0fcce74aa0de96ee
scaramanga <devnull@localhost>
2010-04-14 09:57:57 +0200

    Fixing another bug ...

--- application/lib/survey/survey_manager/component/mailer.class.php
+++ application/lib/survey/survey_manager/component/mailer.class.php
@@ -257,6 +257,7 @@ class SurveyManagerMailerComponent extends SurveyManagerComponent
         $header = new Header();
         $header->add_default_headers();
         $header->set_page_title(PlatformSetting :: get('site_name'));
+        $header->add_html_header('<style type="text/css">body {background-color:white; padding: 10px;}</style>');
         $fullbody[] = $header->toHtml();
         $fullbody[] = '<body>' . "\n";
         $fullbody[] = $email->get_mail_content() . $text . '<br/>';
