2af7a75f9eda0d2f53097bac8bce1d9023ed2c1d
nick.van.loocke <>
2010-04-20 16:15:30 +0200

    Made the builder of a peer assessment object available (to create or browse a competence, an indicator and a criteria). Working on how you could take a peer assessment. Also fixed some small bugs.

---
+++ repository/lib/complex_display/peer_assessment/component/viewer/wizard/inc/peer_assessment_question_display/indicator.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class IndicatorDisplay
+{
+	function display()
+	{
+		/*$html = array();
+		$html[] = 'test';
+		echo implode("\n", $html);
+        echo '<div style="clear: both;"></div>';*/
+	}
+}
+
+?>
