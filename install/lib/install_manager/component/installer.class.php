bd4f2a75000f30adad0bf2d5c2df05f8626f63ae
vanpouckesven <devnull@localhost>
2010-04-23 08:51:37 +0200

    Some changes to the installer

--- install/lib/install_manager/component/installer.class.php
+++ install/lib/install_manager/component/installer.class.php
@@ -6,7 +6,6 @@
  * @author Hans De Bisschop
  */
 require_once dirname(__FILE__) . '/../install_manager.class.php';
-require_once dirname(__FILE__) . '/../install_manager_component.class.php';
 require_once dirname(__FILE__) . '/inc/install_wizard.class.php';
 /**
  * Installer install manager component which allows the user to install the platform
