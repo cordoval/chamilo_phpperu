c13c06c7d4727fa1edbbb8d5f406adf722a60707
Yannick <devnull@localhost>
2010-04-28 10:02:03 +0200

    Small fix  'request_by_course subscriber'

--- application/lib/weblcms/data_manager/database.class.php
+++ application/lib/weblcms/data_manager/database.class.php
@@ -1937,7 +1937,7 @@ class DatabaseWeblcmsDataManager extends Database implements WeblcmsDataManagerI
 
     function count_requests_by_course($condition)
     {
-        $condition = new EqualityCondition(CourseRequest :: PROPERTY_COURSE_ID, $id);
+        //$condition = new EqualityCondition(CourseRequest :: PROPERTY_COURSE_ID, $id);
         return $this->count_objects(CourseRequest :: get_table_name(), $condition);
     }
 
