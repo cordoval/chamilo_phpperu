3c21ff4e6051598cd738d819ce907cd920cf8dd2
nick.van.loocke <>
2010-05-04 13:25:21 +0200

    Peer assessment: for each user in a peer assessment you can choose the criteria for each indicator.

--- repository/lib/complex_display/peer_assessment/component/viewer/wizard/competences_peer_assessment_viewer_wizard_page.class.php
+++ repository/lib/complex_display/peer_assessment/component/viewer/wizard/competences_peer_assessment_viewer_wizard_page.class.php
@@ -14,6 +14,7 @@ class CompetencesPeerAssessmentViewerWizardPage extends PeerAssessmentViewerWiza
 
     function buildForm()
     {
+    	dump($_POST);
         $this->_formBuilt = true;    
         
         // *********************************		
