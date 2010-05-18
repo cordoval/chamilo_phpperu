cb02390db6a9bb03ef06cb2dedad0d29d7b33e92
Ben Vanmassenhove <devnull@localhost>
2010-05-18 09:00:48 +0200

    Gradebook: some more translations added.

--- languages/english/gradebook.inc.php
+++ languages/english/gradebook.inc.php
@@ -26,4 +26,8 @@ $lang['gradebook']['ForumCategories'] = 'Forum Categories';
 $lang['gradebook']['WikiCategories'] = 'Wiki Categories';
 $lang['gradebook']['AssessmentCategories'] = 'Assessment Categories';
 $lang['gradebook']['NoEvaluations'] = 'No Evaluations';
+$lang['gradebook']['AddExternalEvaluation'] = 'Add external evaluation';
+$lang['gradebook']['ExternalItemProperties'] = 'External item properties';
+$lang['gradebook']['RefreshScoreProperties'] = 'Refresh score properties';
+$lang['gradebook']['ScoreProperties'] = 'Score properties';
 ?>
