04d53007b2fada786e41081ccfa3dbed78282575
Ben Vanmassenhove <devnull@localhost>
2010-05-18 11:49:52 +0200

    Gradebook: Breadcrumb changes.

--- application/lib/gradebook/gradebook_manager/component/view_evaluations_on_publication.class.php
+++ application/lib/gradebook/gradebook_manager/component/view_evaluations_on_publication.class.php
@@ -7,7 +7,8 @@ class GradebookManagerViewEvaluationsOnPublicationComponent extends GradebookMan
 	{
 		$trail = new BreadcrumbTrail();
 		$trail->add(new Breadcrumb($this->get_url(array(GradebookManager :: PARAM_ACTION => GradebookManager :: ACTION_VIEW_HOME)), Translation :: get('Gradebook')));
-		$trail->add(new Breadcrumb($this->get_url(array(GradebookManager :: PARAM_ACTION => GradebookManager :: ACTION_VIEW_HOME, GradebookManager :: PARAM_PUBLICATION_TYPE => Request :: get(GradebookManager :: PARAM_PUBLICATION_TYPE))), Translation :: get('BrowsePublicationsOf') . ' ' . Request :: get(GradebookManager :: PARAM_PUBLICATION_TYPE)));
+		$trail->add(new Breadcrumb($this->get_url(array(GradebookManager :: PARAM_ACTION => GradebookManager :: ACTION_VIEW_HOME, GradebookManager :: PARAM_PUBLICATION_TYPE => Request :: get(GradebookManager :: PARAM_PUBLICATION_TYPE))), Translation :: get('BrowsePublications')));
+		$trail->add(new Breadcrumb($this->get_url(array(GradebookManager :: PARAM_ACTION => GradebookManager :: ACTION_VIEW_HOME, GradebookManager :: PARAM_PUBLICATION_TYPE => Request :: get(GradebookManager :: PARAM_PUBLICATION_TYPE), GradebookManager :: PARAM_PUBLICATION_APP => Request :: get(GradebookManager :: PARAM_PUBLICATION_APP))), ucfirst(Request :: get(GradebookManager :: PARAM_PUBLICATION_APP))));
         
         $rtv = new ReportingViewer($this);
         $rtv->add_template_by_name('publication_evaluations_template', GradebookManager :: APPLICATION_NAME);
