65fb968fd72b29fcd16c24444056dacf5e736d80
scaramanga <devnull@localhost>
2010-05-26 11:31:05 +0200

    Refactoring the complex builders

--- repository/lib/complex_builder/component/browser.class.php
+++ repository/lib/complex_builder/component/browser.class.php
@@ -10,45 +10,45 @@ class ComplexBuilderBrowserComponent extends ComplexBuilderComponent
 
     function run()
     {
-        $menu_trail = $this->get_clo_breadcrumbs();
+        $menu_trail = $this->get_complex_content_object_breadcrumbs();
         $trail = new BreadcrumbTrail(false);
         //$trail->add(new Breadcrumb($this->get_url(array('builder_action' => null, Application :: PARAM_ACTION => RepositoryManager :: ACTION_BROWSE_CONTENT_OBJECTS)), Translation :: get('Repository')));
         $trail->merge($menu_trail);
         $trail->add_help('repository builder');
-        
-        if ($this->get_cloi())
+
+        if ($this->get_complex_content_object_item())
         {
-            $lo = RepositoryDataManager :: get_instance()->retrieve_content_object($this->get_cloi()->get_ref());
+            $lo = RepositoryDataManager :: get_instance()->retrieve_content_object($this->get_complex_content_object_item()->get_ref());
         }
         else
         {
-            $lo = $this->get_root_lo();
+            $lo = $this->get_root_content_object();
         }
-        
+
         $this->display_header($trail);
         $action_bar = $this->get_action_bar($lo);
-        
+
         echo '<br />';
         if ($action_bar)
         {
             echo $action_bar->as_html();
             echo '<br />';
         }
-        
-        $display = ContentObjectDisplay :: factory($this->get_root_lo());
+
+        $display = ContentObjectDisplay :: factory($this->get_root_content_object());
         echo $display->get_full_html();
-        
+
         echo '<br />';
         echo $this->get_creation_links($lo);
         echo '<div class="clear">&nbsp;</div><br />';
-        
+
         echo '<div style="width: 18%; overflow: auto; float: left;">';
-        echo $this->get_clo_menu();
+        echo $this->get_complex_content_object_menu();
         echo '</div><div style="width: 80%; float: right;">';
-        echo $this->get_clo_table_html();
+        echo $this->get_complex_content_object_table_html();
         echo '</div>';
         echo '<div class="clear">&nbsp;</div>';
-        
+
         $this->display_footer();
     }
 }
