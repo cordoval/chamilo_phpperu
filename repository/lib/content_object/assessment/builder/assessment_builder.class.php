5d2d8ab452e5e1fbe04fd52d8f5b92c9df717f4e
Magali <magali.gillard@ehb.be>
2010-05-26 15:46:18 +0200

    assessment complex builder

--- repository/lib/content_object/assessment/builder/assessment_builder.class.php
+++ repository/lib/content_object/assessment/builder/assessment_builder.class.php
@@ -38,7 +38,19 @@ class AssessmentBuilder extends ComplexBuilder
                 break;
             case ComplexBuilder::ACTION_CREATE_COMPLEX_CONTENT_OBJECT_ITEM :
                 $component = $this->create_component('Creator');
-                break;    
+                break; 
+            case ComplexBuilder::ACTION_DELETE_COMPLEX_CONTENT_OBJECT_ITEM : 
+            	$component = $this->create_component('Deleter');
+            	break;
+            case ComplexBuilder::ACTION_MOVE_COMPLEX_CONTENT_OBJECT_ITEM :
+            	$component = $this->create_component('Mover');
+            	break;
+            case ComplexBuilder::ACTION_VIEW_COMPLEX_CONTENT_OBJECT_ITEM :
+            	$component = $this->create_component('Viewer');
+            	break;
+            case ComplexBuilder::ACTION_UPDATE_COMPLEX_CONTENT_OBJECT_ITEM :
+            	$component = $this->create_component('Updater');
+            	break;
             default : 
             	$this->set_action(ComplexBuilder :: ACTION_BROWSE);
             	$component = $this->create_component('Browser');
