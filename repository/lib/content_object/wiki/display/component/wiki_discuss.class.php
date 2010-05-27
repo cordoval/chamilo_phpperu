6b54727092c7c84e4d457383214f8222c2694fca
scaramanga <hans.de.bisschop@ehb.be>
2010-05-27 14:32:59 +0200

    Refactoring wikidisplay

---
+++ repository/lib/content_object/wiki/display/component/wiki_discuss.class.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * $Id: wiki_discuss.class.php 200 2009-11-13 12:30:04Z kariboe $
+ * @package repository.lib.complex_display.wiki.component
+ */
+/*
+ * This is the discuss page. Here a user can add feedback to a wiki_page.
+ * Author: Stefan Billiet
+ * Author: Nick De Feyter
+ */
+
+require_once Path :: get_repository_path() . 'lib/content_object/wiki/display/wiki_parser.class.php';
+require_once Path :: get_repository_path() . 'lib/content_object/wiki/displaywiki_display.class.php';
+
+class WikiDisplayWikiDiscussComponent extends WikiDisplay
+{
+    private $action_bar;
+    private $wiki_page_id;
+    private $complex_id;
+    private $feedback_id;
+    private $links;
+    const TITLE_MARKER = '<!-- /title -->';
+    const DESCRIPTION_MARKER = '<!-- /description -->';
+
+    function run()
+    {
+        if (! $this->is_allowed(VIEW_RIGHT))
+        {
+            Display :: not_allowed();
+            return;
+        }
+        
+        $data_manager = RepositoryDataManager :: get_instance();
+        $repository_manager = new RepositoryManager();
+        
+        /*
+         * publication and complex object id are requested.
+         * These are used to retrieve
+         *  1) the complex object ( reference is stored )
+         *  2) the learning object ( actual inforamation about a wiki_page is stored here )
+         *
+         */
+        
+        $this->complex_id = Request :: get(ComplexDisplay :: PARAM_SELECTED_COMPLEX_CONTENT_OBJECT_ITEM_ID);
+        
+        $complexObject = $data_manager->retrieve_complex_content_object_item($this->complex_id);
+        if (isset($complexObject))
+        {
+            $this->wiki_page_id = $complexObject->get_ref();
+            $data_manager->retrieve_content_object($this->wiki_page_id);
+        }
+        $wiki_page = $data_manager->retrieve_content_object($this->wiki_page_id);
+        
+        $this->display_header($this->get_parent()->get_breadcrumbtrail());
+        
+        $this->action_bar = $this->get_parent()->get_toolbar($this, $this->get_root_content_object()->get_id(), $this->get_root_content_object(), $this->complex_id); //$this->get_toolbar();
+        //echo '<div id="trailbox2" style="padding:0px;">' . $this->get_parent()->get_breadcrumbtrail()->render() . '<br /><br /><br /></div>';
+        echo '<div style="float:left; width: 135px;">' . $this->action_bar->as_html() . '</div>';
+        echo '<div style="padding-left: 15px; margin-left: 150px; border-left: 1px solid grey;"><div style="font-size:20px;">' . Translation :: get('DiscussThe') . ' ' . $wiki_page->get_title() . ' ' . Translation :: get('Page') . '<hr style="height:1px;color:#4271B5;width:100%;"></div>';
+        
+        /*
+         *  We make use of the existing ContentObjectDisplay class, changing the type to wiki_page
+         */
+        $display = ContentObjectDisplay :: factory($wiki_page);
+        /*
+         *  Here we make the call to the wiki_parser.
+         *  For more information about the parser, please read the information in the wiki_parser class.
+         */
+        
+        $parser = new WikiParser($this, $this->get_root_content_object()->get_id(), $display->get_full_html(), $this->complex_id);
+        $parser->parse_wiki_text();
+        
+        $this->set_script();
+        echo '<a id="showhide" href="#">[' . Translation :: get('Hide') . ']</a><br /><br />';
+        echo '<div id="content" style="line-height: 110%;">' . $parser->get_wiki_text() . '</div><br />';
+        
+        /*
+         *  We make use of the existing condition framework to show the data we want.
+         *  If the publication id , and the compled object id are equal to the ones passed the feedback will be shown.
+         */
+        
+        if (isset($this->complex_id) && $this->get_root_content_object()->get_id() != null)
+        {
+            if (Request :: get('application') == 'wiki')
+            {
+                $conditions[] = new EqualityCondition(WikiPubFeedback :: PROPERTY_WIKI_PUBLICATION_ID, Request :: get('wiki_publication'));
+                $conditions[] = new EqualityCondition(WikiPubFeedback :: PROPERTY_COMPLEX_CONTENT_OBJECT_ITEM_ID, $this->complex_id);
+                $condition = new AndCondition($conditions);
+                $feedbacks = WikiDataManager :: get_instance()->retrieve_wiki_pub_feedbacks($condition);
+            }
+            else
+            {
+                $conditions[] = new EqualityCondition(ContentObjectPubFeedback :: PROPERTY_PUBLICATION_ID, Request :: get('pid'));
+                $conditions[] = new EqualityCondition(ContentObjectPubFeedback :: PROPERTY_COMPLEX_CONTENT_OBJECT_ITEM_ID, $this->complex_id);
+                $condition = new AndCondition($conditions);
+                $feedbacks = $data_manager->retrieve_content_object_pub_feedback($condition);
+            }
+            while ($feedback = $feedbacks->next_result())
+            {
+                if ($i == 0)
+                {
+                    echo '<div style="font-size:18px;">' . Translation :: get('Feedback') . '</div><hr>';
+                    echo $this->show_add_feedback() . '<br /><br />';
+                }
+                $this->feedback_id = $feedback->get_feedback_id();
+                /*
+                 *  We retrieve the learning object, because that one contains the information we want to show.
+                 *  We then display it using the ContentObjectDisplay and setting the type to feedback
+                 */
+                $feedback_display = $data_manager->retrieve_content_object($this->feedback_id);
+                echo $this->show_feedback($feedback_display);
+                $i ++;
+            
+            }
+        }
+        
+        echo '</div>';
+        
+        $this->display_footer();
+    
+    }
+
+    function build_feedback_actions()
+    {
+        $actions[] = array('href' => $this->get_url(array(WikiDisplay :: PARAM_DISPLAY_ACTION => WikiDisplay :: ACTION_DELETE_FEEDBACK, WikiPubFeedback :: PROPERTY_FEEDBACK_ID => $this->feedback_id, ComplexDisplay :: PARAM_SELECTED_COMPLEX_CONTENT_OBJECT_ITEM_ID => $this->complex_id, 'pid' => Request :: get('pid'), 'wiki_publication' => Request :: get('wiki_publication'))), 'label' => Translation :: get('Delete'), 'img' => Theme :: get_common_image_path() . 'action_delete.png', 'confirm' => true);
+        
+        $actions[] = array('href' => $this->get_url(array(WikiDisplay :: PARAM_DISPLAY_ACTION => WikiDisplay :: ACTION_EDIT_FEEDBACK, WikiPubFeedback :: PROPERTY_FEEDBACK_ID => $this->feedback_id, ComplexDisplay :: PARAM_SELECTED_COMPLEX_CONTENT_OBJECT_ITEM_ID => $this->complex_id, 'pid' => Request :: get('pid'), 'wiki_publication' => Request :: get('wiki_publication'))), 'label' => Translation :: get('Edit'), 'img' => Theme :: get_common_image_path() . 'action_edit.png');
+        
+        return Utilities :: build_toolbar($actions);
+    
+    }
+
+    function show_add_feedback()
+    {
+        $actions[] = array('href' => $this->get_url(array(WikiDisplay :: PARAM_DISPLAY_ACTION => WikiDisplay :: ACTION_FEEDBACK_COMPLEX_CONTENT_OBJECT_ITEM, 'pid' => Request :: get('pid'), 'wiki_publication' => Request :: get('wiki_publication'), ComplexDisplay :: PARAM_SELECTED_COMPLEX_CONTENT_OBJECT_ITEM_ID => $this->complex_id)), 'label' => Translation :: get('AddFeedback'), 'img' => Theme :: get_common_image_path() . 'action_add.png', 'confirm' => false);
+        
+        return Utilities :: build_toolbar($actions);
+    
+    }
+
+    private function show_feedback($object)
+    {
+        $creationDate = $object->get_creation_date();
+        
+        $html = array();
+        $html[] = '<div class="content_object" style="background-image: url(' . Theme :: get_common_image_path() . 'content_object/' . $object->get_icon_name() . ($object->is_latest_version() ? '' : '_na') . '.png);">';
+        $html[] = '<div class="title">' . Utilities :: htmlentities($object->get_title()) . ' | ' . Utilities :: htmlentities(date("F j, Y, H:i:s", $creationDate)) . '</div>';
+        $html[] = self :: TITLE_MARKER;
+        $html[] = $object->get_description();
+        $html[] = self :: DESCRIPTION_MARKER;
+        $html[] = '<div style="float:right">' . $this->build_feedback_actions() . '</div>';
+        $html[] = '<br />';
+        $html[] = '</div>';
+        
+        return implode("\n", $html);
+    }
+
+    private function set_script()
+    {
+        echo ResourceManager :: get_instance()->get_resource_html(Path :: get(WEB_LIB_PATH) . 'javascript/showhide_content.js');
+        ;
+    }
+
+}
+
+?>
---
+++ repository/lib/content_object/wiki/display/component/wiki_discuss/wiki_discuss_browser.class.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * $Id: wiki_discuss_browser.class.php 200 2009-11-13 12:30:04Z kariboe $
+ * @package repository.lib.complex_display.wiki.component.wiki_discuss
+ */
+
+require_once dirname(__FILE__) . '/../../../../content_object_pub_feedback_browser.class.php';
+require_once dirname(__FILE__) . '/../../../../browser/list_renderer/content_object_publication_details_renderer.class.php';
+require_once Path :: get_repository_path() . 'lib/content_object/announcement/announcement.class.php';
+require_once Path :: get_repository_path() . 'lib/content_object/description/description.class.php';
+
+/**
+ * Browser to allow the user to view the published feedback on a wiki page
+ */
+class WikiDiscussBrowser extends ContentObjectPubFeedbackBrowser
+{
+    private $feedbacks;
+
+    function WikiDiscussBrowser($parent)
+    {
+        parent :: __construct($parent, 'wiki');
+        
+        $renderer = new ListContentObjectPublicationListRenderer($this);
+        $actions = array(Tool :: ACTION_DELETE => Translation :: get('DeleteSelected'), Tool :: ACTION_HIDE => Translation :: get('Hide'), Tool :: ACTION_SHOW => Translation :: get('Show'));
+        $renderer->set_actions($actions);
+        
+        $this->set_publication_list_renderer($renderer);
+
+        function get_publications($from, $count, $column, $direction)
+        {
+            if (empty($this->feedbacks))
+            {
+                $datamanager = RepositoryDataManager :: get_instance();
+                $conditions[] = new EqualityCondition(ContentObjectPubFeedback :: PROPERTY_PUBLICATION_ID, $this->publication_id);
+                $conditions[] = new EqualityCondition(ContentObjectPubFeedback :: PROPERTY_COMPLEX_CONTENT_OBJECT_ITEM_ID, $this->complex_id);
+                if ($this->get_parent()->get_condition())
+                    $conditions[] = $this->get_parent()->get_condition();
+                $condition = new AndCondition($conditions);
+                $feedbacks = $datamanager->retrieve_content_object_pub_feedback($condition);
+                while ($feedback = $feedbacks->next_result())
+                {
+                    // If the feedback is hidden and the user is not allowed to DELETE or EDIT, don't show this publication
+                    if (! ($this->is_allowed(DELETE_RIGHT) || $this->is_allowed(EDIT_RIGHT)))
+                    {
+                        continue;
+                    }
+                    $visible_feedbacks[] = $feedback;
+                }
+                
+                $this->feedbacks = $visible_feedbacks;
+            }
+            
+            return $this->feedbacks;
+        
+        }
+
+        function get_publication_count()
+        {
+            return count($this->get_publications());
+        }
+    }
+}
+?>
