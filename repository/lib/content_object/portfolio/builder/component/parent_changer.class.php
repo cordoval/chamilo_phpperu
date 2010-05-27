acb1dea53e40b56a4c6db0de8f85b7bd4cd593bd
nathalie <devnull@localhost>
2010-05-27 09:48:44 +0200

    complex builder refactoring

---
+++ repository/lib/content_object/portfolio/builder/component/parent_changer.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * $Id: updater.class.php 200 2009-11-13 12:30:04Z kariboe $
+ * @package repository.lib.complex_builder.component
+ */
+require_once dirname(__FILE__) . '/../portfolio_builder.class.php';
+
+class PortfolioBuilderParentChangerComponent extends ComplexBuilderComponent
+{
+    const PARAM_NEW_PARENT = 'new_parent';
+
+    function run()
+    {
+        $component = ComplexBuilderComponent :: factory(ComplexBuilderComponent :: PARENT_CHANGER_COMPONENT, $this);
+        $component->run();
+    }
+}
+
+?>
