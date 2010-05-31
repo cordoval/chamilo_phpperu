da4ba201805c72eee8d58f0abcd689e248ee177c
nblocry <nblocry@nathalie-laptop>
2010-05-31 16:37:09 +0200

    some portfolio translations

--- languages/dutch/portfolio.inc.php
+++ languages/dutch/portfolio.inc.php
@@ -6,7 +6,7 @@ $lang['portfolio']['PortfolioItemCreated'] = 'Portfolio item gecreëerd';
 $lang['portfolio']['PortfolioItemNotCreated'] = 'Portfolio item niet gecreëerd';
 $lang['portfolio']['PublishNewPortfolio'] = 'Publiceer nieuw portfolio';
 $lang['portfolio']['AddNewItemToPortfolio'] = 'Voeg een nieuw item toe aan het portfolio';
-$lang['portfolio']['PortfolioIntroduction'] = 'Welkom bij uw persoonlijk portfolio';
+$lang['portfolio']['PortfolioIntroduction'] = 'Welkom bij dit persoonlijk portfolio';
 $lang['portfolio']['PortfolioUpdated'] = 'Portfolio werd geüpdate';
 $lang['portfolio']['PortfolioNotUpdated'] = 'Portfolio werd niet geüpdate';
 $lang['portfolio']['PortfolioPropertiesUpdated'] = 'De eigenschappen van het portfolio werden geüpdate.';
@@ -14,4 +14,52 @@ $lang['portfolio']['PortfolioPropertiesNotUpdated'] = 'De eigenschappen van het 
 $lang['portfolio']['CreatePortfolio'] = 'Creëer portfolio';
 $lang['portfolio']['PortfolioCreated'] = 'Portfolio werd gecreëerd';
 $lang['portfolio']['PortfolioNotCreated'] = 'Portfolio werd niet gecreëerd';
+
+$lang['portfolio']['inheritFromParent'] = 'Gebruik dezelfde instellingen als voor de bovenliggende map';
+$lang['portfolio']['ViewBy'] = 'Dit item lezen';
+$lang['portfolio']['SystemDefaults'] = 'Gebruik de Standaard Systeem Instellingen';
+$lang['portfolio']['AnonymousUsers'] = 'Toegestaan voor alle bezoekers';
+$lang['portfolio']['SystemUsers'] = 'Toegestaan voor alle platform gebruikers (ingelogd)';
+$lang['portfolio']['ViewFeedbackBy'] = 'Feedback voor dit portfolio-item lezen';
+$lang['portfolio']['OnlyMe'] = 'Alleen toegestaan voor mij';
+$lang['portfolio']['GiveFeedbackBy'] = 'Feedback op dit portfolio-item lezen';
+$lang['portfolio']['EditableBy'] = 'Aanpassingen aan dit portfolio-item maken';
+$lang['portfolio']['ViewTitle'] = 'Bekijken';
+$lang['portfolio']['FeedbackTitle'] = 'Feedback';
+$lang['portfolio']['ValidationTitle'] = 'Validation';
+$lang['portfolio']['EditTitle'] = 'Aanpassen';
+$lang['portfolio']['PropertiesTitle'] = 'Rechten';
+$lang['portfolio']['PersonalPortfolio'] = 'Portfolio';
+$lang['portfolio']['set'] = 'Stel specifieke Rechten in voor dit item';
+$lang['portfolio']['inherit_default_set_choice'] = 'rechten setting';
+$lang['portfolio']['view'] = 'Portfolio-item bekijken';
+$lang['portfolio']['viewFeedback'] = 'Feedback bekijken';
+$lang['portfolio']['giveFeedback'] = 'feedback geven';
+$lang['portfolio']['edit'] = 'aanpassen';
+
+$lang['portfolio']['LastChangedDate'] = 'Portfolio laatst aangepast op';
+$lang['portfolio']['LastChangedAction'] = 'Laatste actie';
+$lang['portfolio']['LastChangedItem'] = 'Laatst aangepast item';
+$lang['portfolio']['PortfolioItemAdded'] = 'Portfolio Item werd toegevoegd';
+$lang['portfolio']['PortfolioAdded'] = 'Portfolio werd toegevoegd';
+$lang['portfolio']['FirstPortfolioCreated'] = 'Een eerste portfolio werd gecreëerd';
+$lang['portfolio']['PortfolioEdited'] = 'Een item werd aangepast';
+$lang['portfolio']['PortfolioChanged'] = 'Een aanpassing werd gemaakt';
+$lang['portfolio']['PortfolioDeleted'] = 'Een item werd verwijderd';
+$lang['portfolio']['PortfolioNotUpdatedYet'] = 'Er werd nog niets gepubliceerd';
+$lang['portfolio']['SelectedPortfolioItemDeleted'] = 'Het geselecteerde item werd verwijderd';
+$lang['portfolio']['NoPermissionToViewItem'] = 'De eigenaar van dit portfolio gaf u niet het recht om dit item te bekijken';
+$lang['portfolio']['FeedbackCreated'] = 'Uw Feedback werd toegevoegd';
+
+$lang['portfolio']['wrong'] = 'Fout';
+$lang['portfolio']['right'] = 'Juist';
+$lang['portfolio']['HideFeedback'] = 'Feedback verbergen';
+$lang['portfolio']['PublicationFeedback'] = 'Portfolio Feedback';
+$lang['portfolio']['PublicationGiveFeedback'] = 'Uw eigen Feedback toevoegen';
+$lang['portfolio']['BrowsePortfolios'] = 'Doorblader portfolios';
+
+$lang['portfolio']['NoPermissionToGiveFeedback'] = 'You don\'t have the permission to give feedback on this portfolio item';
+$lang['portfolio']['NoPermissionToViewFeedback'] = 'You don\'t have the permission to view the feedback on this portfolio item';
+
+
 ?>
