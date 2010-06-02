d78e65d4c81b5ffc820b25fe07d795c0ab0342d4
Tristan <devnull@localhost>
2010-06-02 15:09:40 +0200

    Bug fix #1383 &  #1381

--- repository/lib/content_object/learning_path/builder/component/prerequisites_builder.class.php
+++ repository/lib/content_object/learning_path/builder/component/prerequisites_builder.class.php
@@ -16,7 +16,6 @@ class LearningPathBuilderPrerequisitesBuilderComponent extends LearningPathBuild
         
         $menu_trail = $this->get_complex_content_object_breadcrumbs();
         $trail = BreadcrumbTrail :: get_instance();
-        $trail->merge($menu_trail);
         
         $parameters = array(LearningPathBuilder :: PARAM_COMPLEX_CONTENT_OBJECT_ITEM_ID => $parent_complex_content_object_item, LearningPathBuilder :: PARAM_SELECTED_COMPLEX_CONTENT_OBJECT_ITEM_ID => $complex_content_object_item_id);
         
