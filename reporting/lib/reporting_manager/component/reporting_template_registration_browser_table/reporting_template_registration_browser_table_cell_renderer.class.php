9dd376b1958bfe76e72e747de39739df47e57b93
Kariboe <devnull@localhost>
2010-06-09 09:48:01 +0200

    Refactoring Reporting tool_bar Task #1288

--- reporting/lib/reporting_manager/component/reporting_template_registration_browser_table/reporting_template_registration_browser_table_cell_renderer.class.php
+++ reporting/lib/reporting_manager/component/reporting_template_registration_browser_table/reporting_template_registration_browser_table_cell_renderer.class.php
@@ -45,16 +45,24 @@ class ReportingTemplateRegistrationBrowserTableCellRenderer extends DefaultRepor
      */
     private function get_modification_links($reporting_template_registration)
     {
-        $toolbar_data = array();
+    	
+		$toolbar = new Toolbar(); 
         
-        //$viewing_url = ReportingManager::get_reporting_template_registration_url_content($this->browser,$reporting_template_registration->get_classname());
-        $viewing_url = $this->browser->get_reporting_template_registration_viewing_url($reporting_template_registration);
-        $toolbar_data[] = array('href' => $viewing_url, 'label' => Translation :: get('View'), 'img' => Theme :: get_common_image_path() . 'action_reporting.png');
+    	$toolbar->add_item(new ToolbarItem(
+    			Translation :: get('View'),
+    			Theme :: get_common_image_path() . 'action_reporting.png',
+    			$this->browser->get_reporting_template_registration_viewing_url($reporting_template_registration),
+    			ToolbarItem :: DISPLAY_ICON
+    	));
+    	
+    	$toolbar->add_item(new ToolbarItem(
+    			Translation :: get('Edit'),
+    			Theme :: get_common_image_path() . 'action_edit.png',
+    			$this->browser->get_reporting_template_registration_editing_url($reporting_template_registration),
+    			ToolbarItem :: DISPLAY_ICON
+    	));
         
-        $editing_url = $this->browser->get_reporting_template_registration_editing_url($reporting_template_registration);
-        $toolbar_data[] = array('href' => $editing_url, 'label' => Translation :: get('Edit'), 'img' => Theme :: get_common_image_path() . 'action_edit.png');
-        
-        return Utilities :: build_toolbar($toolbar_data);
+        return $toolbar->as_html();
     }
 }
 ?>
