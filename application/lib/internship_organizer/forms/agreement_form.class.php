55dadd873c7b1bf6b7069b5632271234ca49623c
eduard <devnull@localhost>
2010-06-15 13:52:13 +0200

    xml feed fix

--- application/lib/internship_organizer/forms/agreement_form.class.php
+++ application/lib/internship_organizer/forms/agreement_form.class.php
@@ -10,9 +10,8 @@ class InternshipOrganizerAgreementForm extends FormValidator
 {
     
     const APPLICATION_NAME = 'internship_organizer';
-    const PARAM_TARGET = 'target_users_and_groups';
-    const PARAM_TARGET_OPTION = 'target_users_and_groups_option';
-    
+    const PARAM_TARGET = 'target_periods';
+        
     const TYPE_CREATE = 1;
     const TYPE_EDIT = 2;
     
@@ -48,50 +47,14 @@ class InternshipOrganizerAgreementForm extends FormValidator
         $this->add_html_editor(InternshipOrganizerAgreement :: PROPERTY_DESCRIPTION, Translation :: get('Description'), true);
         
         $url = Path :: get(WEB_PATH) . 'application/lib/internship_organizer/xml_feeds/xml_period_feed.php';
-        
-        //        $url = $this->get_path(WEB_PATH) . 'repository/xml_feed.php';
-        //            $locale = array();
-        //            $locale['Display'] = Translation :: get('AddPeriods');
-        //            $locale['Searching'] = Translation :: get('Searching');
-        //            $locale['NoResults'] = Translation :: get('NoResults');
-        //            $locale['Error'] = Translation :: get('Error');
-        //            
-        //
-        //            $elem = $this->addElement('element_finder', 'periods', Translation :: get('SelectPeriods'), $url, $locale, $attachments, $options);
-        //            $this->addElement('category');
-        //			$defaults = array();
-        //            $elem->setDefaults($defaults);
-        //             $elem->excludeElements(array($this->agreement->get_period_id()));
-        //         
-        //$elem->setDefaultCollapsed(count($attachments) == 0);
-        
-
-        $attributes = array();
-        //        $attributes['search_url'] = Path :: get(WEB_PATH) . 'common/xml_feeds/xml_user_group_feed.php';
-        //        $attributes['search_url'] = $url;
-        
-
         $locale = array();
-        $locale['Display'] = Translation :: get('ShareWith');
+        $locale['Display'] = Translation :: get('ChoosePeriods');
         $locale['Searching'] = Translation :: get('Searching');
         $locale['NoResults'] = Translation :: get('NoResults');
         $locale['Error'] = Translation :: get('Error');
         
-        $attributes['locale'] = $locale;
-        $attributes['defaults'] = array();
-        $attributes['options'] = array('load_elements' => false);
-        
-        //        $this->add_(self :: APPLICATION_NAME . '_opt_' . self :: PARAM_TARGET, Translation :: get('Period'), $attributes);
-        
-
-        $url = Path :: get(WEB_PATH) . 'application/lib/internship_organizer/xml_feeds/xml_period_feed.php';
-//        dump($url);
-        
-        //        $periods = $this->parent->get_selected_periods();
-         $url = Path :: get(WEB_PATH) . 'rights/xml_feeds/xml_rights_template_feed.php';
-
-        $elem = $this->addElement('element_finder', 'periods', Translation :: get('Periods'), $url, $locale, array());
-        $defaults = array();
+        $elem = $this->addElement('element_finder', self :: PARAM_TARGET, Translation :: get('Periods'), $url, $locale, array());
+        $defaults = array($this->agreement->get_period_id());
         $elem->setDefaults($defaults);
         $elem->setDefaultCollapsed(false);
     
@@ -133,6 +96,17 @@ class InternshipOrganizerAgreementForm extends FormValidator
         $agreement = $this->agreement;
         $values = $this->exportValues();
         
+        $dm = InternshipOrganizerDataManager::get_instance();
+        
+        $periods = $values[self :: PARAM_TARGET];
+        
+        foreach ($periods as $period_id) {
+        	$period = $dm->retrieve_period($period_id);
+        	dump($period);
+        }
+//        dump($values[self :: PARAM_TARGET]);
+//        exit;
+        
         $agreement->set_name($values[InternshipOrganizerAgreement :: PROPERTY_NAME]);
         $agreement->set_description($values[InternshipOrganizerAgreement :: PROPERTY_DESCRIPTION]);
         
@@ -149,8 +123,6 @@ class InternshipOrganizerAgreementForm extends FormValidator
         
         $defaults[InternshipOrganizerAgreement :: PROPERTY_NAME] = $agreement->get_name();
         $defaults[InternshipOrganizerAgreement :: PROPERTY_DESCRIPTION] = $agreement->get_description();
-        $defaults[self :: APPLICATION_NAME . '_opt_' . self :: PARAM_TARGET_OPTION] = 1;
-        
         parent :: setDefaults($defaults);
     }
 }
