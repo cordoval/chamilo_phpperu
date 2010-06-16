105a75091a16a392f0765f10f6bc745e59fd984e
scaramanga <devnull@localhost>
2010-06-16 23:28:36 +0200

    Small fix.

--- common/javascript/action_bar_horizontal.js
+++ common/javascript/action_bar_horizontal.js
@@ -1,6 +1,7 @@
 $(document).ready(function()
 {	
 	$(".action_bar_hide_container").toggle();
+	$(".action_bar .search_menu").css('padding-right', '20px');
 	
 	$(".action_bar_text").bind("click", showBlockScreen);
 	$(".action_bar_hide").bind("click", hideBlockScreen);
