72c3231b9cc7f08be513051f27f95c74a083a8b1
scaramanga <devnull@localhost>
2010-06-16 08:47:14 +0200

    Re-enabled publication of course homepage introductions.

--- application/lib/weblcms/tool/component/browser.class.php
+++ application/lib/weblcms/tool/component/browser.class.php
@@ -14,27 +14,27 @@ class ToolBrowserComponent extends ToolComponent
     function run()
     {
         $this->action_bar = $this->get_action_bar();
-        
+
         $this->display_header();
-        
+
         $publication_renderer = ContentObjectPublicationListRenderer :: factory($this->get_browser_type(), $this);
-        
+
         $actions[] = new ObjectTableFormAction(Tool :: ACTION_DELETE, Translation :: get('DeleteSelected'));
         $actions[] = new ObjectTableFormAction(Tool :: ACTION_HIDE, Translation :: get('Hide'), false);
         $actions[] = new ObjectTableFormAction(Tool :: ACTION_SHOW, Translation :: get('Show'), false);
-        
+
         $publication_renderer->set_actions($actions);
-        
+
         if ($this->get_course()->get_intro_text())
         {
             echo $this->display_introduction_text();
         }
-        
+
         echo $this->action_bar->as_html();
         echo '<div id="action_bar_browser">';
         echo $publication_renderer->as_html();
         echo '</div>';
-        
+
         $this->display_footer();
     }
 
@@ -56,19 +56,19 @@ class ToolBrowserComponent extends ToolComponent
             {
                 $user_id = $this->get_user_id();
                 $course_groups = $this->get_course_groups();
-                
+
                 $course_group_ids = array();
-                
+
                 foreach ($course_groups as $course_group)
                 {
                     $course_group_ids[] = $course_group->get_id();
                 }
             }
-            
+
             $conditions = array();
             $conditions[] = new EqualityCondition(ContentObjectPublication :: PROPERTY_COURSE_ID, $this->get_course_id());
             $conditions[] = new EqualityCondition(ContentObjectPublication :: PROPERTY_TOOL, $this->get_tool_id());
-            
+
             /* $access = array();
             $access[] = new InCondition('user_id', $user_id, $datamanager->get_alias('content_object_publication_user'));
             $access[] = new InCondition('course_group_id', $course_group_ids, $datamanager->get_alias('content_object_publication_course_group'));
@@ -76,25 +76,25 @@ class ToolBrowserComponent extends ToolComponent
             {
                 $access[] = new AndCondition(array(new EqualityCondition('user_id', null, $datamanager->get_alias('content_object_publication_user')), new EqualityCondition('course_group_id', null, $datamanager->get_alias('content_object_publication_course_group'))));
             }*/
-            
+
             $access = array();
             if ($user_id)
             {
                 $access[] = new InCondition(ContentObjectPublicationUser :: PROPERTY_USER, $user_id, ContentObjectPublicationUser :: get_table_name());
             }
-            
+
             if (count($course_group_ids) > 0)
             {
                 $access[] = new InCondition(ContentObjectPublicationCourseGroup :: PROPERTY_COURSE_GROUP_ID, $course_group_ids, ContentObjectPublicationCourseGroup :: get_table_name());
             }
-            
+
             if (! empty($user_id) || ! empty($course_group_ids))
             {
                 $access[] = new AndCondition(array(new EqualityCondition(ContentObjectPublicationUser :: PROPERTY_USER, null, ContentObjectPublicationUser :: get_table_name()), new EqualityCondition(ContentObjectPublicationCourseGroup :: PROPERTY_COURSE_GROUP_ID, null, ContentObjectPublicationCourseGroup :: get_table_name())));
             }
-            
+
             $conditions[] = new OrCondition($access);
-            
+
             $subselect_conditions = array();
             $subselect_conditions[] = new InCondition(ContentObject :: PROPERTY_TYPE, $this->get_allowed_types());
             if ($this->get_search_condition())
@@ -102,9 +102,9 @@ class ToolBrowserComponent extends ToolComponent
                 $subselect_conditions[] = $this->get_search_condition();
             }
             $subselect_condition = new AndCondition($subselect_conditions);
-            
+
             $conditions[] = new SubselectCondition(ContentObjectPublication :: PROPERTY_CONTENT_OBJECT_ID, ContentObject :: PROPERTY_ID, ContentObject :: get_table_name(), $subselect_condition, null, RepositoryDataManager :: get_instance());
-            
+
             if (method_exists($this->get_parent(), 'get_tool_conditions'))
             {
                 foreach ($this->get_parent()->get_tool_conditions() as $tool_condition)
@@ -112,9 +112,9 @@ class ToolBrowserComponent extends ToolComponent
                     $conditions[] = $tool_condition;
                 }
             }
-            
+
             $condition = new AndCondition($conditions);
-            
+
             $publications = $datamanager->retrieve_content_object_publications_new($condition, new ObjectTableOrder(Announcement :: PROPERTY_DISPLAY_ORDER_INDEX, SORT_ASC));
             $visible_publications = array();
             while ($publication = $publications->next_result())
@@ -128,9 +128,9 @@ class ToolBrowserComponent extends ToolComponent
             }
             $this->publications = $visible_publications;
         }
-        
+
         return $this->publications;
-    
+
     }
 
     /**
@@ -145,15 +145,15 @@ class ToolBrowserComponent extends ToolComponent
     function get_action_bar()
     {
         $action_bar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
-        
+
         $action_bar->set_search_url($this->get_url());
         if ($this->is_allowed(ADD_RIGHT))
         {
             $action_bar->add_common_action(new ToolbarItem(Translation :: get('Publish'), Theme :: get_common_image_path() . 'action_publish.png', $this->get_url(array(Tool :: PARAM_ACTION => Tool :: ACTION_PUBLISH)), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
         }
-        
+
         $action_bar->add_common_action(new ToolbarItem(Translation :: get('ShowAll'), Theme :: get_common_image_path() . 'action_browser.png', $this->get_url(array(Tool :: PARAM_ACTION => null)), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
-        
+
         if (! $this->introduction_text && $this->get_course()->get_intro_text())
         {
             if ($this->is_allowed(EDIT_RIGHT))
@@ -161,14 +161,14 @@ class ToolBrowserComponent extends ToolComponent
                 $action_bar->add_common_action(new ToolbarItem(Translation :: get('PublishIntroductionText'), Theme :: get_common_image_path() . 'action_introduce.png', $this->get_url(array(Tool :: PARAM_ACTION => Tool :: ACTION_PUBLISH_INTRODUCTION)), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
             }
         }
-        
+
         if (method_exists($this->get_parent(), 'get_tool_actions'))
         {
             $action_bar->set_tool_actions($this->get_parent()->get_tool_actions());
         }
-        
+
         $browser_types = $this->get_browser_types();
-        
+
         if (count($this->get_browser_types()) > 1)
         {
             foreach ($this->get_browser_types() as $browser_type)
@@ -176,7 +176,7 @@ class ToolBrowserComponent extends ToolComponent
                 $action_bar->add_tool_action(new ToolbarItem(Translation :: get(Utilities :: underscores_to_camelcase($browser_type) . 'View'), Theme :: get_image_path() . 'view_' . $browser_type . '.png', $this->get_url(array(Tool :: PARAM_BROWSER_TYPE => $browser_type)), ToolbarItem :: DISPLAY_ICON_AND_LABEL));
             }
         }
-        
+
         return $action_bar;
     }
 
@@ -186,54 +186,30 @@ class ToolBrowserComponent extends ToolComponent
     //        $conditions = array();
     //        $conditions[] = new EqualityCondition(ContentObjectPublication :: PROPERTY_COURSE_ID, $this->get_course_id());
     //        $conditions[] = new EqualityCondition(ContentObjectPublication :: PROPERTY_TOOL, $this->get_tool_id());
-    //        
+    //
     //        $subselect_condition = new EqualityCondition(ContentObject :: PROPERTY_TYPE, Introduction :: get_type_name());
     //        $conditions[] = new SubselectCondition(ContentObjectPublication :: PROPERTY_CONTENT_OBJECT_ID, ContentObject :: PROPERTY_ID, ContentObject :: get_table_name(), $subselect_condition, null, RepositoryDataManager :: get_instance());
     //        $condition = new AndCondition($conditions);
-    //        
+    //
     //        $publications = WeblcmsDataManager :: get_instance()->retrieve_content_object_publications_new($condition);
     //        $this->introduction_text = $publications->next_result();
     //    }
-    
+
 
     function display_introduction_text()
     {
         $conditions = array();
         $conditions[] = new EqualityCondition(ContentObjectPublication :: PROPERTY_COURSE_ID, $this->get_course_id());
         $conditions[] = new EqualityCondition(ContentObjectPublication :: PROPERTY_TOOL, $this->get_tool_id());
-        
+
         $subselect_condition = new EqualityCondition(ContentObject :: PROPERTY_TYPE, Introduction :: get_type_name());
         $conditions[] = new SubselectCondition(ContentObjectPublication :: PROPERTY_CONTENT_OBJECT_ID, ContentObject :: PROPERTY_ID, ContentObject :: get_table_name(), $subselect_condition, null, RepositoryDataManager :: get_instance());
         $condition = new AndCondition($conditions);
-        
+
         $publications = WeblcmsDataManager :: get_instance()->retrieve_content_object_publications_new($condition);
         $introduction_text = $publications->next_result();
-        
-        $html = array();
-        
-        if ($introduction_text)
-        {
-            if ($this->is_allowed(EDIT_RIGHT))
-            {
-                $toolbar = new Toolbar();
-                
-                $toolbar->add_item(new ToolbarItem(Translation :: get('Edit'), Theme :: get_common_image_path() . 'action_edit.png', $this->get_url(array(Tool :: PARAM_ACTION => Tool :: ACTION_UPDATE, Tool :: PARAM_PUBLICATION_ID => $introduction_text->get_id())), ToolbarItem :: DISPLAY_ICON));
-                $toolbar->add_item(new ToolbarItem(Translation :: get('Delete'), Theme :: get_common_image_path() . 'action_delete.png', $this->get_url(array(Tool :: PARAM_ACTION => Tool :: ACTION_DELETE, Tool :: PARAM_PUBLICATION_ID => $introduction_text->get_id())), ToolbarItem :: DISPLAY_ICON, true));
-            }
-            
-            $html[] = '<div class="announcements level_1" style="background-image: url('. Theme :: get_common_image_path() .'content_object/introduction.png);">';
-            $html[] = '<div class="title">';
-            $html[] = $introduction_text->get_content_object()->get_title();
-            $html[] = '</div><div class="clear">&nbsp;</div>';
-            $html[] = '<div class="description">';
-            $html[] = $introduction_text->get_content_object()->get_description();
-            $html[] = '</div>';
-            $html[] = $toolbar->as_html() . '<div class="clear"></div>';
-            $html[] = '</div>';
-            $html[] = '<br />';
-        }
-        
-        return implode("\n", $html);
+
+        return $this->get_parent()->display_introduction_text($introduction_text);
     }
 
     function get_search_condition()
@@ -245,14 +221,14 @@ class ToolBrowserComponent extends ToolComponent
             $conditions[] = new PatternMatchCondition(ContentObject :: PROPERTY_DESCRIPTION, '*' . $query . '*');
             return new OrCondition($conditions);
         }
-        
+
         return null;
     }
 
     function get_browser_type()
     {
         $browser_type = Request :: get(Tool :: PARAM_BROWSER_TYPE);
-        
+
         if ($browser_type && in_array($browser_type, $this->get_browser_types()))
         {
             return $browser_type;
