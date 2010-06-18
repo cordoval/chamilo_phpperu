d40947cf6130f2186be0b7c319d6528a9c5af066
David Hosse <devnull@localhost>
2010-06-18 15:24:10 +0200

    Export to Excel for Chamilo in general and Survey Tool in particular. By template, by block and by survey.

--- reporting/lib/exporters/reporting_excel_exporter.class.php
+++ reporting/lib/exporters/reporting_excel_exporter.class.php
@@ -3,23 +3,22 @@ class ReportingExcelExporter extends ReportingExporter
 {
 	function export()
     {
-    	$template = $this->get_template();
-    	$block = $template->get_current_block();
-        $data = $block->retrieve_data();
+    	$template = $this->get_template();      
         
-		$export = Export :: factory('excel', $data);
+        //$data = $template->export();
+        //$export = Export :: factory('excel', $data);
+        $export = Export :: factory('excel', $template);
         $export->set_filename($this->get_file_name());
     	$export->send_to_browser(); 
     }
     
     function save()
     {
-    	$template = $this->get_template();
-    	$block = $template->get_current_block();
-        $data = $block->retrieve_data();
+    	$template = $this->get_template();      
         
-    	$file = $this->get_file_name();
-        $export = Export :: factory('excel', $data);
+        //$data = $template->export();
+        //$export = Export :: factory('excel', $data);
+        $export = Export :: factory('excel', $template);
         $export->set_filename($this->get_file_name());
     	return $export->render_data(); 
     }
