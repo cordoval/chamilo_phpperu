1c92d03faea7b26ef08d00197fa8c1a1ebd7c3c5
vanpouckesven <devnull@localhost>
2010-06-25 09:26:42 +0200

    Task #1623 Some fixes to the learning path clo's

--- application/lib/weblcms/tool/learning_path/component/learning_path_viewer/content_object_display/glossary.class.php
+++ application/lib/weblcms/tool/learning_path/component/learning_path_viewer/content_object_display/glossary.class.php
@@ -11,7 +11,7 @@ class GlossaryDisplay extends LearningPathContentObjectDisplay
     function display_content_object($glossary)
     {
         $html[] = $this->add_tracking_javascript();
-        $link = $this->get_parent()->get_url(array(LearningPathTool :: PARAM_ACTION => LearningPathTool :: ACTION_VIEW_CLO, Tool :: PARAM_PUBLICATION_ID => $glossary->get_id()));
+        $link = $this->get_parent()->get_url(array(LearningPathTool :: PARAM_ACTION => LearningPathTool :: ACTION_VIEW_CLO, Tool :: PARAM_PUBLICATION_ID => Request :: get(Tool :: PARAM_PUBLICATION_ID), LearningPathTool :: PARAM_OBJECT_ID => $glossary->get_id()));
         $html[] = $this->display_link($link);
         
         return implode("\n", $html);
