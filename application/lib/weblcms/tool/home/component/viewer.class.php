137621e0c0a9be70d16d082fb7844cad76cfde2b
vanpouckesven <devnull@localhost>
2010-07-08 08:43:09 +0200

    Fixed some minor bugs in the weblcms introduction text

--- application/lib/weblcms/tool/home/component/viewer.class.php
+++ application/lib/weblcms/tool/home/component/viewer.class.php
@@ -18,19 +18,5 @@ class HomeToolViewerComponent extends HomeTool
         $this->display_footer();
     }
     
-	function get_introduction_text()
-    {
-        $conditions = array();
-        $conditions[] = new EqualityCondition(ContentObjectPublication :: PROPERTY_COURSE_ID, $this->get_course_id());
-        $conditions[] = new EqualityCondition(ContentObjectPublication :: PROPERTY_TOOL, $this->get_tool_id());
-
-        $subselect_condition = new EqualityCondition(ContentObject :: PROPERTY_TYPE, Introduction :: get_type_name());
-        $conditions[] = new SubselectCondition(ContentObjectPublication :: PROPERTY_CONTENT_OBJECT_ID, ContentObject :: PROPERTY_ID, ContentObject :: get_table_name(), $subselect_condition, null, RepositoryDataManager :: get_instance());
-        $condition = new AndCondition($conditions);
-
-        $publications = WeblcmsDataManager :: get_instance()->retrieve_content_object_publications($condition);
-        return $publications->next_result();
-    }
-    
 }
 ?>
