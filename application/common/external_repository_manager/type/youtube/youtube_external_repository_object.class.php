a105007f3a93c9a04b321b7cfcea6ee58b9ef816
scaramanga <hans.de.bisschop@ehb.be>
2010-07-11 18:59:02 +0200

    Continuing refactoring of the external repository manager

--- application/common/external_repository_manager/type/youtube/youtube_external_repository_object.class.php
+++ application/common/external_repository_manager/type/youtube/youtube_external_repository_object.class.php
@@ -1,28 +1,29 @@
 <?php
-require_once dirname(__FILE__) . '/../../streaming_media_object.class.php';
-class YoutubeStreamingMediaObject extends StreamingMediaObject
+require_once dirname(__FILE__) . '/../../general/streaming/streaming_media_object.class.php';
+
+class YoutubeExternalRepositoryObject extends StreamingMediaObject
 {
-	const PROPERTY_CATEGORY = 'category';
-	const PROPERTY_TAGS = 'tags';
-	const STATUS_REJECTED = 'rejected';
-	const STATUS_FAILED = 'failed';
-	const STATUS_PROCESSING = 'processing';
-	
-	function get_category()
+    const PROPERTY_CATEGORY = 'category';
+    const PROPERTY_TAGS = 'tags';
+    const STATUS_REJECTED = 'rejected';
+    const STATUS_FAILED = 'failed';
+    const STATUS_PROCESSING = 'processing';
+
+    function get_category()
     {
-        return $this->get_additional_property(self :: PROPERTY_CATEGORY);
+        return $this->get_default_property(self :: PROPERTY_CATEGORY);
     }
 
     function set_category($category)
     {
-        return $this->set_additional_property(self :: PROPERTY_CATEGORY, $category);
+        return $this->set_default_property(self :: PROPERTY_CATEGORY, $category);
     }
-    
-	function get_tags()
+
+    function get_tags()
     {
-        return $this->get_additional_property(self :: PROPERTY_TAGS);
+        return $this->get_default_property(self :: PROPERTY_TAGS);
     }
-    
+
     function get_type()
     {
         return 'youtube';
@@ -30,33 +31,34 @@ class YoutubeStreamingMediaObject extends StreamingMediaObject
 
     function set_tags($tags)
     {
-        return $this->set_additional_property(self :: PROPERTY_TAGS, $tags);
+        return $this->set_default_property(self :: PROPERTY_TAGS, $tags);
     }
 
-    static function get_additional_property_names()
+    static function get_default_property_names()
     {
-        return array(self :: PROPERTY_CATEGORY, self :: PROPERTY_TAGS);
+        return parent :: get_default_property_names(array(self :: PROPERTY_CATEGORY, self :: PROPERTY_TAGS));
     }
-    
+
     public function get_status_text()
     {
-		$status = $this->get_status();
-    	switch($status)
-    	{ 		
-    		case self :: STATUS_REJECTED : 
-    			return Translation :: get('Rejected');   			
-    			break;
-    		case self :: STATUS_PROCESSING : 
-    			return Translation :: get('Processing');
-    			break;
-    		case self :: STATUS_FAILED : 
-    			return Translation :: get('Failed');
-    			break;
-    		case self :: STATUS_AVAILABLE :
-    			return Translation :: get('Available');
-    			break; 
-    		default : return Translation :: get('Unknown');
-    	}
+        $status = $this->get_status();
+        switch ($status)
+        {
+            case self :: STATUS_REJECTED :
+                return Translation :: get('Rejected');
+                break;
+            case self :: STATUS_PROCESSING :
+                return Translation :: get('Processing');
+                break;
+            case self :: STATUS_FAILED :
+                return Translation :: get('Failed');
+                break;
+            case self :: STATUS_AVAILABLE :
+                return Translation :: get('Available');
+                break;
+            default :
+                return Translation :: get('Unknown');
+        }
     }
 }
 ?>
