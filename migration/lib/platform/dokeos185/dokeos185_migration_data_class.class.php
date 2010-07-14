394e6c093b0005c22b91fcf4574a138b82ede81a
vanpouckesven <devnull@localhost>
2010-07-14 08:42:47 +0200

    The migration fun continues Added a file logger and a timer class Finished the migration wizard Started working on the migration block class

---
+++ migration/lib/platform/dokeos185/dokeos185_migration_data_class.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * $Id: import.class.php 221 2009-11-13 14:36:41Z vanpouckesven $
+ * @package migration.lib.migration_manager
+ */
+
+/**
+ * Abstract import class
+ * @author Sven Vanpoucke
+ */
+abstract class Dokeos185MigrationDataClass extends MigrationDataClass
+{
+	const PLATFORM = 'dokeos185';
+    
+    /**
+     * Factory to retrieve the correct class of an old system
+     * @param string $old_system the old system
+     * @param string $type the class type
+     */
+	
+    static function factory($type)
+    {
+        return parent :: factory(self :: PLATFORM, $type);
+    }
+    
+}
+
+?>
