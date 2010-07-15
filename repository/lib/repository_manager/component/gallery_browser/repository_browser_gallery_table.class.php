c455a02356993a57a3154157b261435f520186a3
scaramanga <devnull@localhost>
2010-07-15 12:53:49 +0200

    Adding a repository browser gallery table view.

---
+++ repository/lib/repository_manager/component/gallery_browser/repository_browser_gallery_table.class.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * $Id: repository_browser_gallery_table.class.php 204 2009-11-13 12:51:30Z kariboe $
+ * @package repository.lib.repository_manager.component.browser
+ */
+require_once dirname(__FILE__) . '/repository_browser_gallery_table_data_provider.class.php';
+require_once dirname(__FILE__) . '/repository_browser_gallery_table_column_model.class.php';
+require_once dirname(__FILE__) . '/repository_browser_gallery_table_cell_renderer.class.php';
+/**
+ * Table to display a set of learning objects.
+ */
+class RepositoryBrowserGalleryTable extends ObjectTable
+{
+    const DEFAULT_NAME = 'repository_browser_gallery_table';
+
+    /**
+     * Constructor
+     * @see ContentObjectTable::ContentObjectTable()
+     */
+    function RepositoryBrowserGalleryTable($browser, $parameters, $condition)
+    {
+        $model = new RepositoryBrowserGalleryTableColumnModel();
+        $renderer = new RepositoryBrowserGalleryTableCellRenderer($browser);
+        $data_provider = new RepositoryBrowserGalleryTableDataProvider($browser, $condition);
+        parent :: __construct($data_provider, RepositoryBrowserGalleryTable :: DEFAULT_NAME, $model, $renderer);
+
+        $action = new ObjectTableFormActions();
+
+        if (get_class($browser) == 'RepositoryManagerBrowserComponent')
+        {
+            $action->add_form_action(new ObjectTableFormAction(RepositoryManager :: ACTION_RECYCLE_CONTENT_OBJECTS, Translation :: get('RemoveSelected')));
+            $action->add_form_action(new ObjectTableFormAction(RepositoryManager :: ACTION_MOVE_CONTENT_OBJECTS, Translation :: get('MoveSelected'), false));
+            $action->add_form_action(new ObjectTableFormAction(RepositoryManager :: ACTION_PUBLISH_CONTENT_OBJECT, Translation :: get('PublishSelected'), false));
+            $action->add_form_action(new ObjectTableFormAction(RepositoryManager :: ACTION_EXPORT_CONTENT_OBJECTS, Translation :: get('ExportSelected'), false));
+            $action->add_form_action(new ObjectTableFormAction(RepositoryManager :: ACTION_EDIT_CONTENT_OBJECT_RIGHTS, Translation :: get('EditSelectedRights'), false));
+
+            if ($browser->get_user()->is_platform_admin())
+            {
+                $action->add_form_action(new ObjectTableFormAction(RepositoryManager :: ACTION_COPY_CONTENT_OBJECT_TO_TEMPLATES, Translation :: get('CopySelectedToTemplates'), false));
+            }
+
+        }
+        if (get_class($browser) == 'RepositoryManagerComplexBrowserComponent')
+        {
+            $action->add_form_action(new ObjectTableFormAction(RepositoryManager :: PARAM_ADD_OBJECTS, Translation :: get('AddObjects'), false));
+        }
+
+        $this->set_additional_parameters($parameters);
+        $this->set_form_actions($action);
+        $this->set_default_row_count(20);
+    }
+
+    static function handle_table_action()
+    {
+        $ids = self :: get_selected_ids(Utilities :: camelcase_to_underscores(__CLASS__));
+        Request :: set_get(RepositoryManager :: PARAM_CONTENT_OBJECT_ID, $ids);
+    }
+}
+?>
