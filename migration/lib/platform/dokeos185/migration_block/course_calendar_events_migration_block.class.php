404ef236718fd3f269ea5b0d50269e79bd4f1b51
pieterjan.broekaert <devnull@localhost>
2010-08-17 10:41:38 +0200

    Migration: Announcement

--- migration/lib/platform/dokeos185/migration_block/course_calendar_events_migration_block.class.php
+++ migration/lib/platform/dokeos185/migration_block/course_calendar_events_migration_block.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__FILE__) . 'course_groups_migration_block.class.php';
+require_once dirname(__FILE__) . '/course_groups_migration_block.class.php';
 require_once dirname(__FILE__) . '/../data_class/dokeos185_calendar_event.class.php';
 require_once dirname(__FILE__) . '/../course_data_migration_block.class.php';
 
