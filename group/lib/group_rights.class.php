ba1605122bee338481c12dc8c06e77d5f2204fe7
eduard <devnull@localhost>
2010-08-18 11:36:12 +0200

    added group rights

---
+++ group/lib/group_rights.class.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * $Id: laika_rights.class.php 196 2009-11-13 12:19:18Z chellee $
+ * @package application.lib.laika
+ */
+
+
+require_once Path :: get_application_path() . 'lib/group_manager/group_manager.class.php';
+
+class GroupRights
+{
+    const VIEW_RIGHT = '1';
+    const ADD_RIGHT = '2';
+    const EDIT_RIGHT = '3';
+    const DELETE_RIGHT = '4';
+
+    const LOCATION_ANALYZER = 1;
+   
+
+    function get_available_rights()
+    {
+        $reflect = new ReflectionClass('GroupRights');
+
+	    $rights = $reflect->getConstants();
+
+	    foreach($rights as $key => $right)
+		{
+			if(substr(strtolower($key), 0, 8) == 'location')
+			{
+				unset($rights[$key]);
+			}
+		}
+
+	    return $rights;
+    }
+
+    function is_allowed($right, $location, $type)
+    {
+        return RightsUtilities :: is_allowed($right, $location, $type, GroupManager :: APPLICATION_NAME);
+    }
+}
+?>
