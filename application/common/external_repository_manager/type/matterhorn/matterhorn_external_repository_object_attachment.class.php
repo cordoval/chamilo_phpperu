ada6d56418554ea2f2409442a76a6e5c3153f760
scaramanga <devnull@localhost>
2010-09-01 17:38:27 +0200

    Adding some missing (temporary) images.

--- application/common/external_repository_manager/type/matterhorn/matterhorn_external_repository_object_attachment.class.php
+++ application/common/external_repository_manager/type/matterhorn/matterhorn_external_repository_object_attachment.class.php
@@ -1,109 +1,122 @@
 <?php
 class MatterhornExternalRepositoryObjectAttachment
 {
-	private $ref;
-	private $type;
-	private $id;
-	private $mimetype;
-	private $tags;
-	private $url;
-	/**
-	 * @return the $ref
-	 */
-	public function get_ref() {
-		return $this->ref;
-	}
+    private $ref;
+    private $type;
+    private $id;
+    private $mimetype;
+    private $tags;
+    private $url;
 
-	/**
-	 * @return the $type
-	 */
-	public function get_type() {
-		return $this->type;
-	}
+    /**
+     * @return the $ref
+     */
+    public function get_ref()
+    {
+        return $this->ref;
+    }
 
-	/**
-	 * @return the $id
-	 */
-	public function get_id() {
-		return $this->id;
-	}
+    /**
+     * @return the $type
+     */
+    public function get_type()
+    {
+        return $this->type;
+    }
 
-	/**
-	 * @return the $mimetype
-	 */
-	public function get_mimetype() {
-		return $this->mimetype;
-	}
+    /**
+     * @return the $id
+     */
+    public function get_id()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @return the $tags
-	 */
-	public function get_tags() {
-		return $this->tags;
-	}
+    /**
+     * @return the $mimetype
+     */
+    public function get_mimetype()
+    {
+        return $this->mimetype;
+    }
 
-	/**
-	 * @return the $url
-	 */
-	public function get_url() {
-		return $this->url;
-	}
+    /**
+     * @return the $tags
+     */
+    public function get_tags()
+    {
+        return $this->tags;
+    }
 
-	/**
-	 * @param $ref the $ref to set
-	 */
-	public function set_ref($ref) {
-		$this->ref = $ref;
-	}
+    /**
+     * @return the $url
+     */
+    public function get_url()
+    {
+        return $this->url;
+    }
 
-	/**
-	 * @param $type the $type to set
-	 */
-	public function set_type($type) {
-		$this->type = $type;
-	}
+    /**
+     * @param $ref the $ref to set
+     */
+    public function set_ref($ref)
+    {
+        $this->ref = $ref;
+    }
 
-	/**
-	 * @param $id the $id to set
-	 */
-	public function set_id($id) {
-		$this->id = $id;
-	}
+    /**
+     * @param $type the $type to set
+     */
+    public function set_type($type)
+    {
+        $this->type = $type;
+    }
 
-	/**
-	 * @param $mimetype the $mimetype to set
-	 */
-	public function set_mimetype($mimetype) {
-		$this->mimetype = $mimetype;
-	}
+    /**
+     * @param $id the $id to set
+     */
+    public function set_id($id)
+    {
+        $this->id = $id;
+    }
 
-	/**
-	 * @param $tags the $tags to set
-	 */
-	public function set_tags($tags) {
-		$this->tags = $tags;
-	}
+    /**
+     * @param $mimetype the $mimetype to set
+     */
+    public function set_mimetype($mimetype)
+    {
+        $this->mimetype = $mimetype;
+    }
 
-	/**
-	 * @param $url the $url to set
-	 */
-	public function set_url($url) {
-		$this->url = $url;
-	}
+    /**
+     * @param $tags the $tags to set
+     */
+    public function set_tags($tags)
+    {
+        $this->tags = $tags;
+    }
 
-	private function get_type_as_image()
-	{
-		$result = str_replace('/', '_', $this->get_type());
-		$result = str_replace('+', '_', $result);
-		return Theme :: get_common_image('external_repository/matterhorn/attachments/' . $result);
-		}
-	
+    /**
+     * @param $url the $url to set
+     */
+    public function set_url($url)
+    {
+        $this->url = $url;
+    }
+
+    private function get_type_as_image()
+    {
+        $result = str_replace('/', '_', $this->get_type());
+        $result = str_replace('+', '_', $result);
+        return Theme :: get_common_image('external_repository/matterhorn/attachments/' . $result, 'png', $this->get_type(), '', ToolbarItem :: DISPLAY_ICON);
+    }
+
     public function as_string()
     {
-    	$html = array();
-    	
-    	$html[] = Utilities::mimetype_to_image($this->get_mimetype());
-    	$html[] = $this->get_type_as_image();
-    	return implode("", $html);
+        $html = array();
+
+        $html[] = Utilities :: mimetype_to_image($this->get_mimetype());
+        $html[] = $this->get_type_as_image();
+        return implode(" ", $html);
     }
 }
