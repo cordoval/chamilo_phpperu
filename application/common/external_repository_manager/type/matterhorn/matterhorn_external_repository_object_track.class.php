69c26b75461c140e0d5ba9fa2ec53edc9ba221cf
scaramanga <devnull@localhost>
2010-09-01 17:42:33 +0200

    Adding some missing translations.

--- application/common/external_repository_manager/type/matterhorn/matterhorn_external_repository_object_track.class.php
+++ application/common/external_repository_manager/type/matterhorn/matterhorn_external_repository_object_track.class.php
@@ -4,174 +4,192 @@ class MatterhornExternalRepositoryObjectTrack
     private $id;
     private $ref;
     private $type;
-	private $mimetype;
+    private $mimetype;
     private $tags;
-   	private $url;
+    private $url;
     private $checksum;
     private $duration;
-    private $audio; 
+    private $audio;
     private $video;
-	
-	
-	/**
-	 * @return the $mimetype
-	 */
-	/**
-	 * @return the $id
-	 */
-	public function get_id() {
-		return $this->id;
-	}
 
-	/**
-	 * @return the $ref
-	 */
-	public function get_ref() {
-		return $this->ref;
-	}
+    /**
+     * @return the $mimetype
+     */
+    /**
+     * @return the $id
+     */
+    public function get_id()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @return the $type
-	 */
-	public function get_type() {
-		return $this->type;
-	}
+    /**
+     * @return the $ref
+     */
+    public function get_ref()
+    {
+        return $this->ref;
+    }
 
-	/**
-	 * @param $id the $id to set
-	 */
-	public function set_id($id) {
-		$this->id = $id;
-	}
+    /**
+     * @return the $type
+     */
+    public function get_type()
+    {
+        return $this->type;
+    }
 
-	/**
-	 * @param $ref the $ref to set
-	 */
-	public function set_ref($ref) {
-		$this->ref = $ref;
-	}
+    /**
+     * @param $id the $id to set
+     */
+    public function set_id($id)
+    {
+        $this->id = $id;
+    }
 
-	/**
-	 * @param $type the $type to set
-	 */
-	public function set_type($type) {
-		$this->type = $type;
-	}
+    /**
+     * @param $ref the $ref to set
+     */
+    public function set_ref($ref)
+    {
+        $this->ref = $ref;
+    }
 
-	public function get_mimetype() {
-		return $this->mimetype;
-	}
+    /**
+     * @param $type the $type to set
+     */
+    public function set_type($type)
+    {
+        $this->type = $type;
+    }
 
-	/**
-	 * @return the $tags
-	 */
-	public function get_tags() {
-		return $this->tags;
-	}
+    public function get_mimetype()
+    {
+        return $this->mimetype;
+    }
 
-	/**
-	 * @return the $url
-	 */
-	public function get_url() {
-		return $this->url;
-	}
+    /**
+     * @return the $tags
+     */
+    public function get_tags()
+    {
+        return $this->tags;
+    }
 
-	/**
-	 * @return the $checksum
-	 */
-	public function get_checksum() {
-		return $this->checksum;
-	}
+    /**
+     * @return the $url
+     */
+    public function get_url()
+    {
+        return $this->url;
+    }
 
-	/**
-	 * @return the $duration
-	 */
-	public function get_duration() {
-		return $this->duration;
-	}
+    /**
+     * @return the $checksum
+     */
+    public function get_checksum()
+    {
+        return $this->checksum;
+    }
 
-	/**
-	 * @return the $audio
-	 */
-	public function get_audio() {
-		return $this->audio;
-	}
+    /**
+     * @return the $duration
+     */
+    public function get_duration()
+    {
+        return $this->duration;
+    }
 
-	/**
-	 * @return the $video
-	 */
-	public function get_video() {
-		return $this->video;
-	}
+    /**
+     * @return the $audio
+     */
+    public function get_audio()
+    {
+        return $this->audio;
+    }
 
-	/**
-	 * @param $mimetype the $mimetype to set
-	 */
-	public function set_mimetype($mimetype) {
-		$this->mimetype = $mimetype;
-	}
+    /**
+     * @return the $video
+     */
+    public function get_video()
+    {
+        return $this->video;
+    }
 
-	/**
-	 * @param $tags the $tags to set
-	 */
-	public function set_tags($tags) {
-		$this->tags = $tags;
-	}
+    /**
+     * @param $mimetype the $mimetype to set
+     */
+    public function set_mimetype($mimetype)
+    {
+        $this->mimetype = $mimetype;
+    }
 
-	/**
-	 * @param $url the $url to set
-	 */
-	public function set_url($url) {
-		$this->url = $url;
-	}
+    /**
+     * @param $tags the $tags to set
+     */
+    public function set_tags($tags)
+    {
+        $this->tags = $tags;
+    }
 
-	/**
-	 * @param $checksum the $checksum to set
-	 */
-	public function set_checksum($checksum) {
-		$this->checksum = $checksum;
-	}
+    /**
+     * @param $url the $url to set
+     */
+    public function set_url($url)
+    {
+        $this->url = $url;
+    }
 
-	/**
-	 * @param $duration the $duration to set
-	 */
-	public function set_duration($duration) {
-		$this->duration = $duration;
-	}
+    /**
+     * @param $checksum the $checksum to set
+     */
+    public function set_checksum($checksum)
+    {
+        $this->checksum = $checksum;
+    }
 
-	/**
-	 * @param $audio the $audio to set
-	 */
-	public function set_audio($audio) {
-		$this->audio = $audio;
-	}
+    /**
+     * @param $duration the $duration to set
+     */
+    public function set_duration($duration)
+    {
+        $this->duration = $duration;
+    }
 
-	/**
-	 * @param $video the $video to set
-	 */
-	public function set_video($video) {
-		$this->video = $video;
-	}
+    /**
+     * @param $audio the $audio to set
+     */
+    public function set_audio($audio)
+    {
+        $this->audio = $audio;
+    }
+
+    /**
+     * @param $video the $video to set
+     */
+    public function set_video($video)
+    {
+        $this->video = $video;
+    }
 
     public function as_string()
     {
-    	$html = array();
-    	
-    	$html[] = '<table class="no_border"><tr><td>';
-    	$html[] = Utilities::mimetype_to_image($this->get_mimetype());
-    	$html[] = '</td><td>' ;
-    	if ($this->get_video())
-    	{
-    		$html[] = '<b>' . Translation :: get('Video') .'</b> ' . $this->get_video()->as_string();
-    		$html[] = '<br/>';
-    	}
-    	if ($this->get_audio())
-    	{
-    		$html[] = '<b>' . Translation :: get('Audio') .'</b> '. $this->get_audio()->as_string();
-    	}
-    	$html[] = '</td></tr></table>';
-    	return implode("", $html);
+        $html = array();
+
+        $html[] = '<table class="no_border"><tr><td style="width: 22px;">';
+        $html[] = Utilities :: mimetype_to_image($this->get_mimetype());
+        $html[] = '</td><td>';
+        if ($this->get_video())
+        {
+            $html[] = '<b>' . Translation :: get('Video') . ':</b> ' . $this->get_video()->as_string();
+            $html[] = '<br/>';
+        }
+        if ($this->get_audio())
+        {
+            $html[] = '<b>' . Translation :: get('Audio') . ':</b> ' . $this->get_audio()->as_string();
+        }
+        $html[] = '</td></tr></table>';
+        return implode("", $html);
     }
-    
+
 }
-    
