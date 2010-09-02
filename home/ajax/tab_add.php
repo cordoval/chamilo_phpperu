a78fb3dd17f891ed18dc044a058c1dba100c05e6
Pieterjan Broekaert <devnull@localhost>
2010-09-02 14:04:35 +0200

    Fixed bug #1922 and #1907
    error when adding new tab or column on homepage

--- home/ajax/tab_add.php
+++ home/ajax/tab_add.php
@@ -45,11 +45,12 @@ if ($user_home_allowed && Authentication :: is_valid())
         $json_result['message'] = Translation :: get('TabColumnNotAdded');
     }
 
+    
     $block = new HomeBlock();
     $block->set_column($column->get_id());
     $block->set_title(Translation :: get('DummyBlock'));
     $block->set_application('repository');
-    $block->set_component('dummy');
+    $block->set_component('linker');
     $block->set_visibility('1');
     $block->set_user($user_id);
     if (! $block->create())
