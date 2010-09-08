bd47d1ad3252bc930992b6c0eb0faffe484289c6
vanpouckesven <devnull@localhost>
2010-09-08 15:11:46 +0200

    Task #1796 Fixed a LARGE bug concerning the nested trees

--- common/database/nested_tree_node.class.php
+++ common/database/nested_tree_node.class.php
@@ -543,7 +543,7 @@ abstract class NestedTreeNode extends DataClass
 
 
 
-            if (!$dm->nested_tree_add_nested_values($this, $previous_visited, 1))
+            if (!$dm->nested_tree_add_nested_values($this, $previous_visited, 1, $this->get_nested_tree_node_condition()))
 //            $func = 'add_' . $this->get_object_name() . '_nested_values';
 //
 //	        if(!method_exists($dm, $func))
@@ -604,7 +604,7 @@ abstract class NestedTreeNode extends DataClass
         		return false;
         }
 
-	    if (!$dm->nested_tree_delete_nested_values($this))
+	    if (!$dm->nested_tree_delete_nested_values($this, $this->get_nested_tree_node_condition()))
 //        $func = 'delete_' . $this->get_object_name() . '_nested_values';
 //
 //    	if(!method_exists($dm, $func))
