690a4f5c862a74ab28a337044c6aa7415744813c
Pieterjan Broekaert <devnull@localhost>
2010-09-17 13:19:49 +0200

    Fixed Bug #2019
    a user can not take an assessment in a course.
    no green arrow visible.

--- application/lib/weblcms/tool/assessment/component/assessment_browser/assessment_cell_renderer.class.php
+++ application/lib/weblcms/tool/assessment/component/assessment_browser/assessment_cell_renderer.class.php
@@ -1,23 +1,27 @@
 <?php
+
 /**
  * $Id: assessment_cell_renderer.class.php 216 2009-11-13 14:08:06Z kariboe $
  * @package application.lib.weblcms.tool.assessment.component.assessment_browser
  */
 require_once dirname(__FILE__) . '/../../../../browser/object_publication_table/object_publication_table_cell_renderer.class.php';
+
 /**
  * This class is a cell renderer for a publication candidate table
  */
 class AssessmentCellRenderer extends ObjectPublicationTableCellRenderer
 {
-
     /*
-	 * Inherited
-	 */
+     * Inherited
+     */
+
     function render_cell($column, $publication)
     {
         if ($column === ObjectPublicationTableColumnModel :: get_action_column())
         {
-            return $this->get_actions($publication)->as_html();
+            $actions = $this->get_actions($publication);
+            $actions->prepend_item(new ToolbarItem(Translation :: get('TakeAssessment'), Theme :: get_common_image_path() . 'action_next.png', $this->table_renderer->get_url(array(Tool :: PARAM_ACTION => AssessmentTool :: ACTION_TAKE_ASSESSMENT, Tool :: PARAM_PUBLICATION_ID => $publication->get_id())), ToolbarItem :: DISPLAY_ICON));
+            return $actions->as_html();
         }
 
         switch ($column->get_name())
@@ -36,5 +40,7 @@ class AssessmentCellRenderer extends ObjectPublicationTableCellRenderer
 
         return parent :: render_cell($column, $publication);
     }
+
 }
+
 ?>
