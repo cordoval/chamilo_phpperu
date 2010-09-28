0a357bd17686a2a7bf96c96564cc86e564b90341
vanpouckesven <devnull@localhost>
2010-09-28 10:00:19 +0200

    Task #1972 Fixed the assessments mark results

--- repository/lib/content_object/assessment/display/component/viewer/wizard/inc/question_result_display/fill_in_blanks_question_result_display.class.php
+++ repository/lib/content_object/assessment/display/component/viewer/wizard/inc/question_result_display/fill_in_blanks_question_result_display.class.php
@@ -8,8 +8,8 @@ require_once dirname(__FILE__) . '/../question_result_display.class.php';
 class FillInBlanksQuestionResultDisplay extends QuestionResultDisplay
 {
 
-    function display_question_result(){
-        
+    function display_question_result()
+    {
         $answers = $this->get_answers();
     
         $answer_text = $this->get_question()->get_answer_text();
@@ -19,25 +19,43 @@ class FillInBlanksQuestionResultDisplay extends QuestionResultDisplay
         $html[] = '<div class="with_borders">';
         $html[] = array_shift($parts);
         $index = 0;
-        foreach($parts as $i => $part){
+        foreach($parts as $i => $part)
+        {
         	$answers[$i] = empty($answers[$i]) ? Translation :: get('NoAnswer') : $answers[$i];
-        	if($this->get_question()->is_correct($i, $answers[$i])){
+        	
+        	$weight = $this->get_question()->get_weight_from_answer($i, $answers[$i]);
+        	$max_question_weight = $this->get_question()->get_question_maximum_weight($i); 
+        	
+        	if($weight == $max_question_weight)
+        	{
         		$html[] = '<span style="color:green"><b>'.$answers[$i].'</b></span>';
-        	}else{
-        		$html[] = '<span style="color:red"><b>'.$answers[$i].'</b></span>';
+        	}else
+        	{
+        		if($weight == 0)
+        		{
+        			$html[] = '<span style="color:red"><b>'.$answers[$i].'</b></span>';
+        		}
+        		else
+        		{
+        			$html[] = '<span style="color:orange"><b>'.$answers[$i].'</b></span>';
+        		}
         	}
+        	
         	$html[] = $part;
         	$index++;
         }
+        
         $html[] = '</div>';
         
-        foreach($parts as $index => $part){
+        foreach($parts as $index => $part)
+        {
         	$html[] = $this->get_question_feedback($index);
         }
         echo implode("\n", $html);
     }
     
-    function get_question_feedback($index){
+    function get_question_feedback($index)
+    {
     	$html[] = '<div class="with_borders"><b>'.Translation::get('Question').' '. ($index+1).'</b></div>';
         $html[] = '<table style="border-top: 1px solid #B5CAE7;" class="data_table take_assessment">';
         $html[] = '<thead>';
@@ -45,17 +63,20 @@ class FillInBlanksQuestionResultDisplay extends QuestionResultDisplay
         $html[] = '<th class="list checkbox">#</th>';
         $html[] = '<th class="list">' . Translation :: get('Answer') . '</th>';
         $html[] = '<th class="list">' . Translation :: get('Feedback') . '</th>';
+        $html[] = '<th class="list">' . Translation :: get('Score') . '</th>';
         $html[] = '</tr>';
         $html[] = '</thead>';
         $html[] = '<tbody>';
         
         $i=0;
         $correct_answers = $this->get_question_answer($index);
-        foreach ($correct_answers as $correct_answer){
+        foreach ($correct_answers as $correct_answer)
+        {
         	$html[] = '<tr class="' . ($i % 2 == 0 ? 'row_even' : 'row_odd') . '">';
             $html[] = '<td>' . ($i + 1) . '</td>';
             $html[] = '<td>' . $correct_answer->get_value() . '</td>';
-            $html[] = '<td>' . $correct_answer->get_comment() . '</td></tr>';
+            $html[] = '<td>' . $correct_answer->get_comment() . '</td>';
+             $html[] = '<td>' . $correct_answer->get_weight() . '</td></tr>';
             $i++;
         }
         
@@ -65,11 +86,14 @@ class FillInBlanksQuestionResultDisplay extends QuestionResultDisplay
         return implode("\n", $html);
     }
     
-    function get_question_answer($index){
+    function get_question_answer($index)
+    {
     	$result = array();
         $answers = $this->get_question()->get_answers();
-        foreach($answers as $answer){
-        	if($answer->get_position()==$index){
+        foreach($answers as $answer)
+        {
+        	if($answer->get_position()==$index)
+        	{
         		$result[] = $answer;
         	}
         }
