927d9d8b9e97617085ff4b2817da61c21f4795c0
Pieterjan Broekaert <devnull@localhost>
2010-10-01 09:47:14 +0200

    Repository content object share components

---
+++ repository/lib/repository_manager/component/content_object_share_rights_editor.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of content_object_share_browser
+ *
+ * @author Pieterjan Broekaert
+ */
+class RepositoryManagerContentObjectShareRightsEditorComponent extends RepositoryManager
+{
+
+    /**
+     * Runs this component and displays its output.
+     */
+    function run()
+    {
+        $ids = Request :: get(RepositoryManager :: PARAM_CONTENT_OBJECT_ID);
+        
+    }
+
+}
+
+?>
