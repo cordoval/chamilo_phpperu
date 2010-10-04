e5d7106dd98ab0fd7d8f369f78fccb4ec39dd884
nblocry <nblocry@nathalie-laptop>
2010-10-04 10:20:45 +0200

    handbook-item Content object

---
+++ repository/lib/content_object/handbook_item/complex_handbook_item_form.class.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * $Id: complex_handbook_item_form.class.php 200 2009-11-13 12:30:04Z kariboe $
+ * @package repository.lib.content_object.handbook_item
+ */
+require_once dirname(__FILE__) . '/complex_handbook.class.php';
+
+class ComplexHandbookItemForm extends ComplexContentObjectItemForm
+{
+}
+?>
---
+++ repository/lib/content_object/handbook_item/handbook_item_display.class.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * $Id: handbook_item_display.class.php 200 2009-11-13 12:30:04Z kariboe $
+ * @package repository.lib.content_object.handbook_item
+ */
+class HandbookItemDisplay extends ContentObjectDisplay
+{
+}
+?>
