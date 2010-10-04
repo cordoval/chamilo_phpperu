59faeb8e525c880b8e4508547fd54283eeae5dc1
nblocry <nblocry@nathalie-laptop>
2010-10-04 10:33:01 +0200

    merge

--- repository/lib/content_object/handbook/complex_handbook.class.php
+++ repository/lib/content_object/handbook/complex_handbook.class.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * $Id: complex_handbook.class.php 200 2009-11-13 12:30:04Z kariboe $
- * @package repository.lib.content_object.handbook
- */
+
 
 class ComplexHandbook extends ComplexContentObjectItem
 {
