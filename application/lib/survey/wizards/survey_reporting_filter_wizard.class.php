4c8c1093f2350fe766832d8891b7f599af9b8b28
David Hosse <devnull@localhost>
2010-10-08 16:08:19 +0200

    Survey Reporting Filter merges data of multiple number of surveys, started with creating new additional reporting filter templates and blocks

--- application/lib/survey/wizards/survey_reporting_filter_wizard.class.php
+++ application/lib/survey/wizards/survey_reporting_filter_wizard.class.php
@@ -5,29 +5,84 @@ class SurveyReportingFilterWizard extends WizardPageValidator
 {
 	private $tabs_generator;
 	private $tabs;
+	private $parameters;
 	
 	const CONTEXT_TEMPLATES_TAB = 'context_templates';
 	const CONTEXTS_TAB = 'contexts';
 	const GROUPS_TAB = 'groups'; 
 	const USERS_TAB = 'users';
 	
-	function SurveyReportingFilterWizard($selected_survey_ids, $actions)
+	const PARAM_CONTEXTS = 'contexts';
+	const PARAM_GROUPS = 'groups';
+	const PARAM_USERS = 'users'; 
+	
+	const TYPE_ALL = 0;
+	const TYPE_CONTEXTS = 1;
+	const TYPE_GROUPS = 2;
+	const TYPE_USERS = 3;
+	const TYPE_CONTEXTS_GROUPS = 4;
+	const TYPE_CONTEXTS_USERS = 5;
+	const TYPE_GROUPS_USERS = 6;
+	
+	function SurveyReportingFilterWizard($type,$selected_survey_ids, $actions)
     {  
     	parent :: __construct('survey_reporting_filter', 'post', $actions);
     	
     	$this->tabs_generator = new DynamicFormTabsRenderer($this->getAttribute('name'), $this);
     	
 		$this->tabs = array();
-		$this->tabs[0]= new DynamicFormTab(SurveyReportingFilterWizard::CONTEXT_TEMPLATES_TAB, Translation :: get('ContextTemplates'), null, 'build_context_templates_form');
-		$this->tabs[1]= new DynamicFormTab(SurveyReportingFilterWizard::CONTEXTS_TAB, Translation :: get('Contexts'), null, 'build_contexts_form');
-		$this->tabs[2]= new DynamicFormTab(SurveyReportingFilterWizard::GROUPS_TAB, Translation :: get('Groups'), null, 'build_groups_form');
-		$this->tabs[3]= new DynamicFormTab(SurveyReportingFilterWizard::USERS_TAB, Translation :: get('Users'), null, 'build_users_form');
 		
-		$this->tabs_generator->add_tab($this->tabs[0]);
-		$this->tabs_generator->add_tab($this->tabs[1]);
-		$this->tabs_generator->add_tab($this->tabs[2]);
-       	$this->tabs_generator->add_tab($this->tabs[3]); 	     
-    	
+		switch($type)
+		{
+			case 0: 
+				/*$this->tabs[0]= new DynamicFormTab(SurveyReportingFilterWizard::CONTEXT_TEMPLATES_TAB, Translation :: get('ContextTemplates'), null, 'build_context_templates_form');
+				$this->tabs[1]= new DynamicFormTab(SurveyReportingFilterWizard::CONTEXTS_TAB, Translation :: get('Contexts'), null, 'build_contexts_form');
+				$this->tabs[2]= new DynamicFormTab(SurveyReportingFilterWizard::GROUPS_TAB, Translation :: get('Groups'), null, 'build_groups_form');
+				$this->tabs[3]= new DynamicFormTab(SurveyReportingFilterWizard::USERS_TAB, Translation :: get('Users'), null, 'build_users_form');
+			
+				$this->tabs_generator->add_tab($this->tabs[0]);
+				$this->tabs_generator->add_tab($this->tabs[1]);
+				$this->tabs_generator->add_tab($this->tabs[2]);
+	       		$this->tabs_generator->add_tab($this->tabs[3]); */	     
+				$this->tabs[0]= new DynamicFormTab(SurveyReportingFilterWizard::CONTEXTS_TAB, Translation :: get('Contexts'), null, 'build_contexts_form');
+				$this->tabs[1]= new DynamicFormTab(SurveyReportingFilterWizard::GROUPS_TAB, Translation :: get('Groups'), null, 'build_groups_form');
+				$this->tabs[2]= new DynamicFormTab(SurveyReportingFilterWizard::USERS_TAB, Translation :: get('Users'), null, 'build_users_form');
+				$this->tabs_generator->add_tab($this->tabs[0]);
+				$this->tabs_generator->add_tab($this->tabs[1]);
+				$this->tabs_generator->add_tab($this->tabs[2]);
+    			break;
+			case 1:
+				$this->tabs[0]= new DynamicFormTab(SurveyReportingFilterWizard::CONTEXTS_TAB, Translation :: get('Contexts'), null, 'build_contexts_form');
+				$this->tabs_generator->add_tab($this->tabs[0]);
+				break;
+			case 2:
+				$this->tabs[1]= new DynamicFormTab(SurveyReportingFilterWizard::GROUPS_TAB, Translation :: get('Groups'), null, 'build_groups_form');
+				$this->tabs_generator->add_tab($this->tabs[1]);
+				break;
+			case 3:
+				$this->tabs[2]= new DynamicFormTab(SurveyReportingFilterWizard::USERS_TAB, Translation :: get('Users'), null, 'build_users_form');
+				$this->tabs_generator->add_tab($this->tabs[2]);
+				break;
+			case 4:
+				$this->tabs[0]= new DynamicFormTab(SurveyReportingFilterWizard::CONTEXTS_TAB, Translation :: get('Contexts'), null, 'build_contexts_form');
+				$this->tabs[1]= new DynamicFormTab(SurveyReportingFilterWizard::GROUPS_TAB, Translation :: get('Groups'), null, 'build_groups_form');
+				$this->tabs_generator->add_tab($this->tabs[0]);
+				$this->tabs_generator->add_tab($this->tabs[1]);
+				break;
+			case 5:
+				$this->tabs[0]= new DynamicFormTab(SurveyReportingFilterWizard::CONTEXTS_TAB, Translation :: get('Contexts'), null, 'build_contexts_form');
+				$this->tabs[2]= new DynamicFormTab(SurveyReportingFilterWizard::USERS_TAB, Translation :: get('Users'), null, 'build_users_form');
+				$this->tabs_generator->add_tab($this->tabs[0]);
+				$this->tabs_generator->add_tab($this->tabs[2]);
+				break;
+			case 6:
+				$this->tabs[1]= new DynamicFormTab(SurveyReportingFilterWizard::GROUPS_TAB, Translation :: get('Groups'), null, 'build_groups_form');
+				$this->tabs[2]= new DynamicFormTab(SurveyReportingFilterWizard::USERS_TAB, Translation :: get('Users'), null, 'build_users_form');
+				$this->tabs_generator->add_tab($this->tabs[1]);
+				$this->tabs_generator->add_tab($this->tabs[2]);
+    			break;
+		}
+    		
        	$this->tabs_generator->render();
 		
     }
@@ -55,7 +110,7 @@ class SurveyReportingFilterWizard extends WizardPageValidator
     	$multi_select->setAttribute("style","width:auto; min-width:200px;");
     	$multi_select->setAttribute("class","normal button");
     	$buttons = array();
-        $buttons[] = $this->createElement('style_submit_button', 'submit', Translation :: get('Filter'), array('class' => 'positive'),SurveyReportingFilterWizard::CONTEXT_TEMPLATES_TAB);
+        $buttons[] = $this->createElement('style_submit_button', 'submit', Translation :: get('Filter'), array('class' => 'positive'),self::CONTEXT_TEMPLATES_TAB);
 
         $this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
     }
@@ -80,7 +135,7 @@ class SurveyReportingFilterWizard extends WizardPageValidator
       	$this->addElement($element_finder);
       	
         $buttons = array();
-        $buttons[] = $this->createElement('style_submit_button','submit', Translation :: get('Filter'), array('class' => 'positive'),SurveyReportingFilterWizard::CONTEXTS_TAB);
+        $buttons[] = $this->createElement('style_submit_button','submit', Translation :: get('Filter'), array('class' => 'positive'),self::CONTEXTS_TAB);
         $this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
         
     }
@@ -104,7 +159,7 @@ class SurveyReportingFilterWizard extends WizardPageValidator
     	$this->addElement($element_finder);
     	
     	$buttons = array();
-        $buttons[] = $this->createElement('style_submit_button', 'submit', Translation :: get('Filter'), array('class' => 'positive'),SurveyReportingFilterWizard::GROUPS_TAB);
+        $buttons[] = $this->createElement('style_submit_button', 'submit', Translation :: get('Filter'), array('class' => 'positive'),self::GROUPS_TAB);
         $this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
 
   
@@ -129,12 +184,19 @@ class SurveyReportingFilterWizard extends WizardPageValidator
     	$this->addElement($element_finder);
     	
         $buttons = array();
-        $buttons[] = $this->createElement('style_submit_button', 'submit', Translation :: get('Filter'), array('class' => 'positive'),SurveyReportingFilterWizard::USERS_TAB);
+        $buttons[] = $this->createElement('style_submit_button', 'submit', Translation :: get('Filter'), array('class' => 'positive'),self::USERS_TAB);
         
         $this->addGroup($buttons, 'buttons', null, '&nbsp;', false);
     }
     
-    
+    function getParameters()
+    {
+    	$values = $this->exportValues();
+    	$this->parameters[self::PARAM_CONTEXTS] = $values['contexts'];
+    	$this->parameters[self::PARAM_GROUPS] = $values['groups']['group'];
+    	$this->parameters[self::PARAM_USERS] = $values['users']['user'];
+    	return $this->parameters;
+    }
 }
 
 ?>
