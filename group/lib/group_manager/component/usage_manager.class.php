c7528e5d2ecc8d0e31f9b4af43df051cf04d314d
Scaramanga <devnull@localhost>
2010-10-08 20:31:19 +0200

    Adding a filtered group menu.

--- group/lib/group_manager/component/usage_manager.class.php
+++ group/lib/group_manager/component/usage_manager.class.php
@@ -1,6 +1,17 @@
 <?php
+require_once dirname(__FILE__) . '/../../group_usage_tree_menu_data_provider.class.php';
+
 class GroupManagerUsageManagerComponent extends GroupManager implements AdministrationComponent
 {
+    /**
+     * @var integer
+     */
+    private $group;
+    
+    /**
+     * @var Group
+     */
+    private $root_group;
 
     /**
      * Runs this component and displays its output.
@@ -9,7 +20,7 @@ class GroupManagerUsageManagerComponent extends GroupManager implements Administ
     {
         $menu = $this->get_menu_html();
         $output = $this->get_user_html();
-
+        
         $this->display_header();
         echo $menu;
         echo $output;
@@ -18,14 +29,12 @@ class GroupManagerUsageManagerComponent extends GroupManager implements Administ
 
     function get_menu_html()
     {
-        //$group_menu = new GroupMenu($this->get_group());
-        //        $group_menu = new TreeMenu('GroupUsageTreeMenu', new GroupUsageTreeMenuDataProvider($this->get_url(), $this->get_group()));
+        $this->get_user()->set_platformadmin(0);
+        $group_menu = new TreeMenu('GroupUsageTreeMenu', new GroupUsageTreeMenuDataProvider($this->get_user(), $this->get_url(), $this->get_group()));
         $html = array();
         $html[] = '<div style="float: left; width: 18%; overflow: auto; height: 500px;">';
-        //        $html[] = $group_menu->render_as_tree();
-        $html[] = 'MENU GOES HERE';
+        $html[] = $group_menu->render_as_tree();
         $html[] = '</div>';
-
         return implode($html, "\n");
     }
 
@@ -36,8 +45,35 @@ class GroupManagerUsageManagerComponent extends GroupManager implements Administ
         $html[] = 'CONTENT GOES HERE';
         $html[] = '</div>';
         $html[] = '<div class="clear"></div>';
-
+        
         return implode($html, "\n");
+    }
+
+    function get_group()
+    {
+        if (! $this->group)
+        {
+            $this->group = Request :: get(GroupManager :: PARAM_GROUP_ID);
+            
+            if (! $this->group)
+            {
+                $this->group = $this->get_root_group()->get_id();
+            }
+        
+        }
+        
+        return $this->group;
+    }
+
+    function get_root_group()
+    {
+        if (! $this->root_group)
+        {
+            $group = $this->retrieve_groups(new EqualityCondition(Group :: PROPERTY_PARENT, 0))->next_result();
+            $this->root_group = $group;
+        }
+        
+        return $this->root_group;
     }
 }
 ?>
