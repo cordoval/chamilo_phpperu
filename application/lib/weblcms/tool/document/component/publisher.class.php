86450e36f43dc53e70d8906b1e27929d2e645aa9
vanpouckesven <devnull@localhost>
2010-10-13 10:40:17 +0200

    Task #2136 Fixed the publishing of documents thanks to hans and ludwig

--- application/lib/weblcms/tool/document/component/publisher.class.php
+++ application/lib/weblcms/tool/document/component/publisher.class.php
@@ -9,13 +9,6 @@ class DocumentToolPublisherComponent extends DocumentTool
 
     function run()
     {
-        //TODO: change this to real roles and rights
-        $category = $this->get_category(Request :: get(WeblcmsManager :: PARAM_CATEGORY));
-        if ($category && $category->get_name() == 'Dropbox')
-        {
-            $this->get_parent()->set_right(ADD_RIGHT, true);
-        }
-        
         ToolComponent :: launch($this);
     }
 
