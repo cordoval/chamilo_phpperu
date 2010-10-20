95a3cf13491646f01be8c68ee90932c88d23b18a
vanpouckesven <devnull@localhost>
2010-10-20 14:10:23 +0200

    Working on a few scripts to check the integrity of the migration

--- migration/lib/platform/dokeos185/scripts/non_existing_documents_counter.php
+++ migration/lib/platform/dokeos185/scripts/non_existing_documents_counter.php
@@ -9,29 +9,26 @@ mysql_connect($config['database_host'], $config['database_user'], $config['datab
 $query = 'SELECT * FROM ' . $config['main_database'] . '.course;';
 $result = mysql_query($query);
 
-$database_deleted_files = 0;
-$real_deleted_files = 0;
+$non_existing_documents = 0;
 
 while($row = mysql_fetch_assoc($result))
 {
     $course_database = $row['db_name'];
     
-    $query = 'SELECT path FROM ' . $course_database . '.document WHERE path LIKE \'%deleted%\';';
+    $query = 'SELECT path FROM ' . $course_database . '.document WHERE filetype=\'file\'';
     $document_result = mysql_query($query);
     
     while($count_row = mysql_fetch_assoc($document_result))
     {
-        $database_deleted_files++;
-        
         $path = $config['root_path'] . $row['directory'] . '/document/' . $count_row['path'];
         if(!file_exists($path))
         {
             var_dump($path);
-            $real_deleted_files++;
+            $non_existing_documents++;
         }
     }
 }
 
-echo 'Total: database deleted = ' . $database_deleted_files . ' - really deleted = ' . $real_deleted_files;
+echo 'Total: non existing files = ' . $non_existing_documents;
 
 ?>
