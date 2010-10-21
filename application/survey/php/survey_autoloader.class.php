f5f219ec48cfb20980cb0d1e854519057539be65
eduard <devnull@localhost>
2010-10-21 16:42:30 +0200

    added exporter

--- application/survey/php/survey_autoloader.class.php
+++ application/survey/php/survey_autoloader.class.php
@@ -17,31 +17,39 @@ class SurveyAutoloader
 		'survey_publication_mailer_form' => 'survey_publication_mailer_form.class.php',
 		
         'survey_manager' => 'survey_manager/survey_manager.class.php');
-		'survey_manager_browser_component' => 'survey_manager/browser.class.php');
-		'survey_manager_builder_component' => 'survey_manager/builder.class.php');
-		'survey_manager_deleter_component' => 'survey_manager/deleter.class.php');
-		'survey_manager_editor_component' => 'survey_manager/editor.class.php');
-		'survey_manager_invitation_canceler_component' => 'survey_manager/invitation_canceler.class.php');
-		'survey_manager_inviter_component' => 'survey_manager/inviter.class.php');
-		'survey_manager_mailer_component' => 'survey_manager/mailer.class.php');
-		'survey_manager_participant_browser_component' => 'survey_manager/participant_browser.class.php');
-		'survey_manager_publisher_component' => 'survey_manager/publisher.class.php');
-		'survey_manager_reporting_component' => 'survey_manager/reporting.class.php');
-		'survey_manager_rights_editor_component' => 'survey_manager/rights_editor.class.php');
-		'survey_manager_subscribe_group_component' => 'survey_manager/subscribe_group.class.php');
-		'survey_manager_subscribe_user_component' => 'survey_manager/subscribe_user.class.php');
-		'survey_manager_taker_component' => 'survey_manager/taker.class.php');
+		'survey_manager_browser_component' => 'survey_manager/component/browser.class.php');
+		'survey_manager_builder_component' => 'survey_manager/component/builder.class.php');
+		'survey_manager_deleter_component' => 'survey_manager/component/deleter.class.php');
+		'survey_manager_editor_component' => 'survey_manager/component/editor.class.php');
+		'survey_manager_invitation_canceler_component' => 'survey_manager/component/invitation_canceler.class.php');
+		'survey_manager_inviter_component' => 'survey_manager/component/inviter.class.php');
+		'survey_manager_mailer_component' => 'survey_manager/component/mailer.class.php');
+		'survey_manager_participant_browser_component' => 'survey_manager/component/participant_browser.class.php');
+		'survey_manager_publisher_component' => 'survey_manager/component/publisher.class.php');
+		'survey_manager_reporting_component' => 'survey_manager/component/reporting.class.php');
+		'survey_manager_rights_editor_component' => 'survey_manager/component/rights_editor.class.php');
+		'survey_manager_subscribe_group_component' => 'survey_manager/component/subscribe_group.class.php');
+		'survey_manager_subscribe_user_component' => 'survey_manager/component/subscribe_user.class.php');
+		'survey_manager_taker_component' => 'survey_manager/component/taker.class.php');
 		
 		'survey_export_manager' => 'export_manager/export_manager.class.php');
+		'survey_export_manager_survey_excel_exporter_component' => 'survey_export_manager/component/survey_excel_exporter.class.php');
+		'survey_export_manager_survey_excel_median_exporter_component' => 'survey_export_manager/component/survey_excel_exporter.class.php');
+		'survey_export_manager_survey_excel_percentage_exporter_component' => 'survey_export_manager/component/survey_excel_exporter.class.php');
+		'survey_export_manager_survey_spss_excel_exporter_component' => 'survey_export_manager/component/survey_excel_exporter.class.php');
+		'survey_export_manager_survey_spss_syntax_exporter_component' => 'survey_export_manager/component/survey_excel_exporter.class.php');
+		
 		
 		'survey_reporting_manager' => 'reporting_manager/reporting_manager.class.php');
-        'survey_reporting_manager_browser_component' => 'reporting_manager/browser.class.php');
-		'survey_reporting_manager_creator_component' => 'reporting_manager/creator.class.php');
-		'survey_reporting_manager_deleter_component' => 'reporting_manager/deleter.class.php');
-		'survey_reporting_manager_editor_component' => 'reporting_manager/editor.class.php');
-		'survey_reporting_manager_reporting_component' => 'reporting_manager/reporting.class.php');
-		'survey_reporting_manager_rights_editor_component' => 'reporting_manager/rights_editor.class.php');
-		
+        'survey_reporting_manager_browser_component' => 'reporting_manager/component/browser.class.php');
+		'survey_reporting_manager_creator_component' => 'reporting_manager/component/creator.class.php');
+		'survey_reporting_manager_deleter_component' => 'reporting_manager/component/deleter.class.php');
+		'survey_reporting_manager_editor_component' => 'reporting_manager/component/editor.class.php');
+		'survey_reporting_manager_reporting_component' => 'reporting_manager/component/reporting.class.php');
+		'survey_reporting_manager_rights_editor_component' => 'reporting_manager/component/rights_editor.class.php');
+		'survey_publication_rel_reporting_template_table' => 'reporting_manager/component/publication_rel_reporting_template_table/table.class.php');
+		'survey_reporting_template_table' => 'reporting_manager/component/reporting_template_table/table.class.php');
+				
 		
         $lower_case = Utilities :: camelcase_to_underscores($classname);
         
