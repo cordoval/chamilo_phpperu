7fec945cb29d85c13c2d41fd9441dd06f28132ce
Pieterjan Broekaert <devnull@localhost>
2010-10-27 09:21:56 +0200

    refactor: rights editor

--- common/extensions/rights_editor_manager/php/component/group_rights_setter.class.php
+++ common/extensions/rights_editor_manager/php/component/group_rights_setter.class.php
@@ -1,6 +1,10 @@
 <?php
 namespace common\extensions\rights_editor_manager;
 use rights\RightsUtilities;
+use common\libraries\Request;
+use common\libraries\Translation;
+use common\libraries\BreadcrumbTrail;
+use common\libraries\Breadcrumb;
 /**
  * $Id: group_rights_setter.class.php 191 2009-11-13 11:50:28Z chellee $
  * @package application.common.rights_editor_manager.component
