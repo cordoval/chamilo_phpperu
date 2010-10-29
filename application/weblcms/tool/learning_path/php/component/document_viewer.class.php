3a6c8dda39bea46640ec753e18b6cbae6316cc58
Scaramanga <devnull@localhost>
2010-10-29 20:42:59 +0200

    More cleanup / tweaks for the learning path.

--- application/weblcms/tool/learning_path/php/component/document_viewer.class.php
+++ application/weblcms/tool/learning_path/php/component/document_viewer.class.php
@@ -2,6 +2,7 @@
 namespace application\weblcms\tool\learning_path;
 
 use common\libraries\Request;
+use repository\RepositoryDataManager;
 
 /**
  * $Id: learning_path_document_viewer.class.php 216 2009-11-13 14:08:06Z kariboe $
