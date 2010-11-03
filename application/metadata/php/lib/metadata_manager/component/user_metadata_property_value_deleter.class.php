1a6b04b48fead056955a142cd18e6682e8cff8e0
koltempleister <devnull@localhost>
2010-11-03 14:18:30 +0100

    user metadata files added

---
+++ application/metadata/php/lib/metadata_manager/component/user_metadata_property_value_deleter.class.php
@@ -0,0 +1,42 @@
+<?php 
+namespace application\metadata;
+use common\libraries\Translation;
+use common\libraries\Request;
+/**
+ * Component to delete metadata_property_values objects
+ * @author Sven Vanpoucke
+ * @author Jens Vanderheyden
+ */
+class MetadataManagerUserMetadataPropertyValueDeleterComponent extends MetadataManager
+{
+    /**
+     * Runs this component and displays its output.
+     */
+    function run()
+    {
+        $id = $_GET[MetadataManager :: PARAM_METADATA_PROPERTY_VALUE];
+        $fail = false;
+
+        if (!empty ($id))
+        {
+            $metadata_property_value = $this->retrieve_user_metadata_property_value($id);
+
+            if(!$metadata_property_value->delete())
+            {
+                $message = 'SelectedMetadataPropertyValueNotDeleted';
+                $fail = true;
+            }
+            else
+            {
+                $message = 'SelectedMetadataPropertyValueDeleted';
+            }
+
+            $this->redirect(Translation :: get($message), ($fail ? true : false), array(MetadataManager :: PARAM_ACTION => MetadataManager :: ACTION_EDIT_USER_METADATA, MetadataManager :: PARAM_USER => Request :: get(MetadataManager :: PARAM_USER)));
+        }
+        else
+        {
+            $this->display_error_page(htmlentities(Translation :: get('NoMetadataPropertyValuesSelected')));
+        }
+    }
+}
+?>
