38328caed35c01941652687db2797f6dfa23939e
Pieterjan Broekaert <devnull@localhost>
2010-11-04 13:00:29 +0100

    refactor weblcms namespaces

--- application/weblcms/tool/course_sections/php/component/updater.class.php
+++ application/weblcms/tool/course_sections/php/component/updater.class.php
@@ -8,6 +8,7 @@ use common\libraries\BreadcrumbTrail;
 use common\libraries\EqualityCondition;
 use common\libraries\Request;
 use common\libraries\Translation;
+use application\weblcms\Tool;
 
 /**
  * $Id: course_sections_updater.class.php 216 2009-11-13 14:08:06Z kariboe $
