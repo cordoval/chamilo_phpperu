b225002af72aebc0d3cb9efe326e833513bf69c2
vanpouckesven <devnull@localhost>
2010-11-09 14:14:42 +0100

    Changed the file scanning script

--- application/cda/php/lib/util/file_scanner/translations_file_scanner.class.php
+++ application/cda/php/lib/util/file_scanner/translations_file_scanner.class.php
@@ -6,21 +6,40 @@ require_once dirname(__FILE__) . '/file_scanner.class.php';
 
 class TranslationsFileScanner extends FileScanner
 {
+    private $pattern = '/Translation[\s]*::[\s]*get\([\'"]([A-Za-z]*)[\'"](?:,[^,\(\)]*(\((?:[^\(\)]*|(?2))*\))?[^,\)\(]*(?:[,\s]+[\'"]*(.*?)[\'"]*)?)?\)/';
+    private $variable_writer;
+    private $namespace;
+
     function scan_file($file, $contents, $namespace, $variable_writer)
     {
-        $pattern = '/Translation :: get\([\'"](\w+)[\'"][,\s]*(array\(.*?\))*.*?([\'"](.*?)[\'"])?\)/i';
+        $this->variable_writer = $variable_writer;
+        $this->namespace = $namespace;
+
+        //$pattern = '/Translation :: get\([\'"](\w+)[\'"][,\s]*(array\(.*\)|.)*?([,\s]+[\'"]*(.*?)[\'"]*)?\)/i';
+
         $matches = array();
-        preg_match_all($pattern, $contents, $matches);
-dump($matches);
+        preg_match_all($this->pattern, $contents, $matches);
+        $this->check_matches($matches);
+    }
+
+    function check_matches($matches)
+    {
         foreach ($matches[1] as $index => $match)
         {
-            $context = $matches[4][$index];
-            if(!$context || trim($context) != '')
+            $recursive_matches = array();
+            preg_match_all($this->pattern, $matches[2][$index], $recursive_matches);
+            if(count($recursive_matches) > 0)
             {
-                $context = $namespace;
+                $this->check_matches($recursive_matches);
             }
 
-            $variable_writer->handle_variable($match, $context);
+            $context = $matches[3][$index];
+            if(!$context || trim($context) == '')
+            {
+                $context = $this->namespace;
+            }
+
+            $this->variable_writer->handle_variable($match, $context);
         }
     }
 }
