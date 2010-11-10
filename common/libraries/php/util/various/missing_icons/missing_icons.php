2714cc94e764c251cb6f4695795e99ec1d0f22f0
Scaramanga <devnull@localhost>
2010-11-10 13:26:39 +0100

    Adding / fixing some CSS for content objects

--- common/libraries/php/util/various/missing_icons/missing_icons.php
+++ common/libraries/php/util/various/missing_icons/missing_icons.php
@@ -3,15 +3,11 @@ namespace common\libraries;
 
 require_once dirname(__FILE__) . '/../../../../../global.inc.php';
 
-echo '<b>Applications</b><br />';
+Display :: small_header();
+
 require_once dirname(__FILE__) . '/missing_application_icons.php';
-echo '<br /><hr /><br />';
-
-echo '<b>Weblcms Tools</b><br />';
 require_once dirname(__FILE__) . '/missing_weblcms_tool_icons.php';
-echo '<br /><hr /><br />';
-
-echo '<b>Content Objects</b><br />';
 require_once dirname(__FILE__) . '/missing_content_object_icons.php';
-echo '<br /><hr /><br />';
+
+Display :: small_footer();
 ?>
