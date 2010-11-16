42bbf1aa3fac2ec9aff89d4403390f6ed625428d
sven.francornel <devnull@localhost>
2010-11-16 13:46:09 +0100

    external repository, dropbox

---
+++ common/extensions/external_repository_manager/implementation/dropbox/php/component/browser.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace common\extensions\external_repository_manager\implementation\dropbox;
+
+use common\extensions\external_repository_manager\ExternalRepositoryComponent;
+
+class DropboxExternalRepositoryManagerBrowserComponent extends DropboxExternalRepositoryManager
+{
+
+    function run()
+    {        
+        ExternalRepositoryComponent :: launch($this);
+    }
+}
+?>
---
+++ common/extensions/external_repository_manager/implementation/dropbox/php/component/configurer.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace common\extensions\external_repository_manager\implementation\dropbox;
+
+class DropboxExternalRepositoryManagerConfigurerComponent extends DropboxExternalRepositoryManager
+{
+
+    function run()
+    {
+        ExternalRepositoryComponent :: launch($this);
+    }
+}
+?>
