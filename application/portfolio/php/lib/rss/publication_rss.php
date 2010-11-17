b7cba461f6a95e4f0e0d103907728f90fdc2f3e0
Philippe Van Eerdenbrugghe <systho@gmail.com>
2010-11-17 16:55:12 +0100

    fixed portfolio for code coverage

--- application/portfolio/php/lib/rss/publication_rss.php
+++ application/portfolio/php/lib/rss/publication_rss.php
@@ -1,6 +1,7 @@
 <?php
 namespace application\portfolio;
 
+require_once dirname(__FILE__).'/../../../../../common/global.inc.php';
 require_once dirname(__FILE__).'/publication_rss.class.php';
 
 $rss = new PortfolioPublicationRSS();
