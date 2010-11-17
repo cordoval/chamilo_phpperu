3cd4fdbd564c066bd300436dbd6c75f39bc6e758
Philippe Van Eerdenbrugghe <systho@gmail.com>
2010-11-17 16:58:54 +0100

    fixed profiler for code coverage

--- application/profiler/php/lib/rss/publication_rss.php
+++ application/profiler/php/lib/rss/publication_rss.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace application\profiler;
-
+require_once dirname(__FILE__).'/../../../../../../common/global.inc.php';
 use common\libraries\WebApplication;
 
 require_once WebApplication :: get_application_class_lib_path('profiler') . 'rss/publication_rss.class.php';
