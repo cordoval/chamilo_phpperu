75ee38ff8b98582c56e5076ce82899d85250a828
Scaramanga <devnull@localhost>
2010-11-18 11:11:15 +0100

    More general cleanup.

--- repository/content_object/matterhorn/php/matterhorn.class.php
+++ repository/content_object/matterhorn/php/matterhorn.class.php
@@ -11,10 +11,6 @@ use repository\ExternalRepositorySetting;
 
 class Matterhorn extends ContentObject implements Versionable
 {
-    const PROPERTY_MATTERHORN_ID = 'matterhorn_id';
-    const PROPERTY_HEIGHT = 'height';
-    const PROPERTY_WIDTH = 'width';
-    const PROPERTY_THUMBNAIL = 'thumbnail';
     const CLASS_NAME = __CLASS__;
 
     static function get_type_name()
@@ -22,64 +18,17 @@ class Matterhorn extends ContentObject implements Versionable
         return Utilities :: get_classname_from_namespace(self :: CLASS_NAME, true);
     }
 
-    function get_matterhorn_id()
-    {
-        return $this->get_additional_property(self :: PROPERTY_MATTERHORN_ID);
-    }
-
-    function set_matterhorn_id($matterhorn_id)
-    {
-        return $this->set_additional_property(self :: PROPERTY_MATTERHORN_ID, $matterhorn_id);
-    }
-
-    function get_height()
-    {
-        return $this->get_additional_property(self :: PROPERTY_HEIGHT);
-    }
-
-    function set_height($height)
-    {
-        return $this->set_additional_property(self :: PROPERTY_HEIGHT, $height);
-    }
-
-    function get_width()
-    {
-        return $this->get_additional_property(self :: PROPERTY_WIDTH);
-    }
-
-    function set_width($width)
-    {
-        return $this->set_additional_property(self :: PROPERTY_WIDTH, $width);
-    }
-
-    function get_thumbnail()
-    {
-        return $this->get_additional_property(self :: PROPERTY_THUMBNAIL);
-    }
-
-    function set_thumbnail($thumbnail)
-    {
-        return $this->set_additional_property(self :: PROPERTY_THUMBNAIL, $thumbnail);
-    }
-
-    static function get_additional_property_names()
-    {
-        return array(self :: PROPERTY_MATTERHORN_ID, self :: PROPERTY_THUMBNAIL/*, self :: PROPERTY_HEIGHT, self :: PROPERTY_WIDTH*/);
-    }
-
     function get_video_url()
     {
+        $synchronization_data = $this->get_synchronization_data();
+
         $conditions = array();
         $conditions[] = new EqualityCondition(ExternalRepositorySetting :: PROPERTY_VARIABLE, 'url');
-        $conditions[] = new EqualityCondition(ExternalRepositorySetting :: PROPERTY_EXTERNAL_REPOSITORY_ID, $this->get_synchronization_data()->get_external_repository_id());
+        $conditions[] = new EqualityCondition(ExternalRepositorySetting :: PROPERTY_EXTERNAL_REPOSITORY_ID, $synchronization_data->get_external_repository_id());
         $condition = new AndCondition($conditions);
-        $settings = $this->get_data_manager()->retrieve_external_repository_settings($condition);
-        if ($settings->size() == 1)
-        {
-            $settings = $settings->next_result();
-            $url = $settings->get_value();
-        }
-        return $url . '/engage/ui/embed.html?id=' . $this->get_matterhorn_id();
+        $settings = $this->get_data_manager()->retrieve_external_repository_settings($condition)->next_result();
+
+        return $settings->get_value() . '/engage/ui/embed.html?id=' . $synchronization_data->get_external_repository_object_id();
     }
 }
 ?>
