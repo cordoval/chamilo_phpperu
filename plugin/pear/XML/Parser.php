26a87ae296c8d14d74f213f4debc19190d015843
Pieterjan Broekaert <devnull@localhost>
2010-11-19 13:48:12 +0100

    Constructor fixes

--- plugin/pear/XML/Parser.php
+++ plugin/pear/XML/Parser.php
@@ -761,7 +761,7 @@ class XML_Parser_Error extends PEAR_Error
                 xml_get_current_line_number($msgorparser),
                 xml_get_current_column_number($msgorparser));
         }
-        parent :: PEAR_Error($msgorparser, $code, $mode, $level);
+        parent :: __construct($msgorparser, $code, $mode, $level);
     }
     // }}}
 }
