61d39aef9f390a25905366a340a1eca975435655
Scaramanga <devnull@localhost>
2010-11-25 17:11:56 +0100

    Fixing some assessment question type bugs.

--- repository/content_object/assessment_matrix_question/php/assessment_matrix_question_difference_display.class.php
+++ repository/content_object/assessment_matrix_question/php/assessment_matrix_question_difference_display.class.php
@@ -2,6 +2,7 @@
 namespace repository\content_object\assessment_matrix_question;
 
 use common\libraries\Path;
+use repository\MatrixQuestionDifferenceDisplay;
 
 /**
  * $Id: assessment_matrix_question_difference_display.class.php 200 2009-11-13 12:30:04Z kariboe $
--- repository/content_object/assessment_rating_question/php/assessment_rating_question_difference_display.class.php
+++ repository/content_object/assessment_rating_question/php/assessment_rating_question_difference_display.class.php
@@ -2,6 +2,7 @@
 namespace repository\content_object\assessment_rating_question;
 
 use common\libraries\Path;
+use repository\RatingQuestionDifferenceDisplay;
 
 /**
  * $Id: assessment_rating_question_difference_display.class.php 200 2009-11-13 12:30:04Z kariboe $
