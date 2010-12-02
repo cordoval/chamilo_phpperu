282ee6bb05488bf441923b4df0f8c3c2b5fa6197
magali.gillard <devnull@localhost>
2010-12-02 15:37:08 +0100

    bbb video conferencing

--- repository/content_object/bbb_meeting/php/bbb_meeting_display.class.php
+++ repository/content_object/bbb_meeting/php/bbb_meeting_display.class.php
@@ -2,6 +2,7 @@
 namespace repository\content_object\bbb_meeting;
 
 use common\libraries\Text;
+use common\libraries\Session;
 use common\extensions\video_conferencing_manager\VideoConferencingObjectDisplay;
 use repository\ContentObjectDisplay;
 
@@ -19,7 +20,10 @@ class BbbMeetingDisplay extends ContentObjectDisplay
     	$display = VideoConferencingObjectDisplay :: factory($external_sync->get_external_object());
     	$html = array();
     	$html[] = $display->get_properties_table();
-    	$html[] = $display->get_join_button();
+    	if ($object->get_owner_id() == Session :: get_user_id())
+    	{
+    		$html[] = $display->get_join_button();
+    	}   	
     	
     	$description = parent :: get_description();
         $object = $this->get_content_object();
