111142581e719a559e9e41d00c8eb4218d56ffe9
Scaramanga <devnull@localhost>
2010-12-07 16:09:36 +0100

    Some more functionality for adaptive assessments

---
+++ repository/content_object/adaptive_assessment/php/display/preview/dummy_lp_attempt_tracker.class.php
@@ -0,0 +1,25 @@
+<?php
+namespace repository\content_object\adaptive_assessment;
+
+class DummyLpAttemptTracker
+{
+
+    function get_id()
+    {
+        return 1;
+    }
+
+    function get_progress()
+    {
+        return 0;
+    }
+
+    function set_progress($progress)
+    {
+    }
+
+    function update()
+    {
+    }
+}
+?>
