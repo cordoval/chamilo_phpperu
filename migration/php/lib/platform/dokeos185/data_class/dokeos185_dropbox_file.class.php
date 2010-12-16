909918f07699495cd32540974a5995e1f7f6610c
Pieterjan Broekaert <devnull@localhost>
2010-12-16 13:26:10 +0100

    migration fixes

--- migration/php/lib/platform/dokeos185/data_class/dokeos185_dropbox_file.class.php
+++ migration/php/lib/platform/dokeos185/data_class/dokeos185_dropbox_file.class.php
@@ -6,8 +6,7 @@ use common\libraries\Translation;
 use repository\RepositoryDataManager;
 use common\libraries\Utilities;
 use common\libraries\Path;
-use Path;
-use FileSystem;
+use common\libraries\FileSystem;
 
 /**
  * $Id: dokeos185_dropbox_file.class.php 221 2009-11-13 14:36:41Z vanpouckesven $
