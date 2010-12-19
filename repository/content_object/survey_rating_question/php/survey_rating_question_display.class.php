04fa94dc83d2bb0e38908df221560b2567b5f026
hans de bisschop <hans.de.bisschop@ehb.be>
2010-12-19 19:44:57 +0100

    Adding red crayons to rubies.

--- repository/content_object/survey_rating_question/php/survey_rating_question_display.class.php
+++ repository/content_object/survey_rating_question/php/survey_rating_question_display.class.php
@@ -37,7 +37,7 @@ class SurveyRatingQuestionDisplay extends ContentObjectDisplay
         $html[] = '</select>';
         $html[] = '</div>';
         $html[] = '</div>';
-        $html[] = ResourceManager :: get_instance()->get_resource_html(Path :: get(WEB_PATH) . 'common/javascript/rating_question.js');
+        $html[] = ResourceManager :: get_instance()->get_resource_html(Path :: get_repository_content_object_path(true) . 'survey_rating_question/resources/javascript/survey_rating_question.js');
         return implode("\n", $html);
     }
 }
