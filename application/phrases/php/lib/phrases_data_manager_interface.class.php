c73755184ba1beb92ed43500e4f5e3abd368dcc9
eduard vossen <eduard.vossen@gmail.com>
2010-12-22 13:08:15 +0100

    removed obsolete methods for the surveyinvitation

--- application/phrases/php/lib/phrases_data_manager_interface.class.php
+++ application/phrases/php/lib/phrases_data_manager_interface.class.php
@@ -38,18 +38,6 @@ interface PhrasesDataManagerInterface
     function retrieve_phrases_publication_categories($condition = null, $offset = null, $count = null, $order_property = null);
 
     function select_next_phrases_publication_category_display_order($parent);
-
-    function create_survey_invitation($survey_invitation);
-
-    function update_survey_invitation($survey_invitation);
-
-    function delete_survey_invitation($survey_invitation);
-
-    function count_survey_invitations($conditions = null);
-
-    function retrieve_survey_invitation($id);
-
-    function retrieve_survey_invitations($condition = null, $offset = null, $count = null, $order_property = null);
-
+   
 }
 ?>
