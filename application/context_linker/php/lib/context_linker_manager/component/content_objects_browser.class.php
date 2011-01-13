6b5b3bf273b76901ea6cf074e3661daa89457ba1
Nathalie <devnull@localhost>
2011-01-13 14:51:25 +0100

    context linker - make table with co's searchable

--- application/context_linker/php/lib/context_linker_manager/component/content_objects_browser.class.php
+++ application/context_linker/php/lib/context_linker_manager/component/content_objects_browser.class.php
@@ -3,6 +3,13 @@ namespace application\context_linker;
 use common\libraries\Application;
 use common\libraries\EqualityCondition;
 use repository\ContentObject;
+use common\libraries\ActionBarRenderer;
+use common\libraries\ToolbarItem;
+use common\libraries\Translation;
+use common\libraries\Utilities;
+use common\libraries\Theme;
+use common\libraries\PatternMatchCondition;
+use common\libraries\AndCondition;
 require_once dirname(__FILE__) . '/content_object_browser/content_object_browser_table.class.php';
 /**
  * context_linker component which allows the user to browse his context_links
@@ -11,12 +18,17 @@ require_once dirname(__FILE__) . '/content_object_browser/content_object_browser
  */
 class ContextLinkerManagerContentObjectsBrowserComponent extends ContextLinkerManager
 {
+    private $action_bar;
+
 
     function run()
     {
+        $this->action_bar = $this->get_action_bar();
         $this->display_header();
 
+
         $html = array();
+        $html[] = $this->action_bar->as_html();
 
         $html[] = $this->get_table();
 
@@ -27,9 +39,33 @@ class ContextLinkerManagerContentObjectsBrowserComponent extends ContextLinkerMa
 
     function get_table()
     {
-        $condition = new EqualityCondition(ContentObject :: PROPERTY_STATE, ContentObject :: STATE_NORMAL);
-        $table = new ContentObjectBrowserTable($this, array(Application :: PARAM_APPLICATION => 'context_linker', Application :: PARAM_ACTION => ContextLinkerManager :: ACTION_BROWSE_CONTENT_OBJECTS),$condition);
+        
+        $table = new ContentObjectBrowserTable($this, array(Application :: PARAM_APPLICATION => 'context_linker', Application :: PARAM_ACTION => ContextLinkerManager :: ACTION_BROWSE_CONTENT_OBJECTS),$this->get_condition());
         return $table->as_html();
+    }
+
+    function get_action_bar()
+    {
+        $action_bar = new ActionBarRenderer(ActionBarRenderer :: TYPE_HORIZONTAL);
+        $action_bar->set_search_url($this->get_url());
+        
+        return $action_bar;
+    }
+
+    function get_condition()
+    {
+
+        $conditions = array();
+        $search = $this->action_bar->get_query();
+
+        if (isset($search) && $search != '')
+        {
+            $conditions[] = new PatternMatchCondition(ContentObject :: PROPERTY_TITLE, '*' . $search . '*');
+        }
+
+        $conditions[] = new EqualityCondition(ContentObject :: PROPERTY_STATE, ContentObject :: STATE_NORMAL);
+        $condition = new AndCondition($conditions);
+        return $condition;
     }
 }
 ?>
