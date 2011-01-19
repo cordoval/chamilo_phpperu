f24da74269b0b04d89c6f1c3c45bea4dbbd0b139
systho <devnull@localhost>
2011-01-19 13:10:58 +0100

    updating phpunit to new version, removing transformation of notices to exception because it causes crashes

--- script/lib/PHPUnit/Util/Fileloader.php
+++ script/lib/PHPUnit/Util/Fileloader.php
@@ -2,7 +2,7 @@
 /**
  * PHPUnit
  *
- * Copyright (c) 2002-2010, Sebastian Bergmann <sebastian@phpunit.de>.
+ * Copyright (c) 2002-2011, Sebastian Bergmann <sebastian@phpunit.de>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -37,7 +37,7 @@
  * @package    PHPUnit
  * @subpackage Util
  * @author     Sebastian Bergmann <sebastian@phpunit.de>
- * @copyright  2002-2010 Sebastian Bergmann <sebastian@phpunit.de>
+ * @copyright  2002-2011 Sebastian Bergmann <sebastian@phpunit.de>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @link       http://www.phpunit.de/
  * @since      File available since Release 2.3.0
@@ -49,9 +49,9 @@
  * @package    PHPUnit
  * @subpackage Util
  * @author     Sebastian Bergmann <sebastian@phpunit.de>
- * @copyright  2002-2010 Sebastian Bergmann <sebastian@phpunit.de>
+ * @copyright  2002-2011 Sebastian Bergmann <sebastian@phpunit.de>
  * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
- * @version    Release: 3.5.5
+ * @version    Release: 3.5.9
  * @link       http://www.phpunit.de/
  * @since      Class available since Release 2.3.0
  */
@@ -68,21 +68,21 @@ class PHPUnit_Util_Fileloader
      */
     public static function checkAndLoad($filename, $syntaxCheck = FALSE)
     {
-        if (!is_readable($filename)) {
-            $filename = './' . $filename;
-        }
+        $includePathFilename = PHPUnit_Util_Filesystem::fileExistsInIncludePath(
+          $filename
+        );
 
-        if (!is_readable($filename)) {
+        if (!$includePathFilename || !is_readable($includePathFilename)) {
             throw new RuntimeException(
               sprintf('Cannot open file "%s".' . "\n", $filename)
             );
         }
 
         if ($syntaxCheck) {
-            self::syntaxCheck($filename);
+            self::syntaxCheck($includePathFilename);
         }
 
-        self::load($filename);
+        self::load($includePathFilename);
     }
 
     /**
@@ -94,10 +94,6 @@ class PHPUnit_Util_Fileloader
      */
     public static function load($filename)
     {
-        $filename = PHPUnit_Util_Filesystem::fileExistsInIncludePath(
-          $filename
-        );
-
         $oldVariableNames = array_keys(get_defined_vars());
 
         include_once $filename;
