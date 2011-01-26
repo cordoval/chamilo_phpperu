5db05379e73f883919c0b64ffce90124deceb6ea
vanpouckesven <devnull@localhost>
2011-01-26 14:17:50 +0100

    Task #2607 Fixed the bug of the descriptions access details

---
+++ application/weblcms/tool/description/php/component/reporting_viewer.class.php
@@ -0,0 +1,31 @@
+<?php
+namespace application\weblcms\tool\description;
+
+use application\weblcms\Tool;
+use common\libraries\Breadcrumb;
+use common\libraries\BreadcrumbTrail;
+use common\libraries\Request;
+use application\weblcms\ToolComponent;
+use common\libraries\DelegateComponent;
+use common\libraries\Translation;
+
+class DescriptionToolReportingViewerComponent extends DescriptionTool implements DelegateComponent
+{
+
+    function run()
+    {
+        ToolComponent :: launch($this);
+    }
+
+    function add_additional_breadcrumbs(BreadcrumbTrail $breadcrumbtrail)
+    {
+        $breadcrumbtrail->add(new Breadcrumb($this->get_url(array(Tool :: PARAM_ACTION => Tool :: ACTION_BROWSE)), Translation :: get('DescriptionToolBrowserComponent')));
+        $breadcrumbtrail->add(new Breadcrumb($this->get_url(array(Tool :: PARAM_ACTION => Tool :: ACTION_VIEW, Tool :: PARAM_PUBLICATION_ID => Request :: get(Tool :: PARAM_PUBLICATION_ID))), Translation :: get('DescriptionToolViewerComponent')));
+    }
+
+    function get_additional_parameters()
+    {
+        return array(Tool :: PARAM_PUBLICATION_ID, Tool :: PARAM_COMPLEX_ID, Tool :: PARAM_TEMPLATE_NAME);
+    }
+}
+?>
