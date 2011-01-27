625f2e99a0a802f686c131c5a35d73ed2e43abdc
vanpouckesven <devnull@localhost>
2011-01-27 09:34:33 +0100

    Task #2654 Migration: fixed the links migration

--- migration/php/lib/platform/dokeos185/data_class/dokeos185_link.class.php
+++ migration/php/lib/platform/dokeos185/data_class/dokeos185_link.class.php
@@ -223,7 +223,16 @@ class Dokeos185Link extends Dokeos185CourseDataMigrationDataClass
         //create link in database
         $chamilo_link->create_all();
 
-        $this->create_publication($chamilo_link, $new_course_code, $new_user_id, 'link', $new_publication_category_id, $new_to_user_id, $new_to_group_id, $this->get_on_homepage());
+        if(is_null($this->get_on_homepage()))
+        {
+            $on_homepage = 0;
+        }
+        else
+        {
+            $on_homepage = $this->get_on_homepage();
+        }
+
+        $this->create_publication($chamilo_link, $new_course_code, $new_user_id, 'link', $new_publication_category_id, $new_to_user_id, $new_to_group_id, $on_homepage);
 
         $this->create_id_reference($this->get_id(), $chamilo_link->get_id());
         $this->set_message(Translation :: get('GeneralConvertedMessage', array('TYPE' => 'link', 'OLD_ID' => $this->get_id(), 'NEW_ID' => $chamilo_link->get_id())));
